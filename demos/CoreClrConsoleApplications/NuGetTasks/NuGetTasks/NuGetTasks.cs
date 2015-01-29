bb9c1471a9f57a83b117132d5ccd1a2e6b697492
Krzysztof Cwalina <kcwalina@microsoft.com>
2015-01-29 10:48:51 -0800

    Simple demo showing a console application written for the CoreCLR
    The HelloWorld solution can be opened in Visual Studio,
    or the project can be built from the command line using msbuild.
    The build process will create a bin\Debug folder with HelloWorld.exe
    which can be run like any other executable.
    Please note that the managed application is in HelloWorld.dll.
    HelloWorld.exe is just as simple CoreCLR host (renamed CoreConsole.exe)
    which loads a manged application with the same name as the exe but with a dll extension.
    Also, keep in mind that the the msbuild scripts used for this demo,
    and the custom task are experimental and probably won’t work for more complex applications.

---
+++ demos/CoreClrConsoleApplications/NuGetTasks/NuGetTasks/NuGetTasks.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Microsoft.NuGet
+{
+    public class ResolvePackages : Task
+	{
+		[Required]
+		public string PackagesDirectory { get; set; }
+
+		[Required]
+		public string PackagesConfig { get; set; }
+
+		[Output]
+		public ITaskItem[] References { get; private set; }
+
+		[Output]
+        public ITaskItem[] Dependencies { get; private set; }
+
+		public override bool Execute()
+		{
+            Log.LogMessage(MessageImportance.Low, "Using PackagesDirectory: {0}", PackagesDirectory);
+            string nuGetDirectory = Path.Combine(PackagesDirectory, @"NuGetExe");
+            string nuGetPath = Path.Combine(nuGetDirectory, @"nuget.exe");
+
+            PackageManager.InstallNugetExe(nuGetPath);
+            if(!PackageManager.InstallPackages(PackagesDirectory, PackagesConfig, nuGetPath, Log)) { return false; }
+
+            List<ITaskItem> references = new List<ITaskItem>();
+            List<ITaskItem> dependencies = new List<ITaskItem>();
+            dependencies.Add(new TaskItem(@"NotYetPackages\CoreCLR\coreclr.dll"));
+            dependencies.Add(new TaskItem(@"NotYetPackages\CoreCLR\mscorlib.dll"));
+
+            var installedPackages = PackageManager.ReadPackagesConfig(PackagesConfig);
+            foreach (var package in installedPackages)
+            {
+                var packageRoot = Path.Combine(PackagesDirectory, package.FullName);
+                var referencePath = Path.Combine(packageRoot, "lib\\contract", package.Id + ".dll");
+                var dependencyPath = Path.Combine(packageRoot, "lib\\aspnetcore50", package.Id + ".dll");
+       
+                if (!File.Exists(referencePath))
+                {
+                    Log.LogWarning("Reference not found: {0}", referencePath);
+                }
+                else
+                {
+                    references.Add(new TaskItem(referencePath));
+                    Log.LogMessage(MessageImportance.Low, "Adding reference: {0}", referencePath);
+                }
+
+                if (!File.Exists(dependencyPath))
+                {
+                    Log.LogWarning("Dependency not found: {0}", dependencyPath);
+                }
+                else
+                {
+                    dependencies.Add(new TaskItem(dependencyPath));
+                    Log.LogMessage(MessageImportance.Low, "Adding dependency: {0}", dependencyPath);
+                }
+            }
+
+            References = references.ToArray();
+            Dependencies = dependencies.ToArray();
+
+            return true;
+        }
+	}
+}
