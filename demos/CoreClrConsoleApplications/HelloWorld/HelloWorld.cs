2992012e045cebd52c1f01799097ec93e4ae2c9e
Immo Landwerth <immol@microsoft.com>
2015-02-03 08:48:29 -0800

    Make coding style consistent with corefx

--- demos/CoreClrConsoleApplications/HelloWorld/HelloWorld.cs
+++ demos/CoreClrConsoleApplications/HelloWorld/HelloWorld.cs
@@ -1,16 +1,20 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 using System;
 
-class Program {
-	static void Main(string[] args)
-	{
-		Console.WriteLine("Hello World!");
-		foreach (var arg in args)
-		{
-			Console.Write("Hello ");
-			Console.Write(arg);
-			Console.WriteLine("!");
-		}
-		Console.WriteLine("Press ENTER to exit ...");
-		Console.ReadLine();
-	}
-} 
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        foreach (var arg in args)
+        {
+            Console.Write("Hello ");
+            Console.Write(arg);
+            Console.WriteLine("!");
+        }
+        Console.WriteLine("Press ENTER to exit ...");
+        Console.ReadLine();
+    }
+}
