eab992c599c7847af7067f19c115ce427567cc5d
Amy (Xin) Xu <axxu3795@gmail.com>
2015-07-06 13:35:11 -0700

    Set up skeleton of System.Drawing.Graphics and Tests

---
+++ src/System.Drawing.Graphics/tests/UnitTests.cs
@@ -0,0 +1,16 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Threading;
+using Xunit;
+
+public partial class GraphicsUnitTests
+{
+    [Fact]
+    public static void FileSystemWatcher_Created_File()
+    {
+        //empty example method from Krzysztof's proj
+    }
+
+}
