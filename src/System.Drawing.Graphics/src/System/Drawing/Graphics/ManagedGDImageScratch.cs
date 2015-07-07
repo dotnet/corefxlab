641f2167eb72f1b431a14b4254c18dc81280d272
Amy (Xin) Xu <axxu3795@gmail.com>
2015-07-07 09:54:24 -0700

    Added x86 build of libgd and test wrapper method

---
+++ src/System.Drawing.Graphics/src/System/Drawing/Graphics/ManagedGDImageScratch.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+namespace System.Drawing.Graphics
+{
+    class ManagedGDImageScratch
+    {
+        [Fact]
+        public void Test()
+        {
+            IntPtr imgPtr = DLLImports.gdImageCreate(1, 1);
+            System.Console.WriteLine(imgPtr.ToInt64());
+            System.Console.WriteLine("hi");
+        }
+    }
+}
---
+++ src/System.Drawing.Graphics/tests/UnitTests.cs
@@ -0,0 +1,21 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Threading;
+using Xunit;
+using System.Drawing.Graphics;
+
+public partial class GraphicsUnitTests
+{
+    [Fact]
+    public void Test()
+    {
+        System.Console.WriteLine("hi");
+
+        IntPtr imgPtr = DLLImports.gdImageCreate(1, 1);
+        System.Console.WriteLine(imgPtr.ToInt64());
+    }
+    
+}
+
