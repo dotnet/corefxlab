7489c0eabc17476f78d99525eef4d1fcc42c6de4
dalbanhi <david_alban_h@hotmail.com>
2015-07-07 16:00:16 -0700

    Added Image Extensions Class, refactored most tests from before repo switch to XUnit

---
+++ src/System.Drawing.Graphics/src/System/Drawing/Graphics/ImageExtensions.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Microsoft. All rights reserved. 
+// Licensed under the MIT license. See LICENSE file in the project root for full license information. 
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Drawing.Graphics
+{
+    public static class ImageExtensions
+    {
+        //Resizing
+        public static void Resize(this Image image, int width, int height)
+        {
+            throw new NotImplementedException();
+        }
+
+        //Transparency
+        public static void SetTransparency(this Image image, double percentTransparency)
+        {
+            throw new NotImplementedException();
+        }
+
+        //Stamping an Image onto another
+        public static void Draw(this Image image, Image imageToDraw, int xOffset, int yOffset)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
