02811224b84d9ab98a26b4f3a00aa9c93068afcf
dalbanhi <david_alban_h@hotmail.com>
2015-07-10 15:50:07 -0700

    Implemented WriteToFile (with Tarek's help) and restructured to make the Struct Ptr public

--- src/System.Drawing.Graphics/src/System/Drawing/Graphics/ImageExtensions.cs
+++ src/System.Drawing.Graphics/src/System/Drawing/Graphics/ImageExtensions.cs
@@ -15,44 +15,7 @@ namespace System.Drawing.Graphics
         //Resizing
         public static Image Resize(this Image sourceImage, int width, int height)
         {
-
             throw new NotImplementedException();
-            //    //throw new NotImplementedException();
-            //    if (width > 0 && height > 0)
-            //    {
-            //        Image imageToRet = Image.Create(width, height);
-
-            //        unsafe
-            //        {
-            //            //fix the pixelData[] for both the new blank image and 
-            //            //source original image
-            //            fixed (byte* imageToRetPixelDataPtr = imageToRet.PixelData)
-            //            fixed (byte* sourceImagePixelDataPtr = sourceImage.PixelData)
-            //            {
-            //                //make a new temporary structure for the new blank image
-            //                DLLImports.gdImageStruct imageToRetStructure = new DLLImports.gdImageStruct();
-            //                //pin pixel data of new image to struct
-            //                imageToRetStructure.pixels = imageToRetPixelDataPtr;
-
-            //                //make a new temporary structure for the source image
-            //                DLLImports.gdImageStruct sourceImageStructure = new DLLImports.gdImageStruct();
-            //                //pin pixel data of  source image to struct
-            //                sourceImageStructure.pixels = sourceImagePixelDataPtr;
-
-            //                //call native library to resize
-            //                DLLImports.gdImageCopyResized(ref imageToRetStructure, ref sourceImageStructure, 0, 0,
-            //                                    0, 0, imageToRet.WidthInPixels, imageToRet.HeightInPixels, sourceImage.WidthInPixels, sourceImage.HeightInPixels);
-
-            //            }
-            //        }
-            //        //return image
-            //        //._pixelData[] should be changed 
-            //        return imageToRet;
-            //    }
-            //    else
-            //    {
-            //        throw new InvalidOperationException("Parameters for resizing an image must be positive integers.");
-            //    }
         }
 
             //Transparency
