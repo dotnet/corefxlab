50eab0f537146085dc00577c072d05a993848535
Amy (Xin) Xu <axxu3795@gmail.com>
2015-08-19 11:16:24 -0700

    Added image filters

--- src/System.Drawing.Graphics/src/System/Drawing/Graphics/ImageExtensions.cs
+++ src/System.Drawing.Graphics/src/System/Drawing/Graphics/ImageExtensions.cs
@@ -7,7 +7,31 @@ namespace System.Drawing.Graphics
 {
     public static class ImageExtensions
     {
-        
+
+        public static double[,] GreyScaleMatrix = { { 1, 0, 0, 0 },
+                                                      { 0, 0.3, 0.3, 0.3},
+                                                      { 0, 0.59, 0.59, 0.59},
+                                                      { 0, 0.11, 0.11, 0.11 } };
+
+        public static double[,] SepiaMatrix = { {1, 0, 0, 0},
+                                                 {0, 0.393, 0.349, 0.272 },
+                                                 {0, 0.769, 0.686,0.534},
+                                                 {0 ,0.189, 0.168 , 0.131}
+                                                 };
+
+        public static double[,] NegativeMatrix = { {1, 0, 0, 0},
+                                                {0, -1, 0, 0 },
+                                                {0, 0, -1, 0},
+                                                { 0 , 0, 0, -1}
+                                                 };
+
+        //test...
+        public static double[,] AlphaMatrix040 = { {0.4, 0, 0, 0},
+                                                   {0, 1, 0, 0 },
+                                                   {0, 0, 1, 0},
+                                                   {0, 0, 0, 1}
+                                                 };
+
         //Resizing
         public static Image Resize(this Image sourceImage, int width, int height)
         {
@@ -57,6 +81,67 @@ namespace System.Drawing.Graphics
                         //make a new color with the new alpha to set the pixel
                         currentColor = (currentColor & 0x00ffffff | ((int)currentAlpha << 24));
                         pStruct->tpixels[y][x] = currentColor;
+                    }
+                }
+            }
+        }
+
+        //helper method for each pixel
+        private static int ApplyFilter(int currentColor, double[,] matrixMultiplier)
+        {
+   
+            //isolate each color
+            double currentAlpha = (currentColor >> 24) & 0xff;
+            double currentRed = (currentColor >> 16) & 0xff;
+            double currentBlue = (currentColor >> 8) & 0xff;
+            double currentGreen = currentColor & 0xff;
+
+            double[] pixel = { currentAlpha, currentRed, currentBlue, currentGreen };
+            double[] pixelFinals = new double[4];
+            for (int i = 0; i < pixel.Length; i++)
+            {
+                double sum = 0;
+                for (int j = 0; j < pixel.Length; j++)
+                {
+                    //should alpha be dealt with seperately???????????? :(
+                    //which direction should it go...
+                    if(i == 0 && j == 0)
+                    {
+                        sum += pixel[j] + ((127 - pixel[j]) * (1 - matrixMultiplier[j, i]));
+
+                    }
+                    else
+                    {
+                        sum += pixel[j] * matrixMultiplier[j, i];
+                    }
+                }
+                if(sum > 255)
+                {
+                    sum = 255;
+                }
+
+                //deal with overflow that is not intentional negation
+                pixelFinals[i] = (int)sum & 0xff;
+            }
+            //shift back into position
+            currentColor = ((int)pixelFinals[0] << 24) | ((int)pixelFinals[1] << 16) | ((int)pixelFinals[2] << 8) | ((int)pixelFinals[3]);
+            return currentColor;
+
+        }
+        public static void ApplyMatrixMultiplier(this Image sourceImage, double[,] matrixMultiplier)
+        {
+
+
+            unsafe
+            {
+                DLLImports.gdImageStruct* pStruct = (DLLImports.gdImageStruct*)sourceImage.gdImageStructPtr;
+
+                for (int y = 0; y < sourceImage.HeightInPixels; y++)
+                {
+                    for (int x = 0; x < sourceImage.WidthInPixels; x++)
+                    {
+                        int currentColor = pStruct->tpixels[y][x];
+                        pStruct->tpixels[y][x] = ApplyFilter(currentColor, matrixMultiplier);
                     }
                 }
             }
--- src/System.Drawing.Graphics/tests/UnitTests.cs
+++ src/System.Drawing.Graphics/tests/UnitTests.cs
@@ -21,6 +21,23 @@ public partial class GraphicsUnitTests
     //static Image jpgdog;
     //static Image pngdog;
 
+    [Fact]
+    public static void ImageFilterTests()
+    {
+        Image img1 = Jpg.Load(Interop.PerformanceTestJpegCat);
+        img1.ApplyMatrixMultiplier(ImageExtensions.GreyScaleMatrix);
+        Jpg.WriteToFile(img1, Interop.PerformanceTestsResultsDirectory + "GreyscaleCat.jpg");
+
+        Image img2 = Jpg.Load(Interop.PerformanceTestJpegCat);
+        img2.ApplyMatrixMultiplier(ImageExtensions.SepiaMatrix);
+        Jpg.WriteToFile(img2, Interop.PerformanceTestsResultsDirectory + "SepiaCat.jpg");
+
+        Image img3 = Jpg.Load(Interop.PerformanceTestJpegCat);
+        img3.ApplyMatrixMultiplier(ImageExtensions.NegativeMatrix);
+        Jpg.WriteToFile(img3, Interop.PerformanceTestsResultsDirectory + "NegativeCat.jpg");
+    }
+
+
     [Fact(Skip ="Until filepath issue is resolved")]
     public static void RunAllTests()
     {
@@ -31,7 +48,7 @@ public partial class GraphicsUnitTests
             runTests(10);
             runTests(1000);
             runTests(5000);
-        streamwriter.Dispose();
+            streamwriter.Dispose();
             fstream.Dispose();
     }
 
