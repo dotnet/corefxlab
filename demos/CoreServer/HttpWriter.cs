ff12fe787e63b9c5dccac5535f6b5ea22b73104e
Krzysztof Cwalina <kcwalina@microsoft.com>
2015-09-16 14:34:03 -0700

    Ported response generation to non-allocating formatter

---
+++ demos/CoreServer/HttpWriter.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.IO.Buffers;
+using System.Text.Formatting;
+using System.Text.Utf8;
+using System.Threading;
+
+namespace System.Net.Http.Buffered
+{
+    public struct HttpWriter
+    {
+        public static Utf8String HttpNewline = new Utf8String(13, 10);
+
+        public static void WriteCommonHeaders(BufferFormatter formatter, string responseLine)
+        {
+            var currentTime = DateTime.UtcNow;
+            formatter.Append(responseLine);
+            formatter.Append(HttpNewline);
+            formatter.Append("Date: ");
+            formatter.Append(currentTime, 'R');
+            formatter.Append(HttpNewline);
+            formatter.Append("Server: .NET Core Sample Server");
+            formatter.Append(HttpNewline);
+            formatter.Append("Last-Modified: ");
+            formatter.Append(currentTime, 'R');
+            formatter.Append(HttpNewline);
+            formatter.Append("Content-Type: text/html; charset=UTF-8");
+            formatter.Append(HttpNewline);
+            formatter.Append("Connection: close");
+            formatter.Append(HttpNewline);
+            formatter.Append(HttpNewline);
+        }
+    }
+}
