8e610e87432687c9361c183fccbdcd119f24978a
Krzysztof Wicher <kwicher@microsoft.com>
2015-10-13 12:34:56 -0700

    Fix Equals issue

---
+++ src/System.Text.Utf8/src/System/Text/Utf16/Utf16LittleEndianCodePointEnumerable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace System.Text.Utf16
+{
+    // TODO: Should this and Utf8 code point enumerators/enumerable be subclasses of Utf8/16Encoder?
+    public struct Utf16LittleEndianEnumerable : IEnumerable<UnicodeCodePoint>, IEnumerable
+    {
+        private string _s;
+
+        public Utf16LittleEndianEnumerable(string s)
+        {
+            _s = s;
+        }
+
+        public Utf16LittleEndianCodePointEnumerator GetEnumerator()
+        {
+            return new Utf16LittleEndianCodePointEnumerator(_s);
+        }
+
+        IEnumerator<UnicodeCodePoint> IEnumerable<UnicodeCodePoint>.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
