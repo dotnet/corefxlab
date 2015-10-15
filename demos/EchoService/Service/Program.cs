e3d4c5292f164e809e860399ce5f36a60b6ccc36
Krzysztof Cwalina <kcwalina@microsoft.com>
2015-10-14 17:04:55 -0700

    Refactored libuv wrapper into a library and added an Echo service using it

---
+++ demos/EchoService/Service/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Libuv;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var loop = new UVLoop();
+
+        var listener = new TcpListener("0.0.0.0", 7, loop);
+
+        listener.ConnectionAccepted += (Tcp connection) =>
+        {
+            connection.ReadCompleted += (ByteSpan data) =>
+            {
+                connection.TryWrite(data);
+            };
+
+            connection.ReadStart();
+        };
+
+        listener.Listen();
+        loop.Run();
+    }
+}
+
