233cdcb104b97ca4181b7203f5887b8943e862fe
Krzysztof Cwalina <kcwalina@microsoft.com>
2015-10-22 08:32:25 -0700

    Added QOTD service

---
+++ demos/QotdService/Service/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Libuv;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        byte[] quote = Encoding.ASCII.GetBytes("Insanity: doing the same thing over and over again and expecting different results. - Albert Einstein");
+        
+        var loop = new UVLoop();
+
+        var listener = new TcpListener("0.0.0.0", 17, loop);
+
+        listener.ConnectionAccepted += (Tcp connection) =>
+        {
+            connection.ReadCompleted += (ByteSpan data) =>
+            {
+                unsafe
+                {
+                    fixed (byte* pQuote = quote)
+                    {
+                        connection.TryWrite(new ByteSpan(pQuote, quote.Length));
+                    }
+                }
+            };
+
+            connection.ReadStart();
+        };
+
+        listener.Listen();
+        loop.Run();
+    }
+}
+
