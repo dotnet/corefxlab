677c6b68581c00b863803c05f9ffdfe6e28b7cc2
Krzysztof Wicher <kwicher@microsoft.com>
2015-11-03 11:54:26 -0800

    Clean up Utf8String

---
+++ src/System.Text.Utf8/tests/TypeConstraintsTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection;
+using Xunit;
+
+namespace System.Text.Utf8.Tests
+{
+    public class TypeConstraintsTests
+    {
+        [Fact]
+        public void Utf8StringIsAStruct()
+        {
+            var utf8String = "anyString"u8;
+            Assert.True(utf8String.GetType().GetTypeInfo().IsValueType);
+        }
+
+        [Fact]
+        public void Utf8StringCodeUnitsEnumeratorIsAStruct()
+        {
+            var utf8String = "anyString"u8;
+            var utf8CodeUnitsEnumerator = utf8String.GetEnumerator();
+            Assert.True(utf8String.GetType().GetTypeInfo().IsValueType);
+        }
+
+        [Fact]
+        public void Utf8StringCodePointEnumerableIsAStruct()
+        {
+            var utf8String = "anyString"u8;
+            Assert.True(utf8String.CodePoints.GetType().GetTypeInfo().IsValueType);
+        }
+
+        [Fact]
+        public void Utf8StringCodePointEnumeratorIsAStruct()
+        {
+            var utf8String = "anyString"u8;
+            var utf8CodePointEnumerator = utf8String.CodePoints.GetEnumerator();
+            Assert.True(utf8CodePointEnumerator.GetType().GetTypeInfo().IsValueType);
+        }
+
+        [Fact]
+        public void Utf8StringReverseCodePointEnumeratorIsAStruct()
+        {
+            var utf8String = "anyString"u8;
+            var utf8CodePointEnumerator = utf8String.CodePoints.GetReverseEnumerator();
+            Assert.True(utf8CodePointEnumerator.GetType().GetTypeInfo().IsValueType);
+        }
+    }
+}
