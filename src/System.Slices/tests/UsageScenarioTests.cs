e1cac2a6114e19bebbbf4e808a6fa9fe369fdc19
Jonathan Miller <jonmill@microsoft.com>
2015-11-03 17:28:04 -0800

    Adding ActiveIssue tag to failing System.Drawing.Graphics tests and System.Text.Json tests

--- src/System.Slices/tests/UsageScenarioTests.cs
+++ src/System.Slices/tests/UsageScenarioTests.cs
@@ -33,7 +33,7 @@ namespace System.Slices.Tests
         [InlineData(new byte[] { 0, 1, 2 })]
         [InlineData(new byte[] { 0, 1, 2, 3 })]
         [InlineData(new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 })]
-        [CLSCompliant(false)]
+        [CLSCompliant(false)] // TODO: find out why the assembly-level CLSCompliant=false doesn't work
         public void CtorSpanOverByteArrayValidCasesWithPropertiesAndBasicOperationsChecks(byte[] array)
         {
             Span<byte> span = new Span<byte>(array);
@@ -304,7 +304,7 @@ namespace System.Slices.Tests
             (byte[])null,
             new byte[] { 7, 7, 7, 7, 7, 7 }, 4, 2,
             new byte[] { 7, 7, 7, 7, 7, 7 }, 5, 1)]
-        [CLSCompliant(false)]
+        [CLSCompliant(false)] // TODO: find out why the assembly-level CLSCompliant=false doesn't work
         public void SpanOfByteCopyToAnotherSpanOfByteTwoDifferentBuffersValidCases(byte[] expected, byte[] a, int aidx, int acount, byte[] b, int bidx, int bcount)
         {
             if (expected != null)
