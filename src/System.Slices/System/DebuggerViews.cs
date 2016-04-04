5ac7166f24c52eafec1aec02fb3b82a0e08495c9
Krzysztof Cwalina <kcwalina@microsoft.com>
2016-04-04 14:50:21 -0700

    Added debugger viewers to slices

---
+++ src/System.Slices/System/DebuggerViews.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Diagnostics;
+
+namespace System
+{
+    internal class SpanDebuggerView<T>
+    {
+        private ReadOnlySpan<T> _slice;
+
+        public SpanDebuggerView(ReadOnlySpan<T> slice)
+        {
+            _slice = slice;
+        }
+
+        [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
+        public T[] Items
+        {
+            get
+            {
+                return _slice.CreateArray();
+            }
+        }
+    }
+}
