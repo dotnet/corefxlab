a73700fd9f71a262701740419db09c4a94baaa23
Wes Haggard <Wes.Haggard@microsoft.com>
2016-05-22 01:41:13 -0700

    Update CoreApp sample to RC2 packages

--- samples/NetCoreSample/NetCoreXunitTestLibrary/Properties/AssemblyInfo.cs
+++ samples/NetCoreSample/NetCoreXunitTestLibrary/Properties/AssemblyInfo.cs
@@ -14,7 +14,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
 
 // Version information for an assembly consists of the following four values:
 //
