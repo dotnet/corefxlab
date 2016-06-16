2a529ea40460ec42912a64f9c7f145ec2adbd0ba
Jon Fortescue <t-jonf@microsoft.com>
2016-06-16 15:09:46 -0700

    Added uint parse tests for byte[] and byte* and improved previous tests. Added parse failure tests.

--- tests/System.Text.Formatting.Tests/Parsing/ParserTests.cs
+++ tests/System.Text.Formatting.Tests/Parsing/ParserTests.cs
@@ -7,29 +7,37 @@ namespace System.Text.Formatting.Tests
     public class ParserTests
     {
         [Theory]
-        [InlineData(new byte[] { 0x31, 0x37, 0x32, 0x38 } /* "1728" */, 0, 1728, 4)]
+        [InlineData(new byte[] { 0x31, 0x37, 0x32, 0x38 } /* "1728" */, true, 0, 1728, 4)]
         [InlineData(new byte[] { 0xCF, 0xA0, 0x20, 0xDF, 0xB7, 0xDE, 0x96, 0xDD, 0xAE, 0x31, 0x37, 0x35, 0x31, 0x31, 0x31, 0x30 }, // stuff + "1751110"
-            9, 1751110, 7)]
+            true, 9, 1751110, 7)]
         [InlineData(new byte[] { 0x39, 0x38, 0x37, 0x66, 0x61, 0x64, 0x66, 0x61, 0x6B, 0x6A, 0x6C, 0x66, 0x68, 0x6B }, // "987" + trailing text
-            0, 987, 3)]
-        public void ParseUtf8ByteArrayToUlong(byte[] text, int index, uint expectedValue, int expectedBytesConsumed)
+            true, 0, 987, 3)]
+        // "The biggest ulong is 9223372036854775808."
+        [InlineData(new byte[] { 0x54, 0x68, 0x65, 0x20, 0x62, 0x69, 0x67, 0x67, 0x65, 0x73, 0x74, 0x20, 0x75, 0x6C, 0x6F, 0x6E, 0x67, 0x20, 0x69, 0x73, 0x20, 0x39, 0x32, 0x32, 0x33, 0x33, 0x37, 0x32, 0x30, 0x33, 0x36, 0x38, 0x35, 0x34, 0x37, 0x37, 0x35, 0x38, 0x30, 0x38, 0x2E },
+            true, 21, 9223372036854775808, 19)]
+        [InlineData(new byte[] { 0x49, 0x20, 0x61, 0x6D, 0x20, 0x31 } /* I am 1 */, false, 0, 0, 0)]
+        public void ParseUtf8ByteArrayToUlong(byte[] text, bool expectSuccess, int index, ulong expectedValue, int expectedBytesConsumed)
         {
             ulong parsedValue;
             int bytesConsumed;
             bool result = InvariantParser.TryParse(text, index, out parsedValue, out bytesConsumed);
 
-            Assert.True(result);
+            Assert.Equal(result, expectSuccess);
             Assert.Equal(parsedValue, expectedValue);
             Assert.Equal(bytesConsumed, expectedBytesConsumed);
         }
 
         [Theory]
-        [InlineData(new byte[] { 0x31, 0x37, 0x32, 0x38 } /* "1728" */, 0, 1728, 4)]
+        [InlineData(new byte[] { 0x31, 0x37, 0x32, 0x38 } /* "1728" */, true, 0, 1728, 4)]
         [InlineData(new byte[] { 0xCF, 0xA0, 0x20, 0xDF, 0xB7, 0xDE, 0x96, 0xDD, 0xAE, 0x31, 0x37, 0x35, 0x31, 0x31, 0x31, 0x30 }, // stuff + "1751110"
-            9, 1751110, 7)]
+            true, 9, 1751110, 7)]
         [InlineData(new byte[] { 0x39, 0x38, 0x37, 0x66, 0x61, 0x64, 0x66, 0x61, 0x6B, 0x6A, 0x6C, 0x66, 0x68, 0x6B }, // "987" + trailing text
-            0, 987, 3)]
-        public unsafe void ParseUtf8ByteStarToUlong(byte[] text, int index, uint expectedValue, int expectedBytesConsumed)
+            true, 0, 987, 3)]
+        // "The biggest ulong is 9223372036854775808."
+        [InlineData(new byte[] { 0x54, 0x68, 0x65, 0x20, 0x62, 0x69, 0x67, 0x67, 0x65, 0x73, 0x74, 0x20, 0x75, 0x6C, 0x6F, 0x6E, 0x67, 0x20, 0x69, 0x73, 0x20, 0x39, 0x32, 0x32, 0x33, 0x33, 0x37, 0x32, 0x30, 0x33, 0x36, 0x38, 0x35, 0x34, 0x37, 0x37, 0x35, 0x38, 0x30, 0x38, 0x2E },
+            true, 21, 9223372036854775808, 19)]
+        [InlineData(new byte[] { 0x49, 0x20, 0x61, 0x6D, 0x20, 0x31 } /* I am 1 */, false, 0, 0, 0)]
+        public unsafe void ParseUtf8ByteStarToUlong(byte[] text, bool expectSuccess, int index, ulong expectedValue, int expectedBytesConsumed)
         {
             ulong parsedValue;
             int bytesConsumed;
@@ -39,7 +47,47 @@ namespace System.Text.Formatting.Tests
 
             bool result = InvariantParser.TryParse(arrayPointer, index, out parsedValue, out bytesConsumed);
 
-            Assert.True(result);
+            Assert.Equal(result, expectSuccess);
+            Assert.Equal(parsedValue, expectedValue);
+            Assert.Equal(bytesConsumed, expectedBytesConsumed);
+        }
+
+        [Theory]
+        [InlineData(new byte[] { 0x31, 0x37, 0x32, 0x38 } /* "1728" */, true, 0, 1728, 4)]
+        [InlineData(new byte[] { 0xCF, 0xA0, 0x20, 0xDF, 0xB7, 0xDE, 0x96, 0xDD, 0xAE, 0x31, 0x37, 0x35, 0x31, 0x31, 0x31, 0x30 }, // stuff + "1751110"
+            true, 9, 1751110, 7)]
+        [InlineData(new byte[] { 0x39, 0x38, 0x37, 0x66, 0x61, 0x64, 0x66, 0x61, 0x6B, 0x6A, 0x6C, 0x66, 0x68, 0x6B }, // "987" + trailing text
+            true, 0, 987, 3)]
+        [InlineData(new byte[] { 0x49, 0x20, 0x61, 0x6D, 0x20, 0x31 } /* I am 1 */, false, 0, 0, 0)]
+        public void ParseUtf8ByteArrayToUint(byte[] text, bool expectSuccess, int index, uint expectedValue, int expectedBytesConsumed)
+        {
+            uint parsedValue;
+            int bytesConsumed;
+            bool result = InvariantParser.TryParse(text, index, out parsedValue, out bytesConsumed);
+
+            Assert.Equal(result, expectSuccess);
+            Assert.Equal(parsedValue, expectedValue);
+            Assert.Equal(bytesConsumed, expectedBytesConsumed);
+        }
+
+        [Theory]
+        [InlineData(new byte[] { 0x31, 0x37, 0x32, 0x38 } /* "1728" */, true, 0, 1728, 4)]
+        [InlineData(new byte[] { 0xCF, 0xA0, 0x20, 0xDF, 0xB7, 0xDE, 0x96, 0xDD, 0xAE, 0x31, 0x37, 0x35, 0x31, 0x31, 0x31, 0x30 }, // stuff + "1751110"
+            true, 9, 1751110, 7)]
+        [InlineData(new byte[] { 0x39, 0x38, 0x37, 0x66, 0x61, 0x64, 0x66, 0x61, 0x6B, 0x6A, 0x6C, 0x66, 0x68, 0x6B }, // "987" + trailing text
+            true, 0, 987, 3)]
+        [InlineData(new byte[] { 0x49, 0x20, 0x61, 0x6D, 0x20, 0x31 } /* I am 1 */, false, 0, 0, 0)]
+        public unsafe void ParseUtf8ByteStarToUint(byte[] text, bool expectSuccess, int index, uint expectedValue, int expectedBytesConsumed)
+        {
+            uint parsedValue;
+            int bytesConsumed;
+
+            GCHandle arrayPinned = GCHandle.Alloc(text, GCHandleType.Pinned);
+            byte* arrayPointer = (byte*)arrayPinned.AddrOfPinnedObject();
+
+            bool result = InvariantParser.TryParse(arrayPointer, index, out parsedValue, out bytesConsumed);
+
+            Assert.Equal(result, expectSuccess);
             Assert.Equal(parsedValue, expectedValue);
             Assert.Equal(bytesConsumed, expectedBytesConsumed);
         }
