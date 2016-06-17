a55d208697f2f04098a9a388be8e058032e26b71
Jon Fortescue <t-jonf@microsoft.com>
2016-06-16 17:14:09 -0700

    Added support and tests for parsing signed integers from UTF-8 byte* and byte[].

---
+++ src/System.Text.Formatting/System/Text/Parsing/InvariantParser_int.cs
@@ -0,0 +1,123 @@
+﻿using System.Diagnostics;
+using System.Text.Formatting;
+using System.Text.Utf8;
+
+namespace System.Text.Parsing
+{
+    public static partial class InvariantParser
+    {
+        /// <summary>
+        /// Parses an unsigned 64-bit integer from a location within a UTF-8 byte array buffer.
+        /// </summary>
+        /// <param name="text">The UTF-8 buffer, passed as a byte array.</param>
+        /// <param name="startIndex">The index location of the value to be parsed within the buffer.</param>
+        /// <param name="value">The parsed value.</param>
+        /// <param name="bytesConsumed">The length (in bytes) of the unparsed value within the UTF-8 buffer.</param>
+        /// <returns>True if parsing is successful; false otherwise.</returns>
+		public static bool TryParse(byte[] text, int startIndex, out int value, out int bytesConsumed)
+        {
+            Precondition.Require(text.Length > 0);
+
+            value = 0;
+            bytesConsumed = 0;
+            bool negative = false;
+
+            if (text[startIndex] == '-')
+            {
+                negative = true;
+                startIndex++;
+                bytesConsumed++;
+            }
+
+            for (int byteIndex = startIndex; byteIndex < text.Length; byteIndex++) // loop through the byte array
+            {
+                if (text[byteIndex] < '0' || text[byteIndex] > '9') // if the next character is not a digit
+                {
+                    if (bytesConsumed == 0) // check to see if we've processed any digits at all
+                    {
+                        value = default(int); // if we haven't, set value to 0 and return false
+                        return false;
+                    }
+                    else
+                    {
+                        if (negative) // We check if the value is negative at the very end to save on comp time
+                        {
+                            value *= -1;
+                        }
+                        return true; // otherwise return true
+                    }
+                }
+                int candidate = (value * 10); // left shift the value
+                candidate += (text[byteIndex] - '0'); // parse the current digit to an int and add it to the temporary value
+                if (candidate >= value) // if it was a digit 0-9, this should be true
+                {
+                    value = candidate;
+                }
+                else // this should never happen, but just in case
+                {
+                    if (negative) // We check if the value is negative at the very end to save on comp time
+                    {
+                        value *= -1;
+                    }
+                    return true;
+                }
+                bytesConsumed++; // increment the number of bytes consumed, then loop
+            }
+
+            if (negative) // We check if the value is negative at the very end to save on comp time
+            {
+                value *= -1;
+            }
+            return true;
+        }
+
+        unsafe public static bool TryParse(byte* text, int startIndex, out int value, out int bytesConsumed)
+        {
+            value = 0;
+            bytesConsumed = 0;
+            bool negative = false;
+
+            if (text[startIndex] == '-')
+            {
+                negative = true;
+                startIndex++;
+                bytesConsumed++;
+            }
+
+            for (int byteIndex = startIndex; ; byteIndex++)
+            {
+                if (text[byteIndex] < '0' || text[byteIndex] > '9')
+                {
+                    if (bytesConsumed == 0)
+                    {
+                        value = default(int);
+                        return false;
+                    }
+                    else
+                    {
+                        if (negative) // We check if the value is negative at the very end to save on comp time
+                        {
+                            value *= -1;
+                        }
+                        return true;
+                    }
+                }
+                int candidate = (value * 10);
+                candidate += (text[byteIndex] - '0');
+                if (candidate >= value)
+                {
+                    value = candidate;
+                }
+                else
+                {
+                    if (negative) // We check if the value is negative at the very end to save on comp time
+                    {
+                        value *= -1;
+                    }
+                    return true;
+                }
+                bytesConsumed++;
+            }
+        }
+    }
+}
--- tests/System.Text.Formatting.Tests/Parsing/ParserTests.cs
+++ tests/System.Text.Formatting.Tests/Parsing/ParserTests.cs
@@ -6,6 +6,7 @@ namespace System.Text.Formatting.Tests
 {
     public class ParserTests
     {
+        #region ulong
         [Theory]
         [InlineData(new byte[] { 0x31, 0x37, 0x32, 0x38 } /* "1728" */, true, 0, 1728, 4)]
         [InlineData(new byte[] { 0xCF, 0xA0, 0x20, 0xDF, 0xB7, 0xDE, 0x96, 0xDD, 0xAE, 0x31, 0x37, 0x35, 0x31, 0x31, 0x31, 0x30 }, // stuff + "1751110"
@@ -51,7 +52,9 @@ namespace System.Text.Formatting.Tests
             Assert.Equal(expectedValue, parsedValue);
             Assert.Equal(expectedBytesConsumed, bytesConsumed);
         }
+        #endregion
 
+        #region uint
         [Theory]
         [InlineData(new byte[] { 0x31, 0x37, 0x32, 0x38 } /* "1728" */, true, 0, 1728, 4)]
         [InlineData(new byte[] { 0xCF, 0xA0, 0x20, 0xDF, 0xB7, 0xDE, 0x96, 0xDD, 0xAE, 0x31, 0x37, 0x35, 0x31, 0x31, 0x31, 0x30 }, // stuff + "1751110"
@@ -91,7 +94,9 @@ namespace System.Text.Formatting.Tests
             Assert.Equal(expectedValue, parsedValue);
             Assert.Equal(expectedBytesConsumed, bytesConsumed);
         }
+        #endregion
 
+        #region ushort
         [Theory]
         [InlineData(new byte[] { 0x31, 0x37, 0x32, 0x38 } /* "1728" */, true, 0, 1728, 4)]
         [InlineData(new byte[] { 0xCF, 0xA0, 0x20, 0xDF, 0xB7, 0xDE, 0x96, 0xDD, 0xAE, 0x33, 0x37, 0x35, 0x31, 0x31 }, // stuff + "37511"
@@ -131,7 +136,9 @@ namespace System.Text.Formatting.Tests
             Assert.Equal(expectedValue, parsedValue);
             Assert.Equal(expectedBytesConsumed, bytesConsumed);
         }
+        #endregion
 
+        #region byte
         [Theory]
         [InlineData(new byte[] { 0x31, 0x37, 0x32 } /* "178" */, true, 0, 172, 3)]
         [InlineData(new byte[] { 0xCF, 0xA0, 0x20, 0xDF, 0xB7, 0xDE, 0x96, 0xDD, 0xAE, 0x33, 0x37 }, // stuff + "37"
@@ -171,5 +178,54 @@ namespace System.Text.Formatting.Tests
             Assert.Equal(expectedValue, parsedValue);
             Assert.Equal(expectedBytesConsumed, bytesConsumed);
         }
+        #endregion
+
+        #region int
+        [Theory]
+        [InlineData(new byte[] { 0x31, 0x37, 0x32, 0x38 } /* "1728" */, true, 0, 1728, 4)]
+        [InlineData(new byte[] { 0xCF, 0xA0, 0x20, 0xDF, 0xB7, 0xDE, 0x96, 0xDD, 0xAE, 0x31, 0x37, 0x35, 0x31, 0x31, 0x31, 0x30 }, // stuff + "1751110"
+            true, 9, 1751110, 7)]
+        [InlineData(new byte[] { 0x39, 0x38, 0x37, 0x66, 0x61, 0x64, 0x66, 0x61, 0x6B, 0x6A, 0x6C, 0x66, 0x68, 0x6B }, // "987" + trailing text
+            true, 0, 987, 3)]
+        // "Letthem-32984eatcake"
+        [InlineData(new byte[] { 0x4C, 0x65, 0x74, 0x74, 0x68, 0x65, 0x6D, 0x2D, 0x33, 0x32, 0x39, 0x38, 0x34, 0x65, 0x61, 0x74, 0x63, 0x61, 0x6B, 0x65 },
+            true, 7, -32984, 6)]
+        [InlineData(new byte[] { 0x49, 0x20, 0x61, 0x6D, 0x20, 0x31 } /* I am 1 */, false, 0, 0, 0)]
+        public void ParseUtf8ByteArrayToInt(byte[] text, bool expectSuccess, int index, int expectedValue, int expectedBytesConsumed)
+        {
+            int parsedValue;
+            int bytesConsumed;
+            bool result = InvariantParser.TryParse(text, index, out parsedValue, out bytesConsumed);
+
+            Assert.Equal(expectSuccess, result);
+            Assert.Equal(expectedValue, parsedValue);
+            Assert.Equal(expectedBytesConsumed, bytesConsumed);
+        }
+
+        [Theory]
+        [InlineData(new byte[] { 0x31, 0x37, 0x32, 0x38 } /* "1728" */, true, 0, 1728, 4)]
+        [InlineData(new byte[] { 0xCF, 0xA0, 0x20, 0xDF, 0xB7, 0xDE, 0x96, 0xDD, 0xAE, 0x31, 0x37, 0x35, 0x31, 0x31, 0x31, 0x30 }, // stuff + "1751110"
+            true, 9, 1751110, 7)]
+        [InlineData(new byte[] { 0x39, 0x38, 0x37, 0x66, 0x61, 0x64, 0x66, 0x61, 0x6B, 0x6A, 0x6C, 0x66, 0x68, 0x6B }, // "987" + trailing text
+            true, 0, 987, 3)]
+        // "Letthem-32984eatcake"
+        [InlineData(new byte[] { 0x4C, 0x65, 0x74, 0x74, 0x68, 0x65, 0x6D, 0x2D, 0x33, 0x32, 0x39, 0x38, 0x34, 0x65, 0x61, 0x74, 0x63, 0x61, 0x6B, 0x65 },
+            true, 7, -32984, 6)]
+        [InlineData(new byte[] { 0x49, 0x20, 0x61, 0x6D, 0x20, 0x31 } /* I am 1 */, false, 0, 0, 0)]
+        public unsafe void ParseUtf8ByteStarToInt(byte[] text, bool expectSuccess, int index, int expectedValue, int expectedBytesConsumed)
+        {
+            int parsedValue;
+            int bytesConsumed;
+
+            GCHandle arrayPinned = GCHandle.Alloc(text, GCHandleType.Pinned);
+            byte* arrayPointer = (byte*)arrayPinned.AddrOfPinnedObject();
+
+            bool result = InvariantParser.TryParse(arrayPointer, index, out parsedValue, out bytesConsumed);
+
+            Assert.Equal(expectSuccess, result);
+            Assert.Equal(expectedValue, parsedValue);
+            Assert.Equal(expectedBytesConsumed, bytesConsumed);
+        }
+        #endregion
     }
 }
