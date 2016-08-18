56be058285e8928e80e3f475c62071bd7a2a51b5
Ben Adams <thundercat@illyriad.co.uk>
2016-08-18 09:37:06 +0100

    Initial interface for Dec64
    Ref: https://github.com/dotnet/corefx/issues/10951
    See: https://github.com/douglascrockford/DEC64

---
+++ src/System.Numerics.Dec64/System/Numerics/Dec64.Operators.cs
@@ -0,0 +1,344 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information. 
+
+namespace System.Numerics
+{
+    public partial struct Dec64 : IComparable, IFormattable, IComparable<Dec64>, IEquatable<Dec64>
+    {
+        public static Dec64 operator +(Dec64 left, Dec64 right)
+        {
+            return default(Dec64);
+        }
+
+        public static Dec64 operator &(Dec64 left, Dec64 right)
+        {
+            return default(Dec64);
+        }
+
+        public static Dec64 operator |(Dec64 left, Dec64 right)
+        {
+            return default(Dec64);
+        }
+
+        public static Dec64 operator --(Dec64 value)
+        {
+            return default(Dec64);
+        }
+
+        public static Dec64 operator /(Dec64 dividend, Dec64 divisor)
+        {
+            return default(Dec64);
+        }
+
+        public static bool operator ==(long left, Dec64 right)
+        {
+            return default(bool);
+        }
+
+        public static bool operator ==(Dec64 left, long right)
+        {
+            return default(bool);
+        }
+
+        public static bool operator ==(Dec64 left, Dec64 right)
+        {
+            return default(bool);
+        }
+
+        public static bool operator ==(Dec64 left, ulong right)
+        {
+            return default(bool);
+        }
+
+        public static bool operator ==(ulong left, Dec64 right)
+        {
+            return default(bool);
+        }
+
+        public static Dec64 operator ^(Dec64 left, Dec64 right)
+        {
+            return default(Dec64);
+        }
+
+        public static explicit operator Dec64(decimal value)
+        {
+            return default(Dec64);
+        }
+
+        public static explicit operator Dec64(double value)
+        {
+            return default(Dec64);
+        }
+
+        public static explicit operator byte(Dec64 value)
+        {
+            return default(byte);
+        }
+
+        public static explicit operator decimal(Dec64 value)
+        {
+            return default(decimal);
+        }
+
+        public static explicit operator double(Dec64 value)
+        {
+            return default(double);
+        }
+
+        public static explicit operator short(Dec64 value)
+        {
+            return default(short);
+        }
+
+        public static explicit operator int(Dec64 value)
+        {
+            return default(int);
+        }
+
+        public static explicit operator long(Dec64 value)
+        {
+            return default(long);
+        }
+
+        public static explicit operator sbyte(Dec64 value)
+        {
+            return default(sbyte);
+        }
+
+        public static explicit operator float(Dec64 value)
+        {
+            return default(float);
+        }
+
+        public static explicit operator ushort(Dec64 value)
+        {
+            return default(ushort);
+        }
+
+        public static explicit operator uint(Dec64 value)
+        {
+            return default(uint);
+        }
+
+        public static explicit operator ulong(Dec64 value)
+        {
+            return default(ulong);
+        }
+
+        public static explicit operator Dec64(float value)
+        {
+            return default(Dec64);
+        }
+
+        public static bool operator >(long left, Dec64 right)
+        {
+            return default(bool);
+        }
+
+        public static bool operator >(Dec64 left, long right)
+        {
+            return default(bool);
+        }
+
+        public static bool operator >(Dec64 left, Dec64 right)
+        {
+            return default(bool);
+        }
+
+        public static bool operator >(Dec64 left, ulong right)
+        {
+            return default(bool);
+        }
+
+        public static bool operator >(ulong left, Dec64 right)
+        {
+            return default(bool);
+        }
+
+        public static bool operator >=(long left, Dec64 right)
+        {
+            return default(bool);
+        }
+
+        public static bool operator >=(Dec64 left, long right)
+        {
+            return default(bool);
+        }
+
+        public static bool operator >=(Dec64 left, Dec64 right)
+        {
+            return default(bool);
+        }
+
+        public static bool operator >=(Dec64 left, ulong right)
+        {
+            return default(bool);
+        }
+
+        public static bool operator >=(ulong left, Dec64 right)
+        {
+            return default(bool);
+        }
+
+        public static implicit operator Dec64(byte value)
+        {
+            return default(Dec64);
+        }
+
+        public static implicit operator Dec64(short value)
+        {
+            return default(Dec64);
+        }
+
+        public static implicit operator Dec64(int value)
+        {
+            return default(Dec64);
+        }
+
+        public static implicit operator Dec64(long value)
+        {
+            return default(Dec64);
+        }
+
+        public static implicit operator Dec64(sbyte value)
+        {
+            return default(Dec64);
+        }
+
+        public static implicit operator Dec64(ushort value)
+        {
+            return default(Dec64);
+        }
+
+        public static implicit operator Dec64(uint value)
+        {
+            return default(Dec64);
+        }
+
+        public static implicit operator Dec64(ulong value)
+        {
+            return default(Dec64);
+        }
+
+        public static Dec64 operator ++(Dec64 value)
+        {
+            return default(Dec64);
+        }
+
+        public static bool operator !=(long left, Dec64 right)
+        {
+            return default(bool);
+        }
+
+        public static bool operator !=(Dec64 left, long right)
+        {
+            return default(bool);
+        }
+
+        public static bool operator !=(Dec64 left, Dec64 right)
+        {
+            return default(bool);
+        }
+
+        public static bool operator !=(Dec64 left, ulong right)
+        {
+            return default(bool);
+        }
+
+        public static bool operator !=(ulong left, Dec64 right)
+        {
+            return default(bool);
+        }
+
+        public static Dec64 operator <<(Dec64 value, int shift)
+        {
+            return default(Dec64);
+        }
+
+        public static bool operator <(long left, Dec64 right)
+        {
+            return default(bool);
+        }
+
+        public static bool operator <(Dec64 left, long right)
+        {
+            return default(bool);
+        }
+
+        public static bool operator <(Dec64 left, Dec64 right)
+        {
+            return default(bool);
+        }
+
+        public static bool operator <(Dec64 left, ulong right)
+        {
+            return default(bool);
+        }
+
+        public static bool operator <(ulong left, Dec64 right)
+        {
+            return default(bool);
+        }
+
+        public static bool operator <=(long left, Dec64 right)
+        {
+            return default(bool);
+        }
+
+        public static bool operator <=(Dec64 left, long right)
+        {
+            return default(bool);
+        }
+
+        public static bool operator <=(Dec64 left, Dec64 right)
+        {
+            return default(bool);
+        }
+
+        public static bool operator <=(Dec64 left, ulong right)
+        {
+            return default(bool);
+        }
+
+        public static bool operator <=(ulong left, Dec64 right)
+        {
+            return default(bool);
+        }
+
+        public static Dec64 operator %(Dec64 dividend, Dec64 divisor)
+        {
+            return default(Dec64);
+        }
+
+        public static Dec64 operator *(Dec64 left, Dec64 right)
+        {
+            return default(Dec64);
+        }
+
+        public static Dec64 operator ~(Dec64 value)
+        {
+            return default(Dec64);
+        }
+
+        public static Dec64 operator >>(Dec64 value, int shift)
+        {
+            return default(Dec64);
+        }
+
+        public static Dec64 operator -(Dec64 left, Dec64 right)
+        {
+            return default(Dec64);
+        }
+
+        public static Dec64 operator -(Dec64 value)
+        {
+            return default(Dec64);
+        }
+
+        public static Dec64 operator +(Dec64 value)
+        {
+            return default(Dec64);
+        }
+    }
+}
---
+++ src/System.Numerics.Dec64/System/Numerics/Dec64.cs
@@ -0,0 +1,69 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information. 
+
+using System.Runtime.InteropServices;
+
+namespace System.Numerics
+{
+    /// <summary>
+    /// Represents a decimal floating point value as defined by Douglas Crockford in https://github.com/douglascrockford/DEC64
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public partial struct Dec64 : IComparable, IFormattable, IComparable<Dec64>, IEquatable<Dec64>
+    {
+        public readonly static Dec64 NaN = new Dec64() {  _value = 0x80UL };
+        public readonly static Dec64 Zero = default(Dec64);
+        public readonly static Dec64 One = new Dec64() { _value = 0x100UL };
+        public readonly static Dec64 True = new Dec64() { _value = 0x380UL };
+        public readonly static Dec64 False = new Dec64() { _value = 0x280UL };
+        public readonly static Dec64 MinusOne = new Dec64() {_value = 0xFFFFFFFFFFFFFF00UL };
+
+        private ulong _value;
+
+        public Dec64(byte[] value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dec64(decimal value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dec64(double value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dec64(int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dec64(long value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dec64(float value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dec64(uint value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dec64(ulong value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int GetHashCode()
+        {
+            return default(int);
+        }
+    }
+}
