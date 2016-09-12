796d29c7807c6be596fa2e61f9ff569ea03263e8
David Fowler <davidfowl@gmail.com>
2016-09-12 09:48:37 -0700

    Moved demos to samples - Added samples to .sln - Added nuget.config and global.json to .sln - Removed seemingly duplicated or irrelevant samples

---
+++ samples/LowAllocationWebServer/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LowAllocationWebServer")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1979687d-c3d1-4d40-9017-269d7c092283")]
---
+++ samples/QotdService/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Libuv;
+using System.Text.Utf8;
+using System.Threading.Tasks;
+
+namespace QotdService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var buffer = new byte[1024];
+            var quote = new Utf8String("Insanity: doing the same thing over and over again and expecting different results. - Albert Einstein"); ;
+
+            var loop = new UVLoop();
+
+            var listener = new TcpListener("0.0.0.0", 17, loop);
+
+            listener.ConnectionAccepted += (Tcp connection) =>
+            {
+                connection.ReadCompleted += (Span<byte> data) =>
+                {
+                    quote.CopyTo(buffer);
+                    connection.TryWrite(buffer, quote.Length);
+                };
+
+                connection.ReadStart();
+            };
+
+            listener.Listen();
+            loop.Run();
+        }
+    }
+}
