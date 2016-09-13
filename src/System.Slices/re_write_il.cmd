4fb71c059c749c68c677076049c9f79a8b35b3c5
David Fowler <davidfowl@gmail.com>
2016-09-13 00:09:06 -0700

    Resign System.Slices when using ilasm
    #687

--- src/System.Slices/re_write_il.cmd
+++ src/System.Slices/re_write_il.cmd
@@ -17,4 +17,4 @@ if not defined VisualStudioVersion (
 @echo on
 tools\ildasm.exe /caverbal /linenum /out:%1\system.slices.beforerewrite.il /nobar %2
 tools\ILSub\ILSub.exe %1\system.slices.beforerewrite.il %1\system.slices.rewritten.il
-tools\ilasm.exe /quiet /pdb /dll /out:%2 /nologo %1\system.slices.rewritten.il
+tools\ilasm.exe /quiet /pdb /dll /out:%2 /nologo %1\system.slices.rewritten.il /key %1\..\..\..\..\..\..\tools\Key.snk
