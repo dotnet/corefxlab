d0ccccb05f2437d308acb0eb9038b5672b8c729b
KrzysztofCwalina <kcwalina@microsoft.com>
2016-09-14 16:16:36 -0700

    Added a type representing Span<byte> future
    The type can be stored on the heap.
    It's subject to tearing, and shoul dnot be used in multithreaded context, or passed to user code.
    
    The type is used as a buffer or abstract buffer pool (either managed or native)

--- src/System.Buffers.Experimental/System/Buffers/ManagedBufferPool.cs
+++ src/System.Buffers.Experimental/System/Buffers/ManagedBufferPool.cs
@@ -9,82 +9,41 @@ using System.Threading;
 namespace System.Buffers
 {
     [Obsolete("Use ArrayPool<T> instead. It can be found in CoreFx System.Buffers package.")]
-    public sealed class ManagedBufferPool<T> where T : struct
+    public sealed class ManagedBufferPool : BufferPool
     {
-        private const int NUMBER_OF_BUFFERS_IN_BUCKET = 50;
-        private int _maxBufferSize;
-        private ManagedBufferBucket<T>[] _buckets;
+        static ManagedBufferPool s_shared = new ManagedBufferPool();
 
-        private static ManagedBufferPool<byte> _sharedInstance = null;
-
-        public static ManagedBufferPool<byte> SharedByteBufferPool
+        public static ManagedBufferPool Shared
         {
             get
             {
-                if (Volatile.Read(ref _sharedInstance) == null)
-                {
-                    Interlocked.CompareExchange(ref _sharedInstance, new ManagedBufferPool<byte>(), null);
-                }
-
-                return _sharedInstance;
+                return s_shared;
             }
         }
 
-        // 2MB taken as the default since the average HTTP page is 1.9MB
-        // per http://httparchive.org/interesting.php, as of October 2015
-        public ManagedBufferPool(int maxBufferSizeInBytes = 2048000)
+        public override Bytes Rent(int minimumBufferSize)
         {
-            int maxBuckets = BufferUtilities.SelectBucketIndex(maxBufferSizeInBytes);
-
-            _maxBufferSize = maxBufferSizeInBytes;
-            _buckets = new ManagedBufferBucket<T>[maxBuckets + 1];
+            var array = ArrayPool<byte>.Shared.Rent(minimumBufferSize);
+            return new Bytes(new ArraySegment<byte>(array, 0, array.Length));
         }
 
-        public T[] RentBuffer(int size, bool clearBuffer = false)
+        public override void Return(Bytes buffer)
         {
-            T[] buffer;
-            if (size > _maxBufferSize)
+            var span = (Span<byte>)buffer;
+            ArraySegment<byte> segment;
+            unsafe
             {
-                buffer = new T[size];
-            }
-            else
-            {
-                int index = BufferUtilities.SelectBucketIndex(size);
-                ManagedBufferBucket<T> bucket = Volatile.Read(ref _buckets[index]);
-                if (bucket == null)
-                {
-                    Interlocked.CompareExchange(ref _buckets[index], new ManagedBufferBucket<T>(BufferUtilities.GetMaxSizeForBucket(index), NUMBER_OF_BUFFERS_IN_BUCKET), null);
-                    bucket = _buckets[index];
+                void* p;          
+                if(!span.TryGetArrayElseGetPointer(out segment, out p)) {
+                    throw new Exception("this buffer was not rented from this pool.");
                 }
-                buffer = bucket.Rent();
-                if (clearBuffer) Array.Clear(buffer, 0, buffer.Length);
             }
-
-            return buffer;
+            ArrayPool<byte>.Shared.Return(segment.Array);
         }
 
-        public void EnlargeBuffer(ref T[] buffer, int newSize, bool clearFreeSpace = false)
+        protected override void Dispose(bool disposing)
         {
-            if (newSize <= buffer.Length)
-                throw new InvalidOperationException("Cannot make buffer smaller or the same size");
-
-            T[] newBuffer = RentBuffer(newSize, false); // Don't clear the whole buffer since we can optimize below
-            Array.Copy(buffer, newBuffer, buffer.Length);
-
-            if (clearFreeSpace) Array.Clear(newBuffer, buffer.Length, newBuffer.Length - buffer.Length);
-
-            ReturnBuffer(ref buffer, true);
-            buffer = newBuffer;
-        }
-
-        public void ReturnBuffer(ref T[] buffer, bool clearBuffer = false)
-        {
-            if (clearBuffer) Array.Clear(buffer, 0, buffer.Length);
-
-            if (buffer.Length > _maxBufferSize)
-                buffer = null;
-            else
-                _buckets[BufferUtilities.SelectBucketIndex(buffer.Length)].Return(ref buffer);
+            throw new NotImplementedException();
         }
     }
 }
