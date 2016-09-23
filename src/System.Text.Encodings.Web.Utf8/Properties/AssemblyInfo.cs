db532ee4431a6795c28b89b924998e3865516d8c
David Fowler <davidfowl@gmail.com>
2016-09-23 16:58:34 -0700

    Made a stub project to port code from ASP.NET

---
+++ src/System.Text.Encodings.Web.Utf8/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("System.Text.Encodings.Web.Utf8")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("20974d06-ebdc-44b8-a034-de1440cf7a45")]
