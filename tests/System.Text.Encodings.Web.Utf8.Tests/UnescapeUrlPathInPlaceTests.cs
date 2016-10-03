54e99ac866a80efcdbad811d99b3e1c1a70d44a7
Troy Dai <troy.dai@outlook.com>
2016-10-03 16:10:38 -0700

    Add UrlEncoder for decode Url Path
    The UrlEncoder provides two methods to decode Url path. One decode the
    raw path in place and the other save output to the given byte span. The
    decoder will unescapse the path string.

---
+++ tests/System.Text.Encodings.Web.Utf8.Tests/UnescapeUrlPathInPlaceTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace System.Text.Encodings.Web.Utf8.Tests
+{
+    /// <summary>
+    /// See <typeparamref name="UrlEncoderTests"/> for test scenarios
+    /// </summary>
+    public class UnescapeUrlPathInPlaceTests : UrlEncoderTests
+    {
+        protected override void TestCore(string raw, string expected)
+        {
+            var input = GetBytes(raw);
+
+            var len = UrlEncoder.DecodeInPlace(input);
+            Assert.True(len <= input.Length);
+
+            var outputDecoded = Encoding.UTF8.GetString(input.Slice(0, len).ToArray());
+            Assert.Equal(expected, outputDecoded);
+        }
+    }
+}
---
+++ tests/System.Text.Encodings.Web.Utf8.Tests/UnescapeUrlPathTests.cs
@@ -0,0 +1,37 @@
+﻿using Xunit;
+
+namespace System.Text.Encodings.Web.Utf8.Tests
+{
+    /// <summary>
+    /// See <typeparamref name="UrlEncoderTests"/> for test scenarios
+    /// </summary>
+    public class UnescapeUrlPathTests : UrlEncoderTests
+    {
+        protected override void TestCore(string raw, string expected)
+        {
+            var input = GetBytes(raw);
+            var destination = new Span<byte>(new byte[input.Length]);
+
+            var len = UrlEncoder.Decode(input, destination);
+            Assert.True(len <= input.Length);
+
+            var unescaped = destination.Slice(0, len);
+            Assert.False(unescaped.ReferenceEquals(input.Slice(0, len)));
+
+            var outputDecoded = Encoding.UTF8.GetString(unescaped.ToArray());
+            Assert.Equal(expected, outputDecoded);
+        }
+
+        [Fact]
+        public void ThrowWhenDesinationIsSmaller()
+        {
+            var input = GetBytes("/test%20test");
+            var destination = new Span<byte>(new byte[input.Length - 1]);
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                UrlEncoder.Decode(input, destination);
+            });
+        }
+    }
+}
