c1878e82a9c07f9fc130265d44cad8341a087b3b
Krzysztof Cwalina <kcwalina@microsoft.com>
2016-11-04 14:48:16 -0700

    Added TryParseUInt32 for parsing sequences of buffers

--- src/System.Buffers.Experimental/System/Buffers/ArraySegmentExtensions.cs
+++ src/System.Buffers.Experimental/System/Buffers/ArraySegmentExtensions.cs
@@ -5,12 +5,23 @@ using System.Collections.Sequences;
 
 namespace System.Buffers
 {
-    static class Extensions
+    public static class VariousExtensions
     {
         public static ArraySegment<T> Slice<T>(this ArraySegment<T> source, int count)
         {
             var result = new ArraySegment<T>(source.Array, source.Offset + count, source.Count - count);
             return result;
+        }
+
+        public static ReadOnlySpan<byte> ToSingleSpan<T>(this T memorySequence) where T : ISequence<ReadOnlyMemory<byte>>
+        {
+            Position position = Position.First;
+            ReadOnlyMemory<byte> memory;
+            ResizableArray<byte> array = new ResizableArray<byte>(1024); // TODO: could this be rented from a pool?
+            while (memorySequence.TryGet(ref position, out memory, advance: true)) {
+                array.AddAll(memory.Span);
+            }
+            return array.Items.Slice(0, array.Count);
         }
     }
 }
