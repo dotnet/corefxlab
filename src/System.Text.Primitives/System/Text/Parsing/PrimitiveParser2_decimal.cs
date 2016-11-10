510e088f4e2c0040aba4fcd52e72679aaacbd2ac
John Bottenberg <jobotten@microsoft.com>
2016-11-10 13:31:19 -0800

    Add PrimitiveParser.TryParseDecimal overloads (#971)
    * Introduce class PrimitiveParser2, and create skeleton implementations of its overloads of TryParseBool and TryParseUInt32.
    
    * Address feedback from PR; remove PrimitiveParser2 type. PrimitiveParser2 still exists in filenames for the purpose of separating the old from the new. Add tests for new overloads.
    
    * In PrimitiveParser.InvariantUtf8/Utf16, renamed out parameter consumed to either bytesConsumed or charactersConsumed for clarity.
    
    * Add PrimitiveParser.TryParseDecimal overloads and a few unit tests

---
+++ src/System.Text.Primitives/System/Text/Parsing/PrimitiveParser2_decimal.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Diagnostics;
+using System.Text.Utf8;
+
+namespace System.Text
+{
+    public static partial class PrimitiveParser
+    {
+        public static bool TryParseDecimal(ReadOnlySpan<byte> text, out decimal value, out int bytesConsumed, EncodingData encoding = default(EncodingData))
+        {
+            bytesConsumed = 0;
+            value = default(decimal);
+
+            if (encoding.IsInvariantUtf8)
+            {
+                return InvariantUtf8.TryParseDecimal(text, out value, out bytesConsumed);
+            }
+            else if (encoding.IsInvariantUtf16)
+            {
+                ReadOnlySpan<char> textChars = text.Cast<byte, char>();
+                int charactersConsumed;
+                bool result = InvariantUtf16.TryParseDecimal(textChars, out value, out charactersConsumed);
+                bytesConsumed = charactersConsumed * sizeof(char);
+                return result;
+            }
+
+            return false;
+        }
+    }
+}
