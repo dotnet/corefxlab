9e86050bea8661621c2da31189ea721da8c80b1d
Krzysztof Cwalina <kcwalina@microsoft.com>
2016-11-10 09:16:56 -0800

    Added OutputFormatter (#967)

---
+++ src/System.Text.Formatting/System/Text/Formatting/OutputFormatter.cs
@@ -0,0 +1,28 @@
+﻿using System.Buffers;
+
+namespace System.Text.Formatting
+{
+    public struct OutputFormatter<TOutput> : ITextOutput where TOutput : IOutput
+    {
+        TOutput _output;
+        EncodingData _encoding;
+
+        public OutputFormatter(TOutput output, EncodingData encoding)
+        {
+            _output = output;
+            _encoding = encoding;
+        }
+
+        public OutputFormatter(TOutput output) : this(output, EncodingData.InvariantUtf8)
+        {
+        }
+
+        public Span<byte> Buffer => _output.Buffer;
+
+        public EncodingData Encoding => _encoding;
+
+        public void Advance(int bytes) => _output.Advance(bytes);
+
+        public void Enlarge(int desiredBufferLength = 0) => _output.Enlarge(desiredBufferLength);
+    }
+}
