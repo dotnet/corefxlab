2a69d38d1d341456d5925585ca2f6afdcdd618c5
Krzysztof Cwalina <kcwalina@microsoft.com>
2017-02-02 15:57:05 -0800

    Optimized Memory<T> Access

--- tests/System.Buffers.Experimental.Tests/BytesReader.cs
+++ tests/System.Buffers.Experimental.Tests/BytesReader.cs
@@ -10,7 +10,7 @@ namespace System.Slices.Tests
 {
     public partial class ReadOnlyBytesTests
     {
-        [Fact(Skip = "[VS2017] Error: The active Test Run was aborted.")]
+        [Fact]
         public void SingleSegmentBytesReader()
         {
             ReadOnlyBytes bytes = Create("AB CD#EF&&");
