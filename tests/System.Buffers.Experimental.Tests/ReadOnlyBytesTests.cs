abbb2e11386ecce38bbb3bb144096b61dd7a7741
Krzysztof Cwalina <kcwalina@microsoft.com>
2017-02-07 13:07:24 -0800

    narrowing more

--- tests/System.Buffers.Experimental.Tests/ReadOnlyBytesTests.cs
+++ tests/System.Buffers.Experimental.Tests/ReadOnlyBytesTests.cs
@@ -19,7 +19,7 @@ namespace System.Slices.Tests
             Assert.Equal(2, bytes.First.Span[0]);
         }
 
-        [Fact(Skip = "flaky")]
+        [Fact]
         public void ReadOnlyBytesIndexOf()
         {
             var bytes = ReadOnlyBytes.Create(new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }, new byte[] { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 });
