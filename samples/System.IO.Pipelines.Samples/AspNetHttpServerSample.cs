29d7aa090242c8e9db327c34812e5f370e67761c
Krzysztof Cwalina <kcwalina@microsoft.com>
2017-02-08 14:41:08 -0800

    Added copyright notices (#1199)

--- samples/System.IO.Pipelines.Samples/AspNetHttpServerSample.cs
+++ samples/System.IO.Pipelines.Samples/AspNetHttpServerSample.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Text;
 using System.IO.Pipelines.Samples.Http;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
--- src/Microsoft.CodeAnalysis.StackOnlyTypes/Microsoft.CodeAnalysis.StackOnlyTypes.Test/Verifiers/CodeFixVerifier.cs
+++ src/Microsoft.CodeAnalysis.StackOnlyTypes/Microsoft.CodeAnalysis.StackOnlyTypes.Test/Verifiers/CodeFixVerifier.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.CodeAnalysis;
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.Diagnostics;
