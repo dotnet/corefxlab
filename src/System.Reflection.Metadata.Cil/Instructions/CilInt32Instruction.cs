5317d42121525ee2761b7eaeef0f54c747e3d28b
Krzysztof Cwalina <kcwalina@microsoft.com>
2017-02-08 14:08:33 -0800

    Added copyright notices

--- src/System.Reflection.Metadata.Cil/Instructions/CilInt32Instruction.cs
+++ src/System.Reflection.Metadata.Cil/Instructions/CilInt32Instruction.cs
@@ -1,4 +1,8 @@
-﻿using System.Reflection.Metadata.Cil.Visitor;
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Reflection.Metadata.Cil.Visitor;
 using System.Reflection.Emit;
 
 namespace System.Reflection.Metadata.Cil.Instructions
--- tests/System.IO.Pipelines.Tests/SocketsFacts.cs
+++ tests/System.IO.Pipelines.Tests/SocketsFacts.cs
@@ -1,4 +1,8 @@
-﻿using System.IO.Pipelines.Networking.Libuv;
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.IO.Pipelines.Networking.Libuv;
 using System.IO.Pipelines.Networking.Sockets;
 using System.IO.Pipelines.Text.Primitives;
 using System;
