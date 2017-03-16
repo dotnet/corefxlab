5c8b81221cbb58bbd0ec3b8f2b9880ea27312930
Robert Anderson <robert.anderson@microsoft.com>
2017-03-16 09:39:10 -0700

    Removing experimental dependency on pipelines.

---
+++ src/System.Buffers.Primitives/System/Buffers/OwnedNativeMemory.cs
@@ -0,0 +1,40 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace System.Buffers
+{
+    public class OwnedNativeMemory : OwnedMemory<byte>
+    {
+        public OwnedNativeMemory(int length) : this(length, Marshal.AllocHGlobal(length))
+        { }
+
+        public OwnedNativeMemory(int length, IntPtr address) : base(null, 0, length, address) { }
+
+        public static implicit operator IntPtr(OwnedNativeMemory owner)
+        {
+            unsafe
+            {
+                return new IntPtr(owner.Pointer);
+            }
+        }
+
+        ~OwnedNativeMemory()
+        {
+            Dispose(false);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (base.Pointer != IntPtr.Zero) {
+                Marshal.FreeHGlobal(base.Pointer);
+            }
+            base.Dispose(disposing);
+        }
+
+        public new unsafe byte* Pointer => (byte*)base.Pointer.ToPointer();
+    }
+}
+
--- src/System.Buffers.Primitives/System/Buffers/OwnedPinnedArray.cs
+++ src/System.Buffers.Primitives/System/Buffers/OwnedPinnedArray.cs
@@ -6,37 +6,6 @@ using System.Runtime.InteropServices;
 
 namespace System.Buffers
 {
-    public class OwnedNativeMemory : OwnedMemory<byte>
-    {
-        public OwnedNativeMemory(int length) : this(length, Marshal.AllocHGlobal(length))
-        { }
-
-        public OwnedNativeMemory(int length, IntPtr address) : base(null, 0, length, address) { }
-
-        public static implicit operator IntPtr(OwnedNativeMemory owner)
-        {
-            unsafe
-            {
-                return new IntPtr(owner.Pointer);
-            }
-        }
-
-        ~OwnedNativeMemory()
-        {
-            Dispose(false);
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (base.Pointer != IntPtr.Zero) {
-                Marshal.FreeHGlobal(base.Pointer);
-            }
-            base.Dispose(disposing);
-        }
-
-        public new unsafe byte* Pointer => (byte*)base.Pointer.ToPointer();
-    }
-
     // This is to support secnarios today covered by Memory<T> in corefxlab
     public class OwnedPinnedArray<T> : OwnedMemory<T>
     {
