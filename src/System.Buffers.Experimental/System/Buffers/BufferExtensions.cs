0bd79f6d5a62e1c505e2765d429111297332149a
ahsonkhan <ahson_ahmedk@yahoo.com>
2017-03-22 16:24:17 -0700

    Adding extension method Slice on string.

--- src/System.Buffers.Experimental/System/Buffers/BufferExtensions.cs
+++ src/System.Buffers.Experimental/System/Buffers/BufferExtensions.cs
@@ -23,6 +23,84 @@ namespace System.Buffers
             return array.Items.Slice(0, array.Count);
         }
 
+        /// <summary>
+        /// Creates a new readonly span over the portion of the target string.
+        /// </summary>
+        /// <param name="text">The target string.</param>
+        /// <exception cref="System.ArgumentNullException">Thrown when <paramref name="text"/> is null.</exception>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe ReadOnlySpan<char> Slice(this string text)
+        {
+            if (text == null)
+                throw new ArgumentNullException(nameof(text));
+
+            int textLength = text.Length;
+
+            if (textLength == 0) return ReadOnlySpan<char>.Empty;
+
+            fixed (char* charPointer = text)
+            {
+                return ReadOnlySpan<char>.DangerousCreate(text, ref *charPointer, textLength);
+            }
+        }
+
+        /// <summary>
+        /// Creates a new readonly span over the portion of the target string, beginning at 'start'.
+        /// </summary>
+        /// <param name="text">The target string.</param>
+        /// <param name="start">The index at which to begin this slice.</param>
+        /// <exception cref="System.ArgumentNullException">Thrown when <paramref name="text"/> is null.</exception>
+        /// <exception cref="System.ArgumentOutOfRangeException">
+        /// Thrown when the specified <paramref name="start"/> index is not in range (&lt;0 or &gt;Length).
+        /// </exception>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe ReadOnlySpan<char> Slice(this string text, int start)
+        {
+            if (text == null)
+                throw new ArgumentNullException(nameof(text));
+
+            int textLength = text.Length;
+
+            if ((uint) start > (uint) textLength)
+                throw new ArgumentOutOfRangeException(nameof(start));
+
+            if (textLength == 0) return ReadOnlySpan<char>.Empty;
+
+            fixed (char* charPointer = text)
+            {
+                return ReadOnlySpan<char>.DangerousCreate(text, ref *(charPointer + start), textLength - start);
+            }
+        }
+
+        /// <summary>
+        /// Creates a new readonly span over the portion of the target string, beginning at <paramref name="start"/>, of given <paramref name="length"/>.
+        /// </summary>
+        /// <param name="text">The target string.</param>
+        /// <param name="start">The index at which to begin this slice.</param>
+        /// <param name="length">The number of items in the span.</param>
+        /// <exception cref="System.ArgumentNullException">Thrown when <paramref name="text"/> is null.</exception>
+        /// <exception cref="System.ArgumentOutOfRangeException">
+        /// Thrown when the specified <paramref name="start"/> or end index is not in range (&lt;0 or &gt;=Length).
+        /// </exception>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe ReadOnlySpan<char> Slice(this string text, int start, int length)
+        {
+            if (text == null)
+                throw new ArgumentNullException(nameof(text));
+
+            int textLength = text.Length;
+
+            if ((uint)start > (uint)textLength || (uint)length > (uint)(textLength - start))
+                throw new ArgumentOutOfRangeException(nameof(start));
+
+            if (length == 0) return ReadOnlySpan<char>.Empty;
+
+            fixed (char* charPointer = text)
+            {
+                return ReadOnlySpan<char>.DangerousCreate(text, ref *(charPointer + start), length);
+            }
+        }
+
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static int IndexOf(this IReadOnlyBufferList<byte> sequence, ReadOnlySpan<byte> value)
         {
