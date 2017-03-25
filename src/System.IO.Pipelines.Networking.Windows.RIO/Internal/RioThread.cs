7367d8cfad6b41b0c65a62098dc53777d7482ee3
Ashley Mannix <ashleymannix@live.com.au>
2017-03-25 19:36:20 +1000

    sort usings

--- src/System.IO.Pipelines.Networking.Windows.RIO/Internal/RioThread.cs
+++ src/System.IO.Pipelines.Networking.Windows.RIO/Internal/RioThread.cs
@@ -1,13 +1,12 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
+using System.Buffers;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO.Pipelines.Networking.Windows.RIO.Internal.Winsock;
 using System.Runtime.InteropServices;
 using System.Threading;
-using System.IO.Pipelines.Networking.Windows.RIO.Internal.Winsock;
-using System.Buffers;
 
 namespace System.IO.Pipelines.Networking.Windows.RIO.Internal
 {
--- src/System.IO.Pipelines.Networking.Windows.RIO/RioTcpConnection.cs
+++ src/System.IO.Pipelines.Networking.Windows.RIO/RioTcpConnection.cs
@@ -1,12 +1,11 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
 using System.Buffers;
-using System.Threading;
-using System.Threading.Tasks;
 using System.IO.Pipelines.Networking.Windows.RIO.Internal;
 using System.IO.Pipelines.Networking.Windows.RIO.Internal.Winsock;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace System.IO.Pipelines.Networking.Windows.RIO
 {
