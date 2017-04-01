c711d6f3e1235a4a8da17b12de71fc74afbc090d
ahsonkhan <ahson_ahmedk@yahoo.com>
2017-04-01 15:58:26 -0700

    Building the solution file instead of parsing it.

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -6,11 +6,14 @@
     [string]$BuildVersion=[System.DateTime]::Now.ToString('eyyMMdd-1')
 )
 
+$file = "corefxlab.sln"
+$root = "$PSScriptRoot\.."
+
 Write-Host "Commencing full build for Configuration=$Configuration."
 
 if (!(Test-Path "dotnet\dotnet.exe")) {
     Write-Host "dotnet.exe not installed, downloading and installing."
-    Invoke-Expression -Command "$PSScriptRoot\install-dotnet.ps1 -Channel $Channel -Version $Version -InstallDir $PSScriptRoot\..\dotnet"
+    Invoke-Expression -Command "$PSScriptRoot\install-dotnet.ps1 -Channel $Channel -Version $Version -InstallDir $root\..\dotnet"
     if ($lastexitcode -ne $null -and $lastexitcode -ne 0) {
         Write-Error "Failed to install dotnet.exe, exit code [$lastexitcode], aborting build."
         exit -1
@@ -19,11 +22,11 @@ if (!(Test-Path "dotnet\dotnet.exe")) {
 
 $env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE = 1
 
-$dotnetExePath="$PSScriptRoot\..\dotnet\dotnet.exe"
+$dotnetExePath="$root\dotnet\dotnet.exe"
 
 if ($Restore -eq "true") {
     Write-Host "Restoring all packages"
-    Invoke-Expression "$dotnetExePath restore corefxlab.sln /p:VersionSuffix=$BuildVersion"
+    Invoke-Expression "$dotnetExePath restore $file /p:VersionSuffix=$BuildVersion"
     if ($lastexitcode -ne 0) {
         Write-Error "Failed to restore packages."
         exit -1
@@ -31,60 +34,27 @@ if ($Restore -eq "true") {
 }
 
 $errorsEncountered = 0
-$projectsFailed = New-Object System.Collections.Generic.List[String]
 
-$sources = @("src\", "samples\")
-$tests = @("tests\")
-$root = "$PSScriptRoot\..\"
+Write-Host "Building solution $file..."
+Invoke-Expression "$dotnetExePath build $file -c $Configuration /p:VersionSuffix=$BuildVersion"
+if ($lastexitcode -ne 0) {
+    Write-Error "Failed to build solution $file"
+    $errorsEncountered++
+}
 
-$reader = [System.IO.File]::OpenText("corefxlab.sln")
-while($null -ne ($line = $reader.ReadLine())) {
-    
-    $pos = $line.IndexOf('.csproj')
-    
-    if ($pos -ne -1)
-    {
-        $projectLinesSplitUp = $line.Split(',',[System.StringSplitOptions]::RemoveEmptyEntries)
+Write-Host "Building and running tests for solution $file..."
+Invoke-Expression "$dotnetExePath test $file -c $Configuration -- -notrait category=performance -notrait category=outerloop"
 
-        $projectLinesSplitUp | Select-String -Pattern $sources -SimpleMatch | ForEach {
-                $fileName = $_ -replace ' "', "" -replace '"', ""
-                $file = -join ($root, $fileName)
-
-                Write-Host "Building $file..."
-                Invoke-Expression "$dotnetExePath build $file -c $Configuration /p:VersionSuffix=$BuildVersion"
-
-                if ($lastexitcode -ne 0) {
-                    Write-Error "Failed to build project $file"
-                    $projectsFailed.Add($file)
-                    $errorsEncountered++
-                }
-        }
-
-        $projectLinesSplitUp | Select-String -Pattern $tests -SimpleMatch | ForEach {
-                $fileName = $_ -replace ' "', "" -replace '"', ""
-                $file = -join ($root, $fileName)
-
-                Write-Host "Building and running tests for project $file..."
-                Invoke-Expression "$dotnetExePath test $file -c $Configuration -- -notrait category=performance -notrait category=outerloop"
-
-                if ($lastexitcode -ne 0) {
-                    Write-Error "Some tests failed in project $file"
-                    $projectsFailed.Add($file)
-                    $errorsEncountered++
-                }
-        }
-    }
-
+if ($lastexitcode -ne 0) {
+    Write-Error "Some tests failed in solution $file"
+    $errorsEncountered++
 }
 
 if ($errorsEncountered -eq 0) {
     Write-Host "** Build succeeded. **" -foreground "green"
 }
 else {
-    Write-Host "** Build failed. $errorsEncountered projects failed to build or test. **" -foreground "red"
-    foreach ($file in $projectsFailed) {
-        Write-Host "    $file" -foreground "red"
-    }
+    Write-Host "** Build failed. **" -foreground "red"
 }
 
 exit $errorsEncountered
