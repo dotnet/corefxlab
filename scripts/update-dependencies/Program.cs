f5e72b8d987c3de322d5ddd4c9a698ca4b0a9a78
Robert Anderson <shiftylogic@users.noreply.github.com>
2017-04-04 13:50:07 -0700

    New dependency update tool; updating to new CLI / CoreFx assemblies (#1422)
    * New dependency update tool; updating to new CLI / CoreFx assemblies
    
    * PR feedback

---
+++ scripts/update-dependencies/Program.cs
@@ -0,0 +1,96 @@
+// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.DotNet.VersionTools;
+using Microsoft.DotNet.VersionTools.Automation;
+using Microsoft.DotNet.VersionTools.Dependencies;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Microsoft.Dotnet.Scripts
+{
+    public static class Program
+    {
+        private static readonly Config config = Config.Instance;
+
+
+        public static void Main(string[] args)
+        {
+            Trace.Listeners.Add(new TextWriterTraceListener(Console.Out));
+
+            bool onlyUpdate = args.Length > 0 && string.Equals("--update", args[0], StringComparison.OrdinalIgnoreCase);
+
+            IEnumerable<IDependencyUpdater> updaters = GetUpdaters();
+            IEnumerable<DependencyBuildInfo> buildInfoDependencies = GetBuildInfoDependencies();
+
+            DependencyUpdateResults updateResults = DependencyUpdateUtils.Update(updaters, buildInfoDependencies);
+
+            if (!onlyUpdate && updateResults.ChangesDetected())
+            {
+                CreatePullRequest(updateResults);
+            }
+        }
+
+        private static void CreatePullRequest(DependencyUpdateResults updateResults)
+        {
+            GitHubAuth gitHubAuth = new GitHubAuth(config.Password, config.UserName, config.Email);
+            GitHubProject origin = new GitHubProject(config.GitHubProject, config.UserName);
+            GitHubBranch upstreamBranch = new GitHubBranch(config.GitHubUpstreamBranch, new GitHubProject(config.GitHubProject, config.GitHubUpstreamOwner));
+
+            string commitMessage = updateResults.GetSuggestedCommitMessage();
+            string body = string.Empty;
+            if (config.GitHubPullRequestNotifications.Any())
+            {
+                body += PullRequestCreator.NotificationString(config.GitHubPullRequestNotifications);
+            }
+
+            new PullRequestCreator(gitHubAuth, origin, upstreamBranch)
+                .CreateOrUpdateAsync(commitMessage, commitMessage + $" ({upstreamBranch.Name})", body)
+                .Wait();
+        }
+
+        private static IEnumerable<DependencyBuildInfo> GetBuildInfoDependencies()
+        {
+            yield return GetBuildInfoDependency("Cli", "cli/master");
+            yield return GetBuildInfoDependency("CoreFx", "corefx/master");
+        }
+
+        private static DependencyBuildInfo GetBuildInfoDependency(string target, string fragment)
+        {
+            BuildInfo buildInfo = BuildInfo.Get(target, $"{config.VersionsUrl}/{fragment}", false);
+            return new DependencyBuildInfo(buildInfo, true, Enumerable.Empty<string>());
+        }
+
+        private static IEnumerable<IDependencyUpdater> GetUpdaters()
+        {
+            yield return CreateRegexPropertyUpdater(config.DependencyFilePath, "SystemCompilerServicesUnsafeVersion", "System.Runtime.CompilerServices.Unsafe");
+            yield return CreateRegexPropertyUpdater(config.DependencyFilePath, "SystemMemoryVersion", "System.Memory");
+            yield return CreateFileUpdater(config.CLIVersionFilePath, "Microsoft.DotNet.Cli.Utils");
+        }
+
+        private static IDependencyUpdater CreateFileUpdater(string path, string packageId)
+        {
+            return new DotNet.VersionTools.Dependencies.FilePackageUpdater()
+            {
+                Path = path,
+                PackageId = packageId
+            };
+        }
+
+        private static IDependencyUpdater CreateRegexPropertyUpdater(string path, string propertyName, string packageId)
+        {
+            return new FileRegexPackageUpdater()
+            {
+                Path = Path.Combine(Directory.GetCurrentDirectory(), path),
+                PackageId = packageId,
+                Regex = new Regex($@"<{propertyName}>(?<version>.*)</{propertyName}>"),
+                VersionGroupName = "version"
+            };
+        }
+    }
+}
