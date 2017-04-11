25c27c3642e9065664c85f92ae09ed1e4665f270
Robert Anderson <robert.anderson@microsoft.com>
2017-04-11 16:21:06 -0700

    Removing an unnecessary helper method.

--- src/System.Text.Primitives/System/Text/Formatting/Internal/FormattingHelpers.cs
+++ src/System.Text.Primitives/System/Text/Formatting/Internal/FormattingHelpers.cs
@@ -27,7 +27,7 @@ namespace System.Text
         public static int WriteFractionDigits(long value, int digitCount, ref byte buffer, int index)
         {
             for (var i = FractionDigits; i > digitCount; i--)
-                value = DivMod10(value, out long m);
+                value = DivMod(value, 10, out long m);
 
             return WriteDigits(value, digitCount, ref buffer, index);
         }
@@ -45,7 +45,7 @@ namespace System.Text
 
             for (var i = digitCount - 1; i >= 0; i--)
             {
-                left = DivMod10(left, out long num);
+                left = DivMod(left, 10, out long num);
                 Unsafe.Add(ref buffer, index + i) = (byte)('0' + num);
             }
 
@@ -56,7 +56,6 @@ namespace System.Text
 
         #region UTF-16 Helper methods
 
-        // This method assumes the buffer passed starting at index has space for at least 2 more chars.
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static unsafe void WriteHexByte(byte value, ref char buffer, int index)
         {
@@ -68,7 +67,7 @@ namespace System.Text
         public static int WriteFractionDigits(long value, int digitCount, ref char buffer, int index)
         {
             for (var i = FractionDigits; i > digitCount; i--)
-                value = DivMod10(value, out long m);
+                value = DivMod(value, 10, out long m);
 
             return WriteDigits(value, digitCount, ref buffer, index);
         }
@@ -86,7 +85,7 @@ namespace System.Text
 
             for (var i = digitCount - 1; i >= 0; i--)
             {
-                left = DivMod10(left, out long num);
+                left = DivMod(left, 10, out long num);
                 Unsafe.Add(ref buffer, index + i) = (char)('0' + num);
             }
 
@@ -95,23 +94,7 @@ namespace System.Text
 
         #endregion UTF-16 Helper methods
 
-        #region Fast Math Helper methods
-
-        /// <summary>
-        /// This method for doing division and modulus for integers by 10 is ~5-6 times faster
-        /// than the division and multiplication operators.
-        /// </summary>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static long DivMod10(long n, out long modulo)
-        {
-            // This commented implementation is faster, but only works for positive integer values 
-            // between 0 and 1073741828.
-            //      long d = ((0x1999999A * n) >> 32);
-            //      modulo = (n - (d * 10));
-            //      return d;
-
-            return DivMod(n, 10, out modulo);
-        }
+        #region Math Helper methods
 
         /// <summary>
         /// We don't have access to Math.DivRem, so this is a copy of the implementation.
@@ -124,7 +107,7 @@ namespace System.Text
             return div;
         }
 
-        #endregion Fast Math Helper methods
+        #endregion Math Helper methods
 
         #region Character counting helper methods
 
@@ -149,7 +132,7 @@ namespace System.Text
             // Remove all the 0 (zero) values from the right.
             while (left > 0 && m == 0 && count > 0)
             {
-                left = DivMod10(left, out m);
+                left = DivMod(left, 10, out m);
                 count--;
             }
 
