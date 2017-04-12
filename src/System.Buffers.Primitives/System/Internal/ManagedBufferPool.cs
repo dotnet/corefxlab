1c83548c8741a6c03779fdf03508eecb0d22f754
ahsonkhan <ahson_ahmedk@yahoo.com>
2017-04-12 13:38:47 -0700

    Removing duplicate code left from split

--- src/System.Buffers.Primitives/System/Internal/ManagedBufferPool.cs
+++ src/System.Buffers.Primitives/System/Internal/ManagedBufferPool.cs
@@ -3,7 +3,7 @@
 
 namespace System.Buffers.Internal
 {
-    internal sealed class ManagedBufferPool : BufferPool
+    internal sealed partial class ManagedBufferPool : BufferPool
     {
         readonly static ManagedBufferPool s_shared = new ManagedBufferPool();
 
@@ -12,29 +12,6 @@ namespace System.Buffers.Internal
             get
             {
                 return s_shared;
-            }
-        }
-
-        public override OwnedBuffer<byte> Rent(int minimumBufferSize)
-        {
-            return new ArrayPoolMemory(minimumBufferSize);
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-        }
-
-        private class ArrayPoolMemory : OwnedBuffer<byte>
-        {
-            public ArrayPoolMemory(int size) : base(ArrayPool<byte>.Shared.Rent(size))
-            {
-            }
-
-            protected override void Dispose(bool disposing)
-            {
-                ArrayPool<byte>.Shared.Return(Array);
-
-                base.Dispose(disposing);
             }
         }
     }
