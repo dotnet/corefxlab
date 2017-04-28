8f4661cf20640e732be6271e4208e28b01b046ee
ahsonkhan <ahson_ahmedk@yahoo.com>
2017-04-27 20:48:40 -0700

    Some more cleanup and fixing namespace of Threading.Tasks.Channels.Tests.

--- tests/System.Threading.Tasks.Channels.Tests/AutoResetAwaiterTests.cs
+++ tests/System.Threading.Tasks.Channels.Tests/AutoResetAwaiterTests.cs
@@ -2,11 +2,10 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Threading;
-using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
 using Xunit;
 
-namespace System.Runtime.CompilerServices.Tests
+namespace System.Threading.Tasks.Channels.Tests
 {
     public class AutoResetAwaiterTests
     {
