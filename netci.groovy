a4d0226dd867e09075e03b8dc93ae87706035238
ahsonkhan <ahson_ahmedk@yahoo.com>
2017-05-08 20:41:29 -0700

    Adding Ubuntu and OSX CI runs.

--- netci.groovy
+++ netci.groovy
@@ -5,40 +5,49 @@ import jobs.generation.JobReport;
 def project = GithubProject
 def branch = GithubBranchName
 
+// Map of osName -> osGroup.
+def osGroupMap = ['Ubuntu16.04':'Linux',
+                  'OSX10.12':'OSX',
+                  'Windows_NT':'Windows_NT']
+
 // Generate the builds for debug and release, commit and PRJob
 [true, false].each { isPR -> // Defines a closure over true and false, value assigned to isPR
-    ['Debug', 'Release'].each { configuration ->
-        // Determine the name for the new job.  The first parameter is the project,
-        // the second parameter is the base name for the job, and the last parameter
-        // is a boolean indicating whether the job will be a PR job.  If true, the
-        // suffix _prtest will be appended.
-        def newJobName = Utilities.getFullJobName(project, configuration, isPR)
-        
-        // Define build string
-        def buildString = """call build.cmd ${configuration}"""
-
-        // Create a new job with the specified name.  The brace opens a new closure
-        // and calls made within that closure apply to the newly created job.
-        def newJob = job(newJobName) {
+    ['Debug', 'Release'].each { configurationGroup ->
+        ['Windows_NT', 'Ubuntu16.04', 'OSX10.12'].each { osName ->   
+             def osGroup = osGroupMap[osName]
+             def osForMachineAffinity = osName
+            // Determine the name for the new job.  The first parameter is the project,
+            // the second parameter is the base name for the job, and the last parameter
+            // is a boolean indicating whether the job will be a PR job.  If true, the
+            // suffix _prtest will be appended.
+            def newJobName = Utilities.getFullJobName(project, configuration, isPR)
             
-            // This opens the set of build steps that will be run.
-            steps {
-                // Indicates that a batch script should be run with the build string (see above)
-                // Also available is:
-                // shell (for unix scripting)
-                batchFile(buildString)
+            // Define build string
+            def buildString = """call build.cmd ${configuration}"""
+
+            // Create a new job with the specified name.  The brace opens a new closure
+            // and calls made within that closure apply to the newly created job.
+            def newJob = job(newJobName) {
+                
+                // This opens the set of build steps that will be run.
+                steps {
+                    // Indicates that a batch script should be run with the build string (see above)
+                    // Also available is:
+                    // shell (for unix scripting)
+                    batchFile(buildString)
+                }
             }
-        }
-        
-        Utilities.setMachineAffinity(newJob, 'Windows_NT', 'latest-or-auto')
-        Utilities.standardJobSetup(newJob, project, isPR, "*/${branch}")
-        // Archive only on commit builds.
-        if (isPR) {
-            Utilities.addGithubPRTriggerForBranch(newJob, branch, "Windows ${configuration}")
-        }
-        else {
-            Utilities.addGithubPushTrigger(newJob)
-        }
+            
+            Utilities.setMachineAffinity(newJob, osForMachineAffinity, 'latest-or-auto')
+            Utilities.standardJobSetup(newJob, project, isPR, "*/${branch}")
+            // Archive only on commit builds.
+            if (isPR) {
+                 // Set PR trigger, we run Windows_NT, Ubuntu 16.04, and OSX on every PR.
+                Utilities.addGithubPRTriggerForBranch(newJob, branch, "Innerloop ${osName} ${configuration} Build and Test")
+            }
+            else {
+                Utilities.addGithubPushTrigger(newJob)
+            }
     }
 }
 
