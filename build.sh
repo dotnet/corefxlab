5db72436b3b56fb3fa47d9057bbabe181f7eae47
Ahson Ahmed Khan <ahsonkhan@users.noreply.github.com>
2017-05-10 15:42:11 -0700

    Adding build script to build repo on linux (#1532)
    * Adding build script to build repo on linux
    
    * Resolving issues to build in linux.
    
    * Addressing PR comments + cleaning up unused using directives.
    
    * Updating the compiler to the correct one.
    
    * Adding dotnet to the path.
    
    * Removing use of new compiler, issue #1536
    
    * Installing shared framework 1.1 used by roslyn
    
    * Removing UTF-8 BOM from install script and preserving rights (executable).

---
+++ build.sh
@@ -0,0 +1,102 @@
+#!/usr/bin/env bash
+
+# adding dotnet to the path. it is needed to run toolset csc. 
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+dotnet_path=$parent_path/dotnet
+export PATH=$PATH:$dotnet_path
+
+usage()
+{
+    echo "build.sh [ Debug(default) | Release ]"
+}
+
+if [ "$1" == "-?" ] || [ "$1" == "-h" ]; then
+    usage
+	exit
+fi
+
+if [ $# -eq 0 ]
+  then
+    Configuration="Debug"
+  else
+    Configuration=$1
+fi
+
+BuildVersion="e$(date '+%y%m%d')-1"
+Version="<default>"
+Restore="true"
+echo "Configuration=$Configuration."
+echo "Restore=$Restore."
+echo "Version=$Version."
+echo "BuildVersion=$BuildVersion."
+
+if [ ! -d "dotnet" ]; then
+  echo "dotnet.exe not installed, downloading and installing."
+  if [ "$Version" = "<default>" ]; then 
+    Version=$(head -n 1 "DotnetCLIVersion.txt")
+  fi
+  ./scripts/install-dotnet.sh -Version $Version -InstallDir "dotnet"
+  ret=$?
+  if [ $ret -ne 0 ]; then
+    echo "Failed to install latest dotnet.exe, exit code "$ret", aborting build."
+    exit -1
+  fi
+  ./scripts/install-dotnet.sh -Version 1.0.0 -InstallDir "dotnet"
+  ret=$?
+  if [ $ret -ne 0 ]; then
+    echo "Failed to install framework version 1.0.0, exit code "$ret", aborting build."
+    exit -1
+  fi
+fi
+
+dotnetExePath="dotnet/dotnet"
+
+myFile="corefxlab.sln"
+
+if [ "$Restore" = "true" ]; then
+  echo "Restoring all packages"
+  ./$dotnetExePath restore $myFile /p:VersionSuffix=$BuildVersion
+  ret=$?
+  if [ $ret -ne 0 ]; then
+    echo "Failed to restore packages."
+    exit -1
+  fi
+fi
+
+echo "Building solution $myFile..."
+
+./$dotnetExePath build $myFile -c $Configuration /p:VersionSuffix=$BuildVersion
+ret=$?
+if [ $ret -ne 0 ]; then
+  echo "Failed to build solution $myFile"
+  exit -1
+fi
+
+errorsEncountered=0
+declare -a projectsFailed
+
+while read testFile;
+do
+  echo "Building and running tests for project $testFile..."
+  ./$dotnetExePath test $testFile -c $Configuration --no-build -- -notrait category=performance -notrait category=outerloop
+  ret=$?
+  if [ $ret -ne 0 ]; then
+    echo "Some tests failed in project $testFile"
+    projectsFailed[${#projectsFailed[@]}]="$testFile"
+	((errorsEncountered=errorsEncountered+1))
+  fi
+done < <(find tests -name "*.csproj")
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+if [ $errorsEncountered -ne 0 ]; then
+  echo -e "${RED}** Build failed. $errorsEncountered projects failed to build or test. **${NC}"
+  for i in "${projectsFailed[@]}"
+  do
+   echo -e "${RED}    $i${NC}"
+  done
+else
+  echo -e "${GREEN}** Build succeeded. **${NC}"
+fi
