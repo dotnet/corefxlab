9f801d0688030d94cae1f18e37614537c73b7395
ahsonkhan <ahkha@microsoft.com>
2017-05-10 21:21:48 +0000

    Removing UTF-8 BOM from install script and preserving rights (executable).

--- scripts/install-dotnet.sh
+++ scripts/install-dotnet.sh
@@ -1,4 +1,4 @@
-﻿#!/usr/bin/env bash
+#!/usr/bin/env bash
 # Copyright (c) .NET Foundation and contributors. All rights reserved.
 # Licensed under the MIT license. See LICENSE file in the project root for full license information.
 #
