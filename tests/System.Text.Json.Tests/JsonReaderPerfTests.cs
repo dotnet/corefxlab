9697be29b48eca9e131099a1c25603f8c3edaf76
Robert Anderson <robert.anderson@microsoft.com>
2017-05-22 09:34:08 -0700

    Adding TechEmPower Hello World perf test.

--- tests/System.Text.Json.Tests/JsonReaderPerfTests.cs
+++ tests/System.Text.Json.Tests/JsonReaderPerfTests.cs
@@ -14,10 +14,10 @@ namespace System.Text.Json.Tests
         [Benchmark(InnerIterationCount = 1000)]
         [InlineData(EncoderTarget.InvariantUtf8)]
         [InlineData(EncoderTarget.InvariantUtf16)]
-        public void ReaderSystemTextJson(EncoderTarget encoderTarget)
+        public void ReaderSystemTextJsonBasic(EncoderTarget encoderTarget)
         {
             var encoder = GetTargetEncoder(encoderTarget);
-            var data = LoadTestData(encoderTarget);
+            var data = EncodeTestData(encoderTarget, TestJson.HeavyNestedJson);
 
             foreach (var iteration in Benchmark.Iterations)
             {
@@ -34,10 +34,53 @@ namespace System.Text.Json.Tests
         [Benchmark(InnerIterationCount = 1000)]
         [InlineData(EncoderTarget.InvariantUtf8)]
         [InlineData(EncoderTarget.InvariantUtf16)]
-        public void ReaderNewtonsoft(EncoderTarget encoderTarget)
+        public void ReaderNewtonsoftBasic(EncoderTarget encoderTarget)
         {
             var enc = encoderTarget == EncoderTarget.InvariantUtf8 ? Encoding.UTF8 : Encoding.Unicode;
-            var data = LoadTestData(encoderTarget);
+            var data = EncodeTestData(encoderTarget, TestJson.HeavyNestedJson);
+            var mem = new MemoryStream(data);
+            var reader = new StreamReader(mem, enc, false, 1024, true);
+
+            foreach (var iteration in Benchmark.Iterations)
+            {
+                using (iteration.StartMeasurement())
+                {
+                    for (int i = 0; i < Benchmark.InnerIterationCount; i++)
+                    {
+                        mem.Seek(0, SeekOrigin.Begin);
+                        TestReaderNewtonsoft(reader);
+                    }
+                }
+            }
+        }
+
+        [Benchmark(InnerIterationCount = 1000)]
+        [InlineData(EncoderTarget.InvariantUtf8)]
+        [InlineData(EncoderTarget.InvariantUtf16)]
+        public void ReaderSystemTextJsonHelloWorld(EncoderTarget encoderTarget)
+        {
+            var encoder = GetTargetEncoder(encoderTarget);
+            var data = EncodeTestData(encoderTarget, TestJson.HelloWorld);
+
+            foreach (var iteration in Benchmark.Iterations)
+            {
+                using (iteration.StartMeasurement())
+                {
+                    for (int i = 0; i < Benchmark.InnerIterationCount; i++)
+                    {
+                        TestReaderSystemTextJson(data, encoder);
+                    }
+                }
+            }
+        }
+
+        [Benchmark(InnerIterationCount = 1000)]
+        [InlineData(EncoderTarget.InvariantUtf8)]
+        [InlineData(EncoderTarget.InvariantUtf16)]
+        public void ReaderNewtonsoftHelloWorld(EncoderTarget encoderTarget)
+        {
+            var enc = encoderTarget == EncoderTarget.InvariantUtf8 ? Encoding.UTF8 : Encoding.Unicode;
+            var data = EncodeTestData(encoderTarget, TestJson.HelloWorld);
             var mem = new MemoryStream(data);
             var reader = new StreamReader(mem, enc, false, 1024, true);
 
@@ -57,21 +100,21 @@ namespace System.Text.Json.Tests
         static void TestReaderSystemTextJson(ReadOnlySpan<byte> data, TextEncoder encoder)
         {
             var json = new System.Text.Json.JsonReader(data, encoder);
-            while (json.Read());
+            while (json.Read()) ;
         }
 
         static void TestReaderNewtonsoft(StreamReader reader)
         {
             using (var json = new Newtonsoft.Json.JsonTextReader(reader))
-                while (json.Read());
+                while (json.Read()) ;
         }
 
-        static byte[] LoadTestData(EncoderTarget encoderTarget)
+        static byte[] EncodeTestData(EncoderTarget encoderTarget, string data)
         {
             if (encoderTarget == EncoderTarget.InvariantUtf16 || encoderTarget == EncoderTarget.SlowUtf16)
-                return Encoding.Unicode.GetBytes(TestJson.HeavyNestedJson);
+                return Encoding.Unicode.GetBytes(data);
             else
-                return Encoding.UTF8.GetBytes(TestJson.HeavyNestedJson);
+                return Encoding.UTF8.GetBytes(data);
         }
     }
 }
--- tests/System.Text.Json.Tests/JsonWriterPerfTests.cs
+++ tests/System.Text.Json.Tests/JsonWriterPerfTests.cs
@@ -21,7 +21,7 @@ namespace System.Text.Json.Tests
         [InlineData(false, EncoderTarget.SlowUtf8)]
         [InlineData(true, EncoderTarget.SlowUtf16)]
         [InlineData(false, EncoderTarget.SlowUtf16)]
-        public void WriterSystemTextJson(bool formatted, EncoderTarget encoderTarget)
+        public void WriterSystemTextJsonBasic(bool formatted, EncoderTarget encoderTarget)
         {
             var encoder = GetTargetEncoder(encoderTarget);
             var f = new ArrayFormatter(BufferSize, encoder);
@@ -33,7 +33,7 @@ namespace System.Text.Json.Tests
                     for (int i = 0; i < Benchmark.InnerIterationCount; i++)
                     {
                         f.Clear();
-                        TestWriterSystemTextJson(formatted, f);
+                        TestWriterSystemTextJsonBasic(formatted, f);
                     }
                 }
             }
@@ -44,7 +44,7 @@ namespace System.Text.Json.Tests
         [InlineData(false, EncoderTarget.InvariantUtf8)]
         [InlineData(true, EncoderTarget.InvariantUtf16)]
         [InlineData(false, EncoderTarget.InvariantUtf16)]
-        public void WriterNewtonsoft(bool formatted, EncoderTarget encoderTarget)
+        public void WriterNewtonsoftBasic(bool formatted, EncoderTarget encoderTarget)
         {
             var enc = encoderTarget == EncoderTarget.InvariantUtf8 ? Encoding.UTF8 : Encoding.Unicode;
             var buffer = new byte[BufferSize];
@@ -58,13 +58,13 @@ namespace System.Text.Json.Tests
                     for (int i = 0; i < Benchmark.InnerIterationCount; i++)
                     {
                         mem.Seek(0, SeekOrigin.Begin);
-                        TestWriterNewtonsoft(formatted, sw);
+                        TestWriterNewtonsoftBasic(formatted, sw);
                     }
                 }
             }
         }
 
-        static void TestWriterSystemTextJson(bool formatted, ArrayFormatter output)
+        static void TestWriterSystemTextJsonBasic(bool formatted, ArrayFormatter output)
         {
             var json = new System.Text.Json.JsonWriter(output, formatted);
 
@@ -93,7 +93,7 @@ namespace System.Text.Json.Tests
             json.WriteObjectEnd();
         }
 
-        static void TestWriterNewtonsoft(bool formatted, StreamWriter writer)
+        static void TestWriterNewtonsoftBasic(bool formatted, StreamWriter writer)
         {
             using (var json = new Newtonsoft.Json.JsonTextWriter(writer))
             {
@@ -130,6 +130,80 @@ namespace System.Text.Json.Tests
                 }
                 json.WriteEnd();
 
+                json.WriteEnd();
+            }
+        }
+
+        [Benchmark(InnerIterationCount = 1000)]
+        [InlineData(true, EncoderTarget.InvariantUtf8)]
+        [InlineData(false, EncoderTarget.InvariantUtf8)]
+        [InlineData(true, EncoderTarget.InvariantUtf16)]
+        [InlineData(false, EncoderTarget.InvariantUtf16)]
+        [InlineData(true, EncoderTarget.SlowUtf8)]
+        [InlineData(false, EncoderTarget.SlowUtf8)]
+        [InlineData(true, EncoderTarget.SlowUtf16)]
+        [InlineData(false, EncoderTarget.SlowUtf16)]
+        public void WriterSystemTextJsonHelloWorld(bool formatted, EncoderTarget encoderTarget)
+        {
+            var encoder = GetTargetEncoder(encoderTarget);
+            var f = new ArrayFormatter(BufferSize, encoder);
+
+            foreach (var iteration in Benchmark.Iterations)
+            {
+                using (iteration.StartMeasurement())
+                {
+                    for (int i = 0; i < Benchmark.InnerIterationCount; i++)
+                    {
+                        f.Clear();
+                        TestWriterSystemTextJsonHelloWorld(formatted, f);
+                    }
+                }
+            }
+        }
+
+        [Benchmark(InnerIterationCount = 1000)]
+        [InlineData(true, EncoderTarget.InvariantUtf8)]
+        [InlineData(false, EncoderTarget.InvariantUtf8)]
+        [InlineData(true, EncoderTarget.InvariantUtf16)]
+        [InlineData(false, EncoderTarget.InvariantUtf16)]
+        public void WriterNewtonsoftHelloWorld(bool formatted, EncoderTarget encoderTarget)
+        {
+            var enc = encoderTarget == EncoderTarget.InvariantUtf8 ? Encoding.UTF8 : Encoding.Unicode;
+            var buffer = new byte[BufferSize];
+            var mem = new MemoryStream(buffer);
+            var sw = new StreamWriter(mem, enc, BufferSize, true);
+
+            foreach (var iteration in Benchmark.Iterations)
+            {
+                using (iteration.StartMeasurement())
+                {
+                    for (int i = 0; i < Benchmark.InnerIterationCount; i++)
+                    {
+                        mem.Seek(0, SeekOrigin.Begin);
+                        TestWriterNewtonsoftHelloWorld(formatted, sw);
+                    }
+                }
+            }
+        }
+
+        static void TestWriterSystemTextJsonHelloWorld(bool formatted, ArrayFormatter output)
+        {
+            var json = new System.Text.Json.JsonWriter(output, formatted);
+
+            json.WriteObjectStart();
+            json.WriteAttribute("message", "Hello, World!");
+            json.WriteObjectEnd();
+        }
+
+        static void TestWriterNewtonsoftHelloWorld(bool formatted, StreamWriter writer)
+        {
+            using (var json = new Newtonsoft.Json.JsonTextWriter(writer))
+            {
+                json.Formatting = formatted ? Newtonsoft.Json.Formatting.Indented : Newtonsoft.Json.Formatting.None;
+
+                json.WriteStartObject();
+                json.WritePropertyName("message");
+                json.WriteValue("Hello, World!");
                 json.WriteEnd();
             }
         }
