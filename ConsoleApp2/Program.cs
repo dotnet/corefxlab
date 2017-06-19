e152fd1cc27133475219bc379fe29c84be58b8a7
Vedin <denistsem@gmail.com>
2017-06-19 14:02:20 -0700

    flush/compress

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+            byte[] bytes = File.ReadAllBytes("ptt5.txt");
+            FileStream fileStreams = File.Create("output.br");
+            BrotliStream brotliStream = new BrotliStream(fileStreams, CompressionMode.Compress,false,65535,CompressionLevel.Optimal);
+            Console.WriteLine("Init OK");
+            brotliStream.Write(bytes, 0, bytes.Length);
+            Console.WriteLine("Write NO error");
+            brotliStream.Flush();
+            Console.WriteLine("Flush NO error");
+            
+        }
+    }
+}
