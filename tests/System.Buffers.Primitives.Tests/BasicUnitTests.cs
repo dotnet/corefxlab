5261df63fb952c5306fa5ed66797e7f8ba029d61
ahsonkhan <ahson_ahmedk@yahoo.com>
2017-06-26 19:38:20 -0700

    Adding owned buffer test, use after gc collection

--- tests/System.Buffers.Primitives.Tests/BasicUnitTests.cs
+++ tests/System.Buffers.Primitives.Tests/BasicUnitTests.cs
@@ -375,5 +375,29 @@ namespace System.Buffers.Tests
                 Assert.Equal(array[i], dangerousArray.Array[i]);
             }          
         }
+
+        [Fact]
+        public void OwnedBufferDisposedAfterFinalizerGCKeepAliveTest()
+        {
+            var owned = (OwnedBuffer<byte>)new byte[1024];
+            var buffer = owned.Buffer;
+            var slice = buffer.Slice(1);
+
+            var span = buffer.Span;
+            var sliceSpan = slice.Span;
+
+            GC.Collect(2);
+            GC.WaitForPendingFinalizers();
+
+            try
+            {
+                span = buffer.Span;
+                span = slice.Span;
+            }
+            catch (ObjectDisposedException ex)
+            {
+                Assert.True(false, "There shouldn't be any Object Disposed Exception here. " + ex.Message);
+            }
+        }
     }
 }
