f0a2f696b142a3093f0d84c11a1609613bde75f4
BrennanConroy <brecon@microsoft.com>
2017-08-10 09:21:22 -0700

    Add cancellation token to stream.CopyToAsync for IPipeWriter

--- src/System.IO.Pipelines.Extensions/StreamExtensions.cs
+++ src/System.IO.Pipelines.Extensions/StreamExtensions.cs
@@ -63,16 +63,17 @@ namespace System.IO.Pipelines
         /// <param name="stream"></param>
         /// <param name="writer"></param>
         /// <returns></returns>
-        public static Task CopyToAsync(this Stream stream, IPipeWriter writer)
+        public static Task CopyToAsync(this Stream stream, IPipeWriter writer, CancellationToken cancellationToken = default(CancellationToken))
         {
-            return stream.CopyToAsync(new PipelineWriterStream(writer));
+            // 81920 is the default bufferSize, there is not stream.CopyToAsync overload that takes only a cancellationToken
+            return stream.CopyToAsync(new PipelineWriterStream(writer, cancellationToken), bufferSize: 81920, cancellationToken: cancellationToken);
         }
 
-        public static async Task CopyToEndAsync(this Stream stream, IPipeWriter writer)
+        public static async Task CopyToEndAsync(this Stream stream, IPipeWriter writer, CancellationToken cancellationToken = default(CancellationToken))
         {
             try
             {
-                await stream.CopyToAsync(writer);
+                await stream.CopyToAsync(writer, cancellationToken);
             }
             catch (Exception ex)
             {
@@ -116,7 +117,7 @@ namespace System.IO.Pipelines
             }
             else
             {
-                // Copy required 
+                // Copy required
                 var array = memory.Span.ToArray();
                 await stream.WriteAsync(array, 0, array.Length).ConfigureAwait(continueOnCapturedContext: false);
             }
@@ -218,11 +219,13 @@ namespace System.IO.Pipelines
 
         private class PipelineWriterStream : Stream
         {
-            private IPipeWriter _writer;
+            private readonly IPipeWriter _writer;
+            private readonly CancellationToken _cancellationToken;
 
-            public PipelineWriterStream(IPipeWriter writer)
+            public PipelineWriterStream(IPipeWriter writer, CancellationToken cancellationToken)
             {
                 _writer = writer;
+                _cancellationToken = cancellationToken;
             }
 
             public override bool CanRead => false;
@@ -266,9 +269,24 @@ namespace System.IO.Pipelines
 
             public override async Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
             {
+                cancellationToken.ThrowIfCancellationRequested();
+                _cancellationToken.ThrowIfCancellationRequested();
+
+                if (_cancellationToken.CanBeCanceled)
+                {
+                    if (cancellationToken.CanBeCanceled)
+                    {
+                        cancellationToken = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken, _cancellationToken).Token;
+                    }
+                    else
+                    {
+                        cancellationToken = _cancellationToken;
+                    }
+                }
+
                 var output = _writer.Alloc();
                 output.Write(new Span<byte>(buffer, offset, count));
-                await output.FlushAsync();
+                await output.FlushAsync(cancellationToken);
             }
         }
     }
