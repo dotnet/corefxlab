2b8ea2fce7eb2eed301b53eaf3eb42a3fb5007fe
Robert Anderson <robert.anderson@microsoft.com>
2017-08-24 11:24:11 -0700

    Small simplification for Guid extension.

--- src/System.Text.Primitives/System/Text/Extensions.Guid.cs
+++ src/System.Text.Primitives/System/Text/Extensions.Guid.cs
@@ -6,13 +6,6 @@ namespace System.Text
     public static partial class Extensions
     {
         public static bool TryFormat(this Guid value, Span<byte> buffer, out int bytesWritten, ParsedFormat format = default, SymbolTable symbolTable = null)
-        {
-            if (symbolTable == null ||  symbolTable == SymbolTable.InvariantUtf8)
-                return Formatters.Utf8.TryFormat(value, buffer, out bytesWritten, format);
-            else if (symbolTable == SymbolTable.InvariantUtf16)
-                return Formatters.Utf16.TryFormat(value, buffer, out bytesWritten, format);
-            else
-                throw new NotSupportedException();
-        }
+            => Formatters.Custom.TryFormat(value, buffer, out bytesWritten, format, symbolTable);
     }
 }
