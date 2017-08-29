e315f75d2469090a38eff2599c105f31c52a42da
ahsonkhan <ahson_ahmedk@yahoo.com>
2017-08-28 19:20:56 -0700

    Revert "Removing OwnedBuffer.Add and using Unsafe.Add (#1713)"
    This reverts commit e5950f4e145529e12915235e44afdf2eedcbca7c.

--- samples/LowAllocationWebServer/LowAllocationWebServerLibrary/OwnedBuffer.cs
+++ samples/LowAllocationWebServer/LowAllocationWebServerLibrary/OwnedBuffer.cs
@@ -5,7 +5,6 @@
 using System;
 using System.Buffers;
 using System.Collections.Sequences;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 namespace Microsoft.Net.Http
