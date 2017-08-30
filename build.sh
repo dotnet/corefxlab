5f8e6c643c61073d327461ab2dceed776c52d4fc
ahsonkhan <ahson_ahmedk@yahoo.com>
2017-08-29 17:49:10 -0700

    Add a step to install framework version 2.0.0 since our tests target netcoreapp2.0.

--- build.sh
+++ build.sh
@@ -48,6 +48,13 @@ if [ ! -d "dotnet" ]; then
     echo "Failed to install framework version 1.0.0, exit code $ret, aborting build."
     exit -1
   fi
+
+  ./scripts/install-dotnet.sh -Version 2.0.0 -InstallDir "dotnet"
+  ret=$?
+  if [ $ret -ne 0 ]; then
+    echo "Failed to install framework version 2.0.0, exit code $ret, aborting build."
+    exit -1
+  fi
 fi
 
 dotnetExePath="dotnet/dotnet"
