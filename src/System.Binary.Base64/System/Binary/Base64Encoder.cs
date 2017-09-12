aac1bee3ebff54e5d46baf4afbe3bb444767f487
Krzysztof Cwalina <kcwalina@microsoft.com>
2017-09-12 09:17:16 -0700

    Completed Base64 Cleanup

--- src/System.Binary.Base64/System/Binary/Base64Encoder.cs
+++ src/System.Binary.Base64/System/Binary/Base64Encoder.cs
@@ -1,34 +1,118 @@
-﻿// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
 
 using System.Buffers;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
 
 namespace System.Binary.Base64
 {
     public static partial class Base64
     {
-        // Pre-computing this table using a custom string(s_characters) and GenerateEncodingMapAndVerify (found in tests)
-        static readonly byte[] s_encodingMap = {
-            65, 66, 67, 68, 69, 70, 71, 72,         //A..H
-            73, 74, 75, 76, 77, 78, 79, 80,         //I..P
-            81, 82, 83, 84, 85, 86, 87, 88,         //Q..X
-            89, 90, 97, 98, 99, 100, 101, 102,      //Y..Z, a..f
-            103, 104, 105, 106, 107, 108, 109, 110, //g..n
-            111, 112, 113, 114, 115, 116, 117, 118, //o..v
-            119, 120, 121, 122, 48, 49, 50, 51,     //w..z, 0..3
-            52, 53, 54, 55, 56, 57, 43, 47          //4..9, +, /
-        };
+        public static readonly BufferEncoder BytesToUtf8Encoder = new ToBase64Utf8();
 
-        const byte s_encodingPad = (byte)'=';              // '=', for padding
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        [Obsolete("Use Base64.BytesToUtf8Length")]
-        public static int ComputeEncodedUtf8Length(int sourceLength)
+        public static OperationStatus BytesToUtf8(ReadOnlySpan<byte> bytes, Span<byte> utf8, out int consumed, out int written)
         {
-            Diagnostics.Debug.Assert(sourceLength >= 0);
-            return ((sourceLength + 2) / 3) << 2;
+            ref byte srcBytes = ref bytes.DangerousGetPinnableReference();
+            ref byte destBytes = ref utf8.DangerousGetPinnableReference();
+
+            int srcLength = bytes.Length;
+            int destLength = utf8.Length;
+
+            int sourceIndex = 0;
+            int destIndex = 0;
+            int result = 0;
+
+            while (sourceIndex < srcLength - 2)
+            {
+                result = Encode(ref Unsafe.Add(ref srcBytes, sourceIndex));
+                if (destIndex > destLength - 4) goto DestinationSmallExit;
+                Unsafe.WriteUnaligned(ref Unsafe.Add(ref destBytes, destIndex), result);
+                destIndex += 4;
+                sourceIndex += 3;
+            }
+
+            if (sourceIndex == srcLength - 1)
+            {
+                result = EncodeAndPadTwo(ref Unsafe.Add(ref srcBytes, sourceIndex));
+                if (destIndex > destLength - 4) goto DestinationSmallExit;
+                Unsafe.WriteUnaligned(ref Unsafe.Add(ref destBytes, destIndex), result);
+                destIndex += 4;
+                sourceIndex += 1;
+            }
+            else if (sourceIndex == srcLength - 2)
+            {
+                result = EncodeAndPadOne(ref Unsafe.Add(ref srcBytes, sourceIndex));
+                if (destIndex > destLength - 4) goto DestinationSmallExit;
+                Unsafe.WriteUnaligned(ref Unsafe.Add(ref destBytes, destIndex), result);
+                destIndex += 4;
+                sourceIndex += 2;
+            }
+
+            consumed = sourceIndex;
+            written = destIndex;
+            return OperationStatus.Done;
+
+            DestinationSmallExit:
+            consumed = sourceIndex;
+            written = destIndex;
+            return OperationStatus.DestinationTooSmall;
+        }
+
+        public static int BytesToUtf8Length(int bytesLength)
+        {
+            Debug.Assert(bytesLength >= 0);
+            return ((bytesLength + 2) / 3) << 2;
+        }
+
+        public static OperationStatus BytesToUtf8InPlace(Span<byte> buffer, int bytesLength, out int written)
+        {
+            var encodedLength = BytesToUtf8Length(bytesLength);
+            if (buffer.Length < encodedLength) goto FalseExit;
+
+            var leftover = bytesLength - bytesLength / 3 * 3; // how many bytes after packs of 3
+
+            var destinationIndex = encodedLength - 4;
+            var sourceIndex = bytesLength - leftover;
+
+            // encode last pack to avoid conditional in the main loop
+            if (leftover != 0)
+            {
+                var sourceSlice = buffer.Slice(sourceIndex, leftover);
+                var desitnationSlice = buffer.Slice(destinationIndex, 4);
+                destinationIndex -= 4;
+                var result = BytesToUtf8(sourceSlice, desitnationSlice, out int consumed, out int tempWritten);
+                Debug.Assert(result == OperationStatus.Done);
+            }
+
+            for (int index = sourceIndex - 3; index >= 0; index -= 3)
+            {
+                var sourceSlice = buffer.Slice(index, 3);
+                var desitnationSlice = buffer.Slice(destinationIndex, 4);
+                destinationIndex -= 4;
+                var result = BytesToUtf8(sourceSlice, desitnationSlice, out int consumed, out int tempWritten);
+                Debug.Assert(result == OperationStatus.Done);
+            }
+
+            written = encodedLength;
+            return OperationStatus.Done;
+
+            FalseExit:
+            written = 0;
+            return OperationStatus.DestinationTooSmall;
+        }
+
+        sealed class ToBase64Utf8 : BufferEncoder
+        {
+            public override OperationStatus Encode(ReadOnlySpan<byte> source, Span<byte> destination, out int bytesConsumed, out int bytesWritten)
+                => Base64.Encode(source, destination, out bytesConsumed, out bytesWritten);
+
+            public override OperationStatus EncodeInPlace(Span<byte> buffer, int inputLength, out int written)
+                => Base64.EncodeInPlace(buffer, inputLength, out written) ? OperationStatus.Done : OperationStatus.DestinationTooSmall;
+
+            public override bool IsEncodeInPlaceSupported => true;
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -48,19 +132,6 @@ namespace System.Binary.Base64
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static int Encode(ref byte srcBytes)
-        {
-            byte b0 = srcBytes;
-            byte b1 = Unsafe.Add(ref srcBytes, 1);
-            byte b2 = Unsafe.Add(ref srcBytes, 2);
-
-            Encode(b0, b1, b2, out byte r0, out byte r1, out byte r2, out byte r3);
-
-            int result = r3 << 24 | r2 << 16 | r1 << 8 | r0;
-            return result;
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static void Encode(byte b0, byte b1, byte b2, out byte r0, out byte r1, out byte r2)
         {
             int i0 = b0 >> 2;
@@ -74,14 +145,6 @@ namespace System.Binary.Base64
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static int EncodePadByOne(ref byte srcBytes)
-        {
-            Encode(srcBytes, Unsafe.Add(ref srcBytes, 1), 0, out byte r0, out byte r1, out byte r2);
-            int result = s_encodingPad << 24 | r2 << 16 | r1 << 8 | r0;
-            return result;
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static void Encode(byte b0, byte b1, out byte r0, out byte r1)
         {
             int i0 = b0 >> 2;
@@ -92,12 +155,51 @@ namespace System.Binary.Base64
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static int EncodePadByTwo(ref byte srcBytes)
+        private static int Encode(ref byte threeBytes)
         {
-            Encode(srcBytes, 0, out byte r0, out byte r1);
+            byte b0 = threeBytes;
+            byte b1 = Unsafe.Add(ref threeBytes, 1);
+            byte b2 = Unsafe.Add(ref threeBytes, 2);
+
+            Encode(b0, b1, b2, out byte r0, out byte r1, out byte r2, out byte r3);
+
+            int result = r3 << 24 | r2 << 16 | r1 << 8 | r0;
+            return result;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static int EncodeAndPadOne(ref byte twoBytes)
+        {
+            Encode(twoBytes, Unsafe.Add(ref twoBytes, 1), 0, out byte r0, out byte r1, out byte r2);
+            int result = s_encodingPad << 24 | r2 << 16 | r1 << 8 | r0;
+            return result;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static int EncodeAndPadTwo(ref byte oneByte)
+        {
+            Encode(oneByte, 0, out byte r0, out byte r1);
             int result = s_encodingPad << 24 | s_encodingPad << 16 | r1 << 8 | r0;
             return result;
         }
+
+        // Pre-computing this table using a custom string(s_characters) and GenerateEncodingMapAndVerify (found in tests)
+        static readonly byte[] s_encodingMap = {
+            65, 66, 67, 68, 69, 70, 71, 72,         //A..H
+            73, 74, 75, 76, 77, 78, 79, 80,         //I..P
+            81, 82, 83, 84, 85, 86, 87, 88,         //Q..X
+            89, 90, 97, 98, 99, 100, 101, 102,      //Y..Z, a..f
+            103, 104, 105, 106, 107, 108, 109, 110, //g..n
+            111, 112, 113, 114, 115, 116, 117, 118, //o..v
+            119, 120, 121, 122, 48, 49, 50, 51,     //w..z, 0..3
+            52, 53, 54, 55, 56, 57, 43, 47          //4..9, +, /
+        };
+
+        const byte s_encodingPad = (byte)'='; // '=', for padding
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        [Obsolete("Use Base64.BytesToUtf8Length")]
+        public static int ComputeEncodedUtf8Length(int sourceLength) => BytesToUtf8Length(sourceLength);
 
         /// <summary>
         /// 
@@ -108,52 +210,7 @@ namespace System.Binary.Base64
         [EditorBrowsable(EditorBrowsableState.Never)]
         [Obsolete("Use Base64.BytesToUtf8InPlace")]
         public static OperationStatus Encode(ReadOnlySpan<byte> source, Span<byte> destination, out int bytesConsumed, out int bytesWritten)
-        {
-            ref byte srcBytes = ref source.DangerousGetPinnableReference();
-            ref byte destBytes = ref destination.DangerousGetPinnableReference();
-
-            int srcLength = source.Length;
-            int destLength = destination.Length;
-
-            int sourceIndex = 0;
-            int destIndex = 0;
-            int result = 0;
-
-            while (sourceIndex < srcLength - 2)
-            {
-                result = Encode(ref Unsafe.Add(ref srcBytes, sourceIndex));
-                if (destIndex > destLength - 4) goto DestinationSmallExit;
-                Unsafe.WriteUnaligned(ref Unsafe.Add(ref destBytes, destIndex), result);
-                destIndex += 4;
-                sourceIndex += 3;
-            }
-
-            if (sourceIndex == srcLength - 1)
-            {
-                result = EncodePadByTwo(ref Unsafe.Add(ref srcBytes, sourceIndex));
-                if (destIndex > destLength - 4) goto DestinationSmallExit;
-                Unsafe.WriteUnaligned(ref Unsafe.Add(ref destBytes, destIndex), result);
-                destIndex += 4;
-                sourceIndex += 1;
-            }
-            else if (sourceIndex == srcLength - 2)
-            {
-                result = EncodePadByOne(ref Unsafe.Add(ref srcBytes, sourceIndex));
-                if (destIndex > destLength - 4) goto DestinationSmallExit;
-                Unsafe.WriteUnaligned(ref Unsafe.Add(ref destBytes, destIndex), result);
-                destIndex += 4;
-                sourceIndex += 2;
-            }
-
-            bytesConsumed = sourceIndex;
-            bytesWritten = destIndex;
-            return OperationStatus.Done;
-
-            DestinationSmallExit:
-            bytesConsumed = sourceIndex;
-            bytesWritten = destIndex;
-            return OperationStatus.DestinationTooSmall;
-        }
+            => BytesToUtf8(source, destination, out bytesConsumed, out bytesWritten);
 
         /// <summary>
         /// 
@@ -164,49 +221,6 @@ namespace System.Binary.Base64
         [Obsolete("Use Base64.BytesToUtf8InPlace")]
         [EditorBrowsable(EditorBrowsableState.Never)]
         public static bool EncodeInPlace(Span<byte> buffer, int sourceLength, out int bytesWritten)
-        {
-            var encodedLength = ComputeEncodedUtf8Length(sourceLength);
-            if (buffer.Length < encodedLength) goto FalseExit;
-
-            var leftover = sourceLength - sourceLength / 3 * 3; // how many bytes after packs of 3
-
-            var destinationIndex = encodedLength - 4;
-            var sourceIndex = sourceLength - leftover;
-
-            // encode last pack to avoid conditional in the main loop
-            if (leftover != 0)
-            {
-                var sourceSlice = buffer.Slice(sourceIndex, leftover);
-                var desitnationSlice = buffer.Slice(destinationIndex, 4);
-                destinationIndex -= 4;
-                Encode(sourceSlice, desitnationSlice, out int consumed, out int written);
-            }
-
-            for (int index = sourceIndex - 3; index >= 0; index -= 3)
-            {
-                var sourceSlice = buffer.Slice(index, 3);
-                var desitnationSlice = buffer.Slice(destinationIndex, 4);
-                destinationIndex -= 4;
-                Encode(sourceSlice, desitnationSlice, out int consumed, out int written);
-            }
-            
-            bytesWritten = encodedLength;
-            return true;
-
-            FalseExit:
-            bytesWritten = 0;
-            return false;
-        }
-
-        sealed class ToBase64Utf8 : BufferEncoder
-        {
-            public override OperationStatus Encode(ReadOnlySpan<byte> source, Span<byte> destination, out int bytesConsumed, out int bytesWritten)
-                => Base64.Encode(source, destination, out bytesConsumed, out bytesWritten);
-
-            public override OperationStatus EncodeInPlace(Span<byte> buffer, int inputLength, out int written)
-                => Base64.EncodeInPlace(buffer, inputLength, out written)?OperationStatus.Done:OperationStatus.DestinationTooSmall;
-
-            public override bool IsEncodeInPlaceSupported => true;
-        }
+            => BytesToUtf8InPlace(buffer, sourceLength, out bytesWritten) == OperationStatus.Done;
     }
 }
