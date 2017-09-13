343866271d1b9e56d4ae67c4051cd2e2680d7f94
ahsonkhan <ahson_ahmedk@yahoo.com>
2017-09-13 15:06:13 -0700

    Adding concrete owned memory for tests and making ownedarray public

---
+++ src/System.Buffers.Primitives/System/Buffers/OwnedArray.cs
@@ -0,0 +1,85 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Runtime;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace System.Buffers
+{
+    public class OwnedArray<T> : OwnedMemory<T>
+    {
+        T[] _array;
+        int _referenceCount;
+
+        public OwnedArray(int length)
+        {
+            _array = new T[length];
+        }
+
+        public OwnedArray(T[] array)
+        {
+            if (array == null) BufferPrimitivesThrowHelper.ThrowArgumentNullException(nameof(array)); 
+            _array = array;
+        }
+
+        public static implicit operator OwnedArray<T>(T[] array) => new OwnedArray<T>(array);
+
+        public override int Length => _array.Length;
+
+        public override Span<T> AsSpan()
+        {
+            if (IsDisposed) BufferPrimitivesThrowHelper.ThrowObjectDisposedException(nameof(OwnedArray<T>));
+            return new Span<T>(_array);
+        }
+
+        public override MemoryHandle Pin()
+        {
+            unsafe
+            {
+                Retain();
+                var handle = GCHandle.Alloc(_array, GCHandleType.Pinned);
+                return new MemoryHandle(this, (void*)handle.AddrOfPinnedObject(), handle);
+            }
+        }
+
+        protected override bool TryGetArray(out ArraySegment<T> arraySegment)
+        {
+            if (IsDisposed) BufferPrimitivesThrowHelper.ThrowObjectDisposedException(nameof(OwnedArray<T>));
+            arraySegment = new ArraySegment<T>(_array);
+            return true;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _array = null;
+        }
+
+        public override void Retain()
+        {
+            if (IsDisposed) BufferPrimitivesThrowHelper.ThrowObjectDisposedException(nameof(OwnedArray<T>));
+            Interlocked.Increment(ref _referenceCount);
+        }
+
+        public override bool Release()
+        {
+            int newRefCount = Interlocked.Decrement(ref _referenceCount);
+            if (newRefCount < 0)  BufferPrimitivesThrowHelper.ThrowInvalidOperationException();
+            if (newRefCount == 0)
+            {
+                OnNoReferences();
+                return false;
+            }
+            return true;
+        }
+
+        protected virtual void OnNoReferences()
+        {
+        }
+
+        protected override bool IsRetained => _referenceCount > 0;
+
+        public override bool IsDisposed => _array == null;
+    }
+}
