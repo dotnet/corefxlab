6568dce8d1c0214f2e19727232497b79cfd27cd8
Eric St. John <ericstj@microsoft.com>
2017-10-24 15:13:07 -0700

    Add TensorShape experiment

---
+++ src/System.Numerics.Tensors/System/Numerics/Range.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.Numerics
+{
+    // To be replaced once C# natively supports ranges and Range syntax (lower:upper) or (lower..upper) (lower:) or (lower..) etc
+    public struct Range
+    {
+        public Range(int lowerBound, int upperBound)
+        {
+            if (lowerBound > upperBound)
+            {
+                throw new ArgumentException($"{nameof(upperBound)} must be greater than or equal to {nameof(lowerBound)}", nameof(upperBound));
+            }
+
+            LowerBound = lowerBound;
+            UpperBound = upperBound;
+        }
+
+        public int LowerBound { get; }
+        public int UpperBound { get; }
+
+        public int Length
+        {
+            get
+            {
+                return UpperBound - LowerBound + 1;
+            }
+        }
+
+    }
+}
