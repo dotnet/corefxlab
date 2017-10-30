981181f58a6f5e47ec5d31e35b3e07f3c25c8900
ahsonkhan <ahson_ahmedk@yahoo.com>
2017-10-30 15:39:48 -0700

    Set environment variable in the package script

--- scripts/package.ps1
+++ scripts/package.ps1
@@ -9,6 +9,8 @@ $dotnetExePath="$repoRoot\dotnetcli\dotnet.exe"
 $nugetPath = "$repoRoot\nuget\nuget.exe"
 $packagesPath = "$repoRoot\packages"
 
+$env:DOTNET_MULTILEVEL_LOOKUP = 0
+
 Function Ensure-Nuget-Exists {
     if (!(Test-Path "$nugetPath")) {
         if (!(Test-Path "$repoRoot\nuget")) {
