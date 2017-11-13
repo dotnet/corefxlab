2729012cafbf358b6e109ab91cd3d84ce1d50ffb
Matt Johnson <matt.johnson@microsoft.com>
2017-11-13 09:48:31 -0800

    Add some unit tests for remaining extension methods

--- tests/System.Time.Tests/DateTimeOffsetTests.cs
+++ tests/System.Time.Tests/DateTimeOffsetTests.cs
@@ -1,10 +1,36 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using Xunit;
+
 namespace System._Time.Tests
 {
     public class DateTimeOffsetTests
     {
+        [Fact]
+        public void CanGetDateFromDateTimeOffset()
+        {
+            DateTimeOffset dto = DateTimeOffset.MaxValue;
 
+            Date d = dto.GetDate();
+
+            Assert.Equal(dto.Year, d.Year);
+            Assert.Equal(dto.Month, d.Month);
+            Assert.Equal(dto.Day, d.Day);
+        }
+
+        [Fact]
+        public void CanGetTimeFromDateTimeOffset()
+        {
+            DateTimeOffset dto = DateTimeOffset.MaxValue;
+
+            Time t = dto.GetTime();
+
+            Assert.Equal(dto.Hour, t.Hour);
+            Assert.Equal(dto.Minute, t.Minute);
+            Assert.Equal(dto.Second, t.Second);
+            Assert.Equal(dto.Millisecond, t.Millisecond);
+            Assert.Equal(dto.Ticks % TimeSpan.TicksPerMillisecond, t.Ticks % TimeSpan.TicksPerMillisecond);
+        }
     }
 }
--- tests/System.Time.Tests/DateTimeTests.cs
+++ tests/System.Time.Tests/DateTimeTests.cs
@@ -1,10 +1,36 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using Xunit;
+
 namespace System._Time.Tests
 {
     public class DateTimeTests
     {
+        [Fact]
+        public void CanGetDateFromDateTime()
+        {
+            DateTime dt = DateTime.MaxValue;
 
+            Date d = dt.GetDate();
+
+            Assert.Equal(dt.Year, d.Year);
+            Assert.Equal(dt.Month, d.Month);
+            Assert.Equal(dt.Day, d.Day);
+        }
+
+        [Fact]
+        public void CanGetTimeFromDateTime()
+        {
+            DateTime dt = DateTime.MaxValue;
+
+            Time t = dt.GetTime();
+
+            Assert.Equal(dt.Hour, t.Hour);
+            Assert.Equal(dt.Minute, t.Minute);
+            Assert.Equal(dt.Second, t.Second);
+            Assert.Equal(dt.Millisecond, t.Millisecond);
+            Assert.Equal(dt.Ticks % TimeSpan.TicksPerMillisecond, t.Ticks % TimeSpan.TicksPerMillisecond);
+        }
     }
 }
