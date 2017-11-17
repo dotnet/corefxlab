ad3d72181555032ac6d59f5ee5c7f7707df609cd
Krzysztof Cwalina <kcwalina@microsoft.com>
2017-11-17 15:22:27 -0800

    AddedSequenceExtensions.IndexOf

---
+++ src/System.Buffers.Experimental/System/Buffers/Sequences/SequenceExtensions.cs
@@ -0,0 +1,24 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Sequences;
+
+namespace System.Buffers
+{
+    public static class SequenceExtensions
+    {
+        public static long IndexOf(this ISequence<ReadOnlyMemory<byte>> sequence, byte value)
+        {
+            Position position = default;
+            int totalIndex = 0;
+            while (sequence.TryGet(ref position, out ReadOnlyMemory<byte> memory))
+            {
+                var index = memory.Span.IndexOf(value);
+                if (index != -1) return index + totalIndex;
+                totalIndex += memory.Length;
+            }
+            return -1;
+        }
+    }
+}
---
+++ tests/System.Buffers.Experimental.Tests/SequenceExtensionsTests.cs
@@ -0,0 +1,64 @@
+﻿using System.IO.Pipelines;
+using System.IO.Pipelines.Testing;
+using Xunit;
+
+namespace System.Buffers.Tests
+{
+    public class SequenceExtensionsTests
+    {
+        [Fact]
+        public void SequenceIndexOfSingleSegment()
+        {
+            var array = new byte[] { 1, 2, 3, 4, 5 };
+            var bytes = new ReadOnlyBytes(array);
+            Assert.Equal(array.Length, bytes.Length);
+
+            // Static method call to avoid calling ReadOnlyBytes.IndexOf
+            Assert.Equal(-1, SequenceExtensions.IndexOf(bytes, 0));
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Assert.Equal(i, SequenceExtensions.IndexOf(bytes, (byte)(i+1)));
+            }
+        }
+
+        [Fact]
+        public void SequenceIndexOfMultiSegment()
+        {
+            ReadOnlyBytes bytes = ListHelper.CreateRob(
+                new byte[] { 1, 2},
+                new byte[] { 3, 4 }
+            );
+
+            Assert.Equal(4, bytes.Length);
+
+            // Static method call to avoid calling ReadOnlyBytes.IndexOf
+            Assert.Equal(-1, SequenceExtensions.IndexOf(bytes, 0));
+
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                Assert.Equal(i, SequenceExtensions.IndexOf(bytes, (byte)(i + 1)));
+            }
+        }
+
+        [Fact]
+        public void SequenceIndexOfMultiSegmentSliced()
+        {
+            ReadOnlyBytes bytes = ListHelper.CreateRob(
+                new byte[] { 1, 2 },
+                new byte[] { 3, 4 }
+            );
+            bytes = bytes.Slice(1);
+
+            Assert.Equal(3, bytes.Length);
+
+            // Static method call to avoid calling ReadOnlyBytes.IndexOf
+            Assert.Equal(-1, SequenceExtensions.IndexOf(bytes, 0));
+
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                Assert.Equal(i, SequenceExtensions.IndexOf(bytes, (byte)(i + 2)));
+            }
+        }
+    }
+}
