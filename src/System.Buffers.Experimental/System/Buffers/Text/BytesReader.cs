694f5c5a5c9c3cc116b17d379772c4fab0c3f9e0
Krzysztof Cwalina <kcwalina@microsoft.com>
2017-11-30 14:14:36 -0800

    Added BytesReader.TryRead(out int) (#1964)
    * Added TryRead
    
    * Added Perf Tests

--- src/System.Buffers.Experimental/System/Buffers/Text/BytesReader.cs
+++ src/System.Buffers.Experimental/System/Buffers/Text/BytesReader.cs
@@ -1,21 +1,13 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System.Buffers.Text;
+using System.Buffers.Binary;
 using System.Collections.Sequences;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
 
 namespace System.Buffers.Text
 {
-    public static class BytesReader
-    {
-        public static BytesReader<T> Create<T>(T sequence) where T : ISequence<ReadOnlyMemory<byte>>
-        {
-            return new BytesReader<T>(sequence);
-        }
-    }
-
     /// <summary>
     /// Used to read text from byte buffers
     /// </summary>
@@ -24,11 +16,8 @@ namespace System.Buffers.Text
         readonly TSequence _bytes;
         Position _currentSegmentPosition;
         Position _nextSegmentPosition;
-        ReadOnlyMemory<byte> _currentMemory; // TODO: I think this can be removed
         ReadOnlySpan<byte> _currentSpan;
         int _currentSpanIndex;
-
-        readonly SymbolTable _symbolTable;
 
         // TODO: should there be a ctor that takes sequence + position? 
         // TODO: should there be a type that is sequence + position?
@@ -37,9 +26,8 @@ namespace System.Buffers.Text
             _bytes = bytes;
             _currentSegmentPosition = bytes.First;
             _nextSegmentPosition = _currentSegmentPosition;
-            _bytes.TryGet(ref _nextSegmentPosition, out _currentMemory);
-            _currentSpan = _currentMemory.Span;
-            _symbolTable = SymbolTable.InvariantUtf8;
+            _bytes.TryGet(ref _nextSegmentPosition, out ReadOnlyMemory<byte> memory);
+            _currentSpan = memory.Span;
             _currentSpanIndex = 0;
         }
 
@@ -48,9 +36,7 @@ namespace System.Buffers.Text
             _bytes = default;
             _nextSegmentPosition = default;
             _currentSegmentPosition = Position.Create(0);
-            _currentMemory = bytes;
             _currentSpan = bytes.Span;
-            _symbolTable = SymbolTable.InvariantUtf8;
             _currentSpanIndex = 0;
         }
 
@@ -59,9 +45,7 @@ namespace System.Buffers.Text
             _bytes = default;
             _nextSegmentPosition = default;
             _currentSegmentPosition = Position.Create(0);
-            _currentMemory = default;
             _currentSpan = bytes;
-            _symbolTable = SymbolTable.InvariantUtf8;
             _currentSpanIndex = 0;
         }
 
@@ -109,58 +93,80 @@ namespace System.Buffers.Text
             range.To = PositionOf(delimiter);
             if (!range.To.IsEnd)
             {
-                Seek(range.To);
-                Seek(delimiter.Length);
+                Advance(range.To);
+                Advance(delimiter.Length);
             }
             return range;
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public void Seek(long count)
+        public void Advance(long count)
         {
             var unreadLength = _currentSpan.Length - _currentSpanIndex;
-            if (count <= unreadLength)
-            {
+            if (count < unreadLength) {
                 _currentSpanIndex += (int)count;
             }
-            else
-            {
-                if (!_bytes.TryGet(ref _nextSegmentPosition, out _currentMemory))
-                {
-                    throw new ArgumentOutOfRangeException(nameof(count));
-                }
-                _currentSpan = _currentMemory.Span;
-                _currentSpanIndex = 0;
-                Seek(count - unreadLength);
+            else {
+                AdvanceNextSegment(count, unreadLength);
             }
         }
 
-        public void Seek(Position position)
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Advance(int count)
+        {
+            var unreadLength = _currentSpan.Length - _currentSpanIndex;
+            if (count < unreadLength)
+            {
+                _currentSpanIndex += count;
+            }
+            else
+            {
+                AdvanceNextSegment(count, unreadLength);
+            }
+        }
+
+        private void AdvanceNextSegment(long count, int currentSegmentUnread)
+        {
+            if (!_bytes.TryGet(ref _nextSegmentPosition, out ReadOnlyMemory<byte> memory))
+            {
+                if (count > currentSegmentUnread) throw new ArgumentOutOfRangeException(nameof(count));
+                else
+                {
+                    Debug.Assert(count == currentSegmentUnread);
+                    _currentSpan = Span<byte>.Empty;
+                    _currentSpanIndex = 0;
+                    return;
+                }
+            }
+            _currentSpan = memory.Span;
+            _currentSpanIndex = 0;
+            Advance(count - currentSegmentUnread);
+        }
+
+        public void Advance(Position position)
         {
             _currentSegmentPosition = position;
             _nextSegmentPosition = position;
-            if (_bytes.TryGet(ref _nextSegmentPosition, out _currentMemory))
+            if (_bytes.TryGet(ref _nextSegmentPosition, out ReadOnlyMemory<byte> memory))
             {
-                _currentSpan = _currentMemory.Span;
+                _currentSpan = memory.Span;
             }
             else
             {
                 _currentSpan = default;
             }
             _currentSpanIndex = 0;
-
         }
 
         #region Parsing Methods
         // TODO: how to we chose the lengths of the temp buffers?
         // TODO: these methods call the slow overloads of Parsers.Custom.TryParseXXX. Do we need fast path?
         // TODO: these methods hardcode the format. Do we need this to be something that can be specified?
-        public bool TryParseBoolean(out bool value)
+        public bool TryParse(out bool value)
         {
             var unread = Unread;
-            if (CustomParser.TryParseBoolean(unread, out value, out int consumed, _symbolTable))
+            if (Utf8Parser.TryParse(unread, out value, out int consumed))
             {
-                Debug.Assert(consumed <= unread.Length);
                 if (unread.Length > consumed)
                 {
                     _currentSpanIndex += consumed;
@@ -168,22 +174,48 @@ namespace System.Buffers.Text
                 }
             }
 
-            Span<byte> tempSpan = stackalloc byte[15];
-            CopyTo(this, tempSpan, out var copied);
+            Span<byte> tempSpan = stackalloc byte[5];
+            var copied = CopyTo(this, tempSpan);
 
-            if (CustomParser.TryParseBoolean(tempSpan.Slice(0, copied), out value, out consumed, _symbolTable))
+            if (Utf8Parser.TryParse(tempSpan.Slice(0, copied), out value, out consumed))
             {
-                Seek(consumed);
+                Advance(consumed);
                 return true;
             }
 
             return false;
         }
 
-        public bool TryParseUInt64(out ulong value)
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool TryParse(out int value)
         {
             var unread = Unread;
-            if (CustomParser.TryParseUInt64(unread, out value, out int consumed, default, _symbolTable))
+            if (Utf8Parser.TryParse(unread, out value, out int consumed))
+            {
+                if (unread.Length > consumed) {
+                    _currentSpanIndex += consumed;
+                    return true;
+                }
+            }
+            return TryParseStraddling(out value);
+        }
+
+        private bool TryParseStraddling(out int value)
+        {
+            Span<byte> tempSpan = stackalloc byte[15];
+            var copied = CopyTo(this, tempSpan);
+            if (Utf8Parser.TryParse(tempSpan.Slice(0, copied), out value, out int consumed))
+            {
+                Advance(consumed);
+                return true;
+            }
+            return false;
+        }
+
+        public bool TryParse(out ulong value)
+        {
+            var unread = Unread;
+            if (Utf8Parser.TryParse(unread, out value, out int consumed))
             {
                 if (unread.Length > consumed)
                 {
@@ -192,16 +224,47 @@ namespace System.Buffers.Text
                 }
             }
 
-            Span<byte> tempSpan = stackalloc byte[32];
-            CopyTo(this, tempSpan, out int copied);
-
-            if (CustomParser.TryParseUInt64(tempSpan.Slice(0, copied), out value, out consumed, 'G', _symbolTable))
+            Span<byte> tempSpan = stackalloc byte[30];
+            var copied = CopyTo(this, tempSpan);
+            if (Utf8Parser.TryParse(tempSpan.Slice(0, copied), out value, out consumed))
             {
-                Seek(consumed);
+                Advance(consumed);
                 return true;
             }
-
             return false;
+        }
+        #endregion
+
+        #region Binary Read APIs
+        public bool TryRead(out int value, bool littleEndian = false)
+        {
+            var unread = Unread;
+            if (littleEndian) {
+                if (BinaryPrimitives.TryReadInt32LittleEndian(unread, out value)) {
+                    Advance(sizeof(int));
+                    return true;
+                }
+            }
+            else if (BinaryPrimitives.TryReadInt32BigEndian(unread, out value)) {
+                Advance(sizeof(int));
+                return true;
+            }           
+
+            Span<byte> span = stackalloc byte[4];
+            var copied = CopyTo(this, span);
+            if (copied < 4) {
+                value = default;
+                return false;
+            }
+
+            if (littleEndian) {
+                value = BinaryPrimitives.ReadInt32LittleEndian(span);
+            }
+            else {
+                value = BinaryPrimitives.ReadInt32BigEndian(span);
+            }
+            Advance(sizeof(int));
+            return true;
         }
         #endregion
 
@@ -231,21 +294,20 @@ namespace System.Buffers.Text
             var nextPosition = _nextSegmentPosition;
             var currentPosition = _currentSegmentPosition;
             var previousPosition = _nextSegmentPosition;
-            var memory = _currentMemory;
-            while (_bytes.TryGet(ref _nextSegmentPosition, out _currentMemory))
+            while (_bytes.TryGet(ref _nextSegmentPosition, out ReadOnlyMemory<byte> memory))
             {
-                index = _currentMemory.Span.IndexOf(value);
+                var span = memory.Span;
+                index = span.IndexOf(value);
                 if (index != -1)
                 {
                     _currentSegmentPosition = previousPosition;
-                    _currentSpan = _currentMemory.Span;
+                    _currentSpan = span;
                     _currentSpanIndex = index + 1;
                     return _currentSegmentPosition + index;
                 }
                 previousPosition = _nextSegmentPosition;
             }
 
-            _currentMemory = memory;
             _nextSegmentPosition = nextPosition;
             _currentSegmentPosition = currentPosition;
             return Position.End;
@@ -276,25 +338,32 @@ namespace System.Buffers.Text
             throw new NotImplementedException();
         }
 
-        static void CopyTo(BytesReader<TSequence> bytes, Span<byte> buffer, out int copied)
+        static int CopyTo(BytesReader<TSequence> bytes, Span<byte> buffer)
         {
             var first = bytes.Unread;
             if (first.Length > buffer.Length)
             {
                 first.Slice(0, buffer.Length).CopyTo(buffer);
-                copied = buffer.Length;
+                return buffer.Length;
             }
             else if (first.Length == buffer.Length)
             {
                 first.CopyTo(buffer);
-                copied = buffer.Length;
+                return buffer.Length;
             }
             else
             {
                 first.CopyTo(buffer);
-                copied = first.Length;
-                copied += Sequence.Copy(bytes._bytes, bytes._nextSegmentPosition, buffer.Slice(copied));
+                return first.Length + Sequence.Copy(bytes._bytes, bytes._nextSegmentPosition, buffer.Slice(first.Length));
             }
+        }
+    }
+
+    public static class BytesReader
+    {
+        public static BytesReader<T> Create<T>(T sequence) where T : ISequence<ReadOnlyMemory<byte>>
+        {
+            return new BytesReader<T>(sequence);
         }
     }
 }
