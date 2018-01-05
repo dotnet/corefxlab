def1cd384c09c618370b71ec0c24ef588b085d7c
Pavel Krymets <pavel@krymets.com>
2018-01-04 17:48:41 -0800

    Extension methods and tests

---
+++ src/System.Buffers.Primitives/System/Buffers/Class1.cs
@@ -0,0 +1,19 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace System.Buffers
+{
+    public static class ReadOnlyBufferReaderExtensions
+    {
+        public static ReadOnlyBuffer SliceBefore(this BufferReader<ReadOnlyBuffer> reader)
+        {
+            return reader.Sequence.Slice(reader.Sequence.Start, reader.Position);
+        }
+
+        public static ReadOnlyBuffer SliceAfter(this BufferReader<ReadOnlyBuffer> reader)
+        {
+            return reader.Sequence.Slice(reader.Position);
+        }
+    }
+}
