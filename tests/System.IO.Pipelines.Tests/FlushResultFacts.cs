3460f1d19d7b66c625c72977344e6359d9b885fd
David Fowler <davidfowl@gmail.com>
2018-02-02 11:40:28 -0800

    Added tests

---
+++ tests/System.IO.Pipelines.Tests/FlushResultFacts.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace System.IO.Pipelines.Tests
+{
+    public class FlushResultFacts
+    {
+        [InlineData(true, true)]
+        [InlineData(true, false)]
+        [InlineData(false, true)]
+        [InlineData(false, false)]
+        [Theory]
+        public void FlushResultCanBeConstructed(bool cancelled, bool completed)
+        {
+            var result = new FlushResult(cancelled, completed);
+
+            Assert.Equal(cancelled, result.IsCancelled);
+            Assert.Equal(completed, result.IsCompleted);
+        }
+    }
+}
