0d94c3ae9e1c2bd29a9e125ae70aaf1137a6f85a
Krzysztof Cwalina <kcwalina@microsoft.com>
2018-02-13 09:23:55 -0800

    AzCopy-like sample using non-allocating APIs and pipelines (#2118)

---
+++ samples/AzCopyCore/AzCopyCore/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Azure.Authentication;
+using System.Azure.Storage;
+using System.Azure.Storage.Requests;
+using System.Buffers;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+
+static class Program
+{
+    internal static TraceSource Log = new TraceSource("AzCopyCore");
+
+    static void PrintUsage()
+    {
+        Console.WriteLine("dotnet AzCopyCore.dll /Source:<source> /Dest:<destination> [Options]");
+        Console.WriteLine("\tOptions:");
+        Console.WriteLine("\t - /DestKey:<key>");
+        Console.WriteLine("\t - /SourceKey:<key>");
+    }
+
+    static void Main(string[] args)
+    {
+        Log.Listeners.Add(new TextWriterTraceListener(Console.Out));
+        Log.Switch.Level = SourceLevels.Error;
+
+        var options = new CommandOptions(args);
+        ReadOnlyMemory<char> source = options.Get("/Source:");
+        ReadOnlyMemory<char> destination = options.Get("/Dest:");
+
+        // transfer from file system to storage
+        if (destination.Span.StartsWith("http://")) {
+            TransferDirectoryToStorage(source, destination, options);
+        }
+
+        // transfer from storage to file system
+        else if (source.Span.StartsWith("http://")) {
+            TransferStorageFileToDirectory(source, destination, options);
+        }
+
+        else { PrintUsage(); }
+
+        if (Debugger.IsAttached)
+        {
+            Console.WriteLine("Press ENTER to exit ...");
+            Console.ReadLine();
+        }
+    }
+
+    static void TransferDirectoryToStorage(ReadOnlyMemory<char> localDirectory, ReadOnlyMemory<char> storageDirectory, CommandOptions options)
+    {
+        var directoryPath = new string(localDirectory.Span);     
+        if (!Directory.Exists(directoryPath))
+        {
+            Console.WriteLine($"Source directory not found.");
+            return;
+        }
+        if (!options.Contains("/DestKey:"))
+        {
+            Console.WriteLine("/DestKey option not found.");
+            return;
+        }
+
+        ReadOnlyMemory<char> key = options.Get("/DestKey:");
+        byte[] keyBytes = key.Span.ComputeKeyBytes();
+
+        ReadOnlyMemory<char> storageFullPath = storageDirectory.Slice(7);
+        int pathStart = storageFullPath.Span.IndexOf('/');
+        ReadOnlyMemory<char> host = storageFullPath.Slice(0, pathStart);
+        ReadOnlyMemory<char> path = storageFullPath.Slice(pathStart + 1);
+        ReadOnlyMemory<char> account = storageFullPath.Slice(0, storageFullPath.Span.IndexOf('.'));
+
+        using (var client = new StorageClient(keyBytes, account, host))
+        {
+            client.Log = Log;           
+            foreach (var filepath in Directory.EnumerateFiles(directoryPath))
+            {
+                var filename = Path.GetFileName(filepath);
+                var storagePath = new string(path.Span) + "/" + filename;
+
+                // TODO (pri 3): this loop keeps going through all files, even if the key is wrong
+                if (CopyLocalFileToStorageFile(client, filepath, storagePath).Result)
+                {
+                    Console.WriteLine($"Uploaded {filepath} to {storagePath}");
+                }
+            }
+        }
+    }
+
+    static void TransferStorageFileToDirectory(ReadOnlyMemory<char> storageFile, ReadOnlyMemory<char> localDirectory, CommandOptions options)
+    {
+        var directory = new string(localDirectory.Span);
+        if (!options.Contains("/SourceKey:"))
+        {
+            Console.WriteLine("/SourceKey option not found.");
+            return;
+        }
+
+        ReadOnlyMemory<char> key = options.Get("/SourceKey:");
+        byte[] keyBytes = key.Span.ComputeKeyBytes();
+
+        ReadOnlyMemory<char> storageFullPath = storageFile.Slice(7);
+        int pathStart = storageFullPath.Span.IndexOf('/');
+        ReadOnlyMemory<char> host = storageFullPath.Slice(0, pathStart);
+        ReadOnlyMemory<char> storagePath = storageFullPath.Slice(pathStart + 1);
+        ReadOnlyMemory<char> account = storageFullPath.Slice(0, storageFullPath.Span.IndexOf('.'));
+        ReadOnlyMemory<char> file = storagePath.Slice(storagePath.Span.LastIndexOf('/') + 1);
+
+        // TODO (pri 3): use the new directory APIs once they become avaliable     
+        if (!Directory.Exists(directory))
+        {
+            Directory.CreateDirectory(directory);
+        }
+
+        string destinationPath = directory + "\\" + new string(file.Span);
+        using (var client = new StorageClient(keyBytes, account, host))
+        {
+            if(CopyStorageFileToLocalFile(client, new string(storagePath.Span), destinationPath).Result)
+            {
+                Console.WriteLine($"Downloaded {storagePath} to {destinationPath}");
+            }
+        }
+    }
+
+    static async ValueTask<bool> CopyLocalFileToStorageFile(StorageClient client, string localFilePath, string storagePath)
+    {
+        // TODO (pri 3): make file i/o more efficient
+        FileInfo fileInfo = new FileInfo(localFilePath);
+
+        var createRequest = new CreateFileRequest(storagePath, fileInfo.Length);
+        var response = await client.SendRequest(createRequest).ConfigureAwait(false);
+
+        if (response.StatusCode == 201)
+        {
+            using (var bytes = new FileStream(localFilePath, FileMode.Open, FileAccess.Read, FileShare.Read, 4096, true))
+            {
+                var putRequest = new PutRangeRequest(storagePath, bytes);
+                response = await client.SendRequest(putRequest).ConfigureAwait(false);
+                if (response.StatusCode == 201) return true;
+            }
+        }
+
+        Log.WriteError($"Response Error {response.StatusCode}");
+        return false;
+    }
+
+    static async ValueTask<bool> CopyStorageFileToLocalFile(StorageClient client, string storagePath, string localFilePath)
+    {
+        var request = new GetFileRequest(storagePath);
+        var response = await client.SendRequest(request).ConfigureAwait(false);
+
+        if (response.StatusCode != 200)
+        {
+            Log.WriteError($"Response Error {response.StatusCode}");
+            return false;
+        }
+
+        ulong bytesToRead = response.ContentLength;
+        using (var file = new FileStream(localFilePath, FileMode.Create, FileAccess.Write, FileShare.None, 4096, FileOptions.Asynchronous))
+        {
+            await file.WriteAsync(response.Body, bytesToRead);
+        }
+
+        return true;
+    }
+}
+
+
---
+++ samples/AzCopyCore/AzCopyCore/SocketClient.cs
@@ -0,0 +1,207 @@
+﻿using System.Buffers;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipelines;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Text.Http.Parser;
+using System.Threading.Tasks;
+
+// SocketClient is an experimental low-allocating/low-copy HTTP client API
+// TODO (pri 2): need to support cancellations
+namespace System.Net.Experimental
+{
+    public interface IResponse : IHttpHeadersHandler, IHttpResponseLineHandler {
+        void OnBody(PipeReader body);
+    }
+
+    public abstract class RequestWriter<T> where T : IPipeWritable
+    {
+        public abstract Text.Http.Parser.Http.Method Verb { get; }
+
+        public async Task WriteAsync(PipeWriter writer, T request)
+        {
+            WriteRequestLineAndHeaders(writer, ref request);
+            await WriteBody(writer, request).ConfigureAwait(false);
+            await writer.FlushAsync();
+        }
+
+        // TODO (pri 2): writing the request line should not be abstract; writing headers should.
+        protected abstract void WriteRequestLineAndHeaders(PipeWriter writer, ref T request);
+        protected virtual Task WriteBody(PipeWriter writer, T request) { return Task.CompletedTask; }
+    }
+
+    public struct SocketClient : IDisposable
+    {
+        readonly Pipe _requestPipe;
+        readonly Pipe _responsePipe;
+        readonly Socket _socket;
+        readonly Stream _stream;
+
+        // TODO (pri 3): would be nice to make this whole struct read-only
+        Task _responseReader;
+        Task _requestWriter;
+        public TraceSource Log;
+
+        SocketClient(Socket socket, SslStream stream)
+        {
+            _socket = socket;
+            _stream = stream;
+            _requestPipe = new Pipe();
+            _responsePipe = new Pipe();
+            _responseReader = null;
+            _requestWriter = null;
+            Log = null;
+        }
+
+        public static async Task<SocketClient> ConnectAsync(string host, int port, bool tls = false)
+        {
+            var socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+
+            // TODO (pri 3): all this TLS code is not tested
+            // TODO (pri 3): would be great to get flat APIs for TLS
+            SslStream tlsStream = null;
+            if (tls)
+            {
+                var networkStream = new NetworkStream(socket);
+                tlsStream = new SslStream(networkStream);
+                await tlsStream.AuthenticateAsClientAsync(host).ConfigureAwait(false);
+            }
+            else
+            {                    
+                await socket.ConnectAsync(host, port).ConfigureAwait(false);
+            }
+
+            var client = new SocketClient(socket, tlsStream);
+            client._responseReader = client.ReceiveAsync();
+            client._requestWriter = client.SendAsync();
+
+            return client;
+        }
+
+        public async ValueTask<TResponse> SendRequest<TRequest, TResponse>(TRequest request)
+            where TRequest : IPipeWritable
+            where TResponse : IResponse, new()
+        {
+            await request.WriteAsync(_requestPipe.Writer).ConfigureAwait(false);
+
+            var reader = _responsePipe.Reader;
+            var response = await HttpExtensions.ParseAsync<TResponse>(reader, Log).ConfigureAwait(false);
+            response.OnBody(reader);
+            return response;
+        }
+
+        async Task SendAsync()
+        {
+            var reader = _requestPipe.Reader;
+            try
+            {
+                while (true)
+                {
+                    var result = await reader.ReadAsync();
+                    var buffer = result.Buffer;
+
+                    try
+                    {
+                        if (!buffer.IsEmpty)
+                        {
+                            for (var position = buffer.Start; buffer.TryGet(ref position, out ReadOnlyMemory<byte> segment);)
+                            {
+                                await WriteToSocketAsync(segment).ConfigureAwait(false);
+                            }
+                        }
+                        else if (result.IsCompleted)
+                        {
+                            break;
+                        }
+                    }
+                    finally
+                    {
+                        reader.AdvanceTo(buffer.End);
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                Log.WriteError(e.ToString());
+            }
+            finally
+            {
+                reader.Complete();
+            }
+        }
+        
+        async Task ReceiveAsync()
+        {
+            var writer = _responsePipe.Writer;
+            try
+            {
+                while (true)
+                {
+                    // just wait for data in the socket
+                    await ReadFromSocketAsync(Memory<byte>.Empty);
+
+                    while (HasData)
+                    {
+                        var buffer = writer.GetMemory();
+                        var readBytes = await ReadFromSocketAsync(buffer).ConfigureAwait(false);
+                        if (readBytes == 0) break;
+
+                        if (Log != null) Log.WriteInformation($"RESPONSE {readBytes}", buffer.Slice(0, readBytes));
+
+                        writer.Advance(readBytes);
+                        await writer.FlushAsync();
+                    }
+                }
+            }
+            finally
+            {
+                writer.Complete();
+            }
+        }
+
+        async Task WriteToSocketAsync(ReadOnlyMemory<byte> buffer)
+        {
+            if (_stream != null)
+            {
+                await _stream.WriteAsync(buffer).ConfigureAwait(false);
+                await _stream.FlushAsync().ConfigureAwait(false);
+            }
+            else
+            {
+                await _socket.SendAsync(buffer, SocketFlags.None).ConfigureAwait(false);
+            }
+        }
+
+        async ValueTask<int> ReadFromSocketAsync(Memory<byte> buffer)
+        {
+            if (_stream != null)
+            {
+                return await _stream.ReadAsync(buffer).ConfigureAwait(false);
+            }
+            else
+            {
+                return await _socket.ReceiveAsync(buffer, SocketFlags.None).ConfigureAwait(false);
+            }
+        }
+        
+        bool HasData
+        {
+            get {
+                if (_stream != null) return _stream.Length != 0;
+                return _socket.Available != 0;
+            }
+        }
+
+        public void Dispose()
+        {
+            _stream?.Dispose();
+            _socket.Dispose();
+        }
+
+        public bool IsConnected => _socket != null;
+    }
+}
+
+
+
