c177745831ef8293ab35e44d21963a3183b546f0
Levi Broderick <levib@microsoft.com>
2018-03-21 15:25:29 -0700

    Initial commit of Utf8String surface area

---
+++ src/System.Text.Utf8/System/Text/UnicodeScalar.cs
@@ -0,0 +1,155 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.ComponentModel;
+
+namespace System.Text
+{
+    /// <summary>
+    /// Represents a Unicode scalar value.
+    /// </summary>
+    /// <remarks>
+    /// A Unicode scalar value is an unsigned integer in the range U+0000..U+D7FF, inclusive;
+    /// or within the range U+E000..U+10FFFF, inclusive.
+    /// </remarks>
+    public struct UnicodeScalar : IComparable<UnicodeScalar>, IEquatable<UnicodeScalar>
+    {
+        /// <summary>
+        /// Creates a <see cref="UnicodeScalar"/> from the provided UTF-16 code unit.
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// If <paramref name="ch"/> represents a UTF-16 surrogate code point
+        /// U+D800..U+DFFF, inclusive.
+        /// </exception>
+        public UnicodeScalar(char ch) => throw null;
+
+        /// <summary>
+        /// Creates a <see cref="UnicodeScalar"/> from the provided Unicode scalar value.
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// If <paramref name="scalarValue"/> does not represent a value Unicode scalar value.
+        /// </exception>
+        public UnicodeScalar(int scalarValue) { throw null; }
+
+        /// <summary>
+        /// Creates a <see cref="UnicodeScalar"/> from the provided Unicode scalar value.
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// If <paramref name="scalarValue"/> does not represent a value Unicode scalar value.
+        /// </exception>
+        public UnicodeScalar(uint scalarValue) { throw null; }
+
+        /// <summary>
+        /// Compares two <see cref="UnicodeScalar"/> instances for equality.
+        /// </summary>
+        public static bool operator ==(UnicodeScalar a, UnicodeScalar b) => throw null;
+
+        /// <summary>
+        /// Compares two <see cref="UnicodeScalar"/> instances for inequality.
+        /// </summary>
+        public static bool operator !=(UnicodeScalar a, UnicodeScalar b) => throw null;
+
+        /// <summary>
+        /// Returns true iff this scalar value is ASCII ([ U+0000..U+007F ])
+        /// and therefore representable by a single UTF-8 code unit.
+        /// </summary>
+        public bool IsAscii => throw null;
+
+        /// <summary>
+        /// Returns true iff this scalar value is within the BMP ([ U+0000..U+FFFF ])
+        /// and therefore representable by a single UTF-16 code unit.
+        /// </summary>
+        public bool IsBmp => throw null;
+
+        /// <summary>
+        /// A <see cref="UnicodeScalar"/> instance that represents the Unicode replacement character U+FFFD.
+        /// </summary>
+        public static UnicodeScalar ReplacementChar => throw null;
+
+        /// <summary>
+        /// Returns the length in code units (<see cref="Char"/>) of the
+        /// UTF-16 sequence required to represent this scalar value.
+        /// </summary>
+        /// <remarks>
+        /// The return value will be 1 or 2.
+        /// </remarks>
+        public int Utf16SequenceLength => throw null;
+
+        /// <summary>
+        /// Returns the length in code units (<see cref="Utf8Char"/>) of the
+        /// UTF-8 sequence required to represent this scalar value.
+        /// </summary>
+        /// <remarks>
+        /// The return value will be 1 through 4, inclusive.
+        /// </remarks>
+        public int Utf8SequenceLength => throw null;
+
+        /// <summary>
+        /// Returns the Unicode scalar value as an unsigned integer.
+        /// </summary>
+        public uint Value => throw null;
+
+        /// <summary>
+        /// Compares this <see cref="UnicodeScalar"/> instance to another <see cref="UnicodeScalar"/> instance.
+        /// </summary>
+        public int CompareTo(UnicodeScalar other) => throw null;
+
+        /// <summary>
+        /// Creates a <see cref="UnicodeScalar"/> from the provided Unicode scalar value
+        /// without validating the input value for well-formedness.
+        /// </summary>
+        /// <remarks>
+        /// The caller is expected to have validated that <paramref name="scalarValue"/> is
+        /// a valid value. The behavior of this type is undefined if the input value is invalid.
+        /// </remarks>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public static UnicodeScalar DangerousCreateWithoutValidation(uint scalarValue) => throw null;
+
+        /// <summary>
+        /// Returns true iff this <see cref="UnicodeScalar"/> instance is equal to the provided object.
+        /// </summary>
+        public override bool Equals(object obj) => throw null;
+
+        /// <summary>
+        /// Returns true iff this <see cref="UnicodeScalar"/> instance is equal to the provided <see cref="UnicodeScalar"/> instance.
+        /// </summary>
+        public bool Equals(UnicodeScalar other) => throw null;
+
+        /// <summary>
+        /// Returns a hash code for this <see cref="UnicodeScalar"/> instance suitable for use in a dictionary.
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode() => throw null;
+
+        /// <summary>
+        /// Returns a <see cref="String"/> representation of this <see cref="UnicodeScalar"/> instance.
+        /// </summary>
+        public override string ToString() => throw null;
+
+        /// <summary>
+        /// Writes this scalar value as a UTF-16 sequence to the output buffer, returning
+        /// the number of code units (<see cref="Char"/>) written.
+        /// </summary>
+        /// <exception cref="ArgumentException">
+        /// Thrown if <paramref name="output"/> is too short to contain the output.
+        /// The required length can be queried ahead of time via the <see cref="Utf16SequenceLength"/> property.
+        /// </exception>
+        public int ToUtf16(Span<char> output) => throw null;
+
+        /// <summary>
+        /// Writes this scalar value as a UTF-8 sequence to the output buffer, returning
+        /// the number of code units (<see cref="Utf8Char"/>) written.
+        /// </summary>
+        /// <exception cref="ArgumentException">
+        /// Thrown if <paramref name="output"/> is too short to contain the output.
+        /// The required length can be queried ahead of time via the <see cref="Utf8SequenceLength"/> property.
+        /// </exception>
+        public int ToUtf8(Span<Utf8Char> output) => throw null;
+
+        /// <summary>
+        /// Returns a <see cref="Utf8String"/> representation of this <see cref="UnicodeScalar"/> instance.
+        /// </summary>
+        public Utf8String ToUtf8String() => throw null;
+    }
+}
---
+++ src/System.Text.Utf8/System/Text/Utf8String.cs
@@ -0,0 +1,218 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Buffers;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace System.Text
+{
+    /// <summary>
+    /// A UTF-8 string.
+    /// </summary>
+    public sealed class Utf8String : IComparable, IComparable<Utf8String>, IConvertible, IEnumerable<Utf8Char>, IEquatable<Utf8String>
+    {
+        public Utf8String(ReadOnlySpan<byte> value) => throw null;
+
+        public Utf8String(ReadOnlySpan<char> value) => throw null;
+
+        public Utf8String(ReadOnlySpan<Utf8Char> value) => throw null;
+
+        public Utf8String(string value) => throw null;
+
+        public static bool operator ==(Utf8String a, Utf8String b) => throw null;
+
+        public static bool operator !=(Utf8String a, Utf8String b) => throw null;
+
+        public static implicit operator ReadOnlySpan<Utf8Char>(Utf8String value) => throw null;
+
+        public Utf8Char this[int index] => throw null;
+
+        public static readonly Utf8String Empty;
+
+        public int Length => throw null;
+
+        public int CompareTo(Utf8String other) => throw null;
+
+        public static Utf8String Concat(IEnumerable<Utf8String> values) => throw null;
+
+        public Utf8String Concat(Utf8String str0, Utf8String str1) => throw null;
+
+        public Utf8String Concat(Utf8String str0, Utf8String str1, Utf8String str2) => throw null;
+
+        public Utf8String Concat(Utf8String str0, Utf8String str1, Utf8String str3, Utf8String str4) => throw null;
+
+        public Utf8String Concat(params Utf8String[] values) => throw null;
+
+        public bool Contains(Utf8Char value) => throw null;
+
+        public bool Contains(Utf8String value) => throw null;
+
+        public static Utf8String Create<TState>(int length, TState state, SpanAction<Utf8Char, TState> action) => throw null;
+
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public static Utf8String DangerousCreateWithoutValidation<TState>(int length, TState state, SpanAction<Utf8Char, TState> action) => throw null;
+
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public static Utf8String DangerousCreateWithoutValidation(ReadOnlySpan<Utf8Char> value) => throw null;
+
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public ref readonly Utf8Char DangerousGetPinnableReference() => throw null;
+
+        public static bool EndsWith(Utf8Char value) => throw null;
+
+        public static bool EndsWith(Utf8String value) => throw null;
+
+        public override bool Equals(object other) => throw null;
+
+        public bool Equals(Utf8String other) => throw null;
+
+        public Utf8CharEnumerator GetEnumerator() => throw null;
+
+        public override int GetHashCode() => throw null;
+
+        public int IndexOf(Utf8Char value) => throw null;
+
+        public int IndexOf(Utf8Char value, int startIndex) => throw null;
+
+        public int IndexOf(Utf8Char value, int startIndex, int count) => throw null;
+
+        public int IndexOf(Utf8String value) => throw null;
+
+        public int IndexOf(Utf8String value, int startIndex) => throw null;
+
+        public int IndexOf(Utf8String value, int startIndex, int count) => throw null;
+
+        public int IndexOfAny(ReadOnlySpan<Utf8Char> value) => throw null;
+
+        public int IndexOfAny(ReadOnlySpan<Utf8Char> value, int startIndex) => throw null;
+
+        public int IndexOfAny(ReadOnlySpan<Utf8Char> value, int startIndex, int count) => throw null;
+
+        public Utf8String Insert(int startIndex, Utf8String value) => throw null;
+
+        public static bool IsNullOrEmpty(Utf8String value) => throw null;
+
+        public static bool IsNullOrWhiteSpace(Utf8String value) => throw null;
+
+        public int LastIndexOf(Utf8Char value) => throw null;
+
+        public int LastIndexOf(Utf8Char value, int startIndex) => throw null;
+
+        public int LastIndexOf(Utf8Char value, int startIndex, int count) => throw null;
+
+        public int LastIndexOf(Utf8String value) => throw null;
+
+        public int LastIndexOf(Utf8String value, int startIndex) => throw null;
+
+        public int LastIndexOf(Utf8String value, int startIndex, int count) => throw null;
+
+        public int LastIndexOfAny(ReadOnlySpan<Utf8Char> value) => throw null;
+
+        public int LastIndexOfAny(ReadOnlySpan<Utf8Char> value, int startIndex) => throw null;
+
+        public int LastIndexOfAny(ReadOnlySpan<Utf8Char> value, int startIndex, int count) => throw null;
+
+        public Utf8String PadLeft(int totalWidth) => throw null;
+
+        public Utf8String PadLeft(int totalWidth, Utf8Char paddingChar) => throw null;
+
+        public Utf8String PadRight(int totalWidth) => throw null;
+
+        public Utf8String PadRight(int totalWidth, Utf8Char paddingChar) => throw null;
+
+        public Utf8String Remove(int startIndex) => throw null;
+
+        public Utf8String Remove(int startIndex, int count) => throw null;
+
+        public Utf8String Replace(Utf8String oldValue, Utf8String newValue) => throw null;
+
+        public Utf8String[] Split(Utf8Char separator, int count, StringSplitOptions options = default) => throw null;
+
+        public Utf8String[] Split(Utf8Char separator, StringSplitOptions options = default) => throw null;
+
+        public Utf8String[] Split(Utf8Char[] separator, int count, StringSplitOptions options = default) => throw null;
+
+        public Utf8String[] Split(Utf8Char[] separator, StringSplitOptions options = default) => throw null;
+
+        public Utf8String[] Split(Utf8String separator, int count, StringSplitOptions options = default) => throw null;
+
+        public Utf8String[] Split(Utf8String separator, StringSplitOptions options = default) => throw null;
+
+        public Utf8String[] Split(Utf8String[] separator, int count, StringSplitOptions options = default) => throw null;
+
+        public Utf8String[] Split(Utf8String[] separator, StringSplitOptions options = default) => throw null;
+
+        public bool StartsWith(Utf8Char value) => throw null;
+
+        public bool StartsWith(Utf8String value) => throw null;
+
+        public Utf8String Substring(int startIndex) => throw null;
+
+        public Utf8String Substring(int startIndex, int length) => throw null;
+
+        public override string ToString() => throw null;
+
+        public string ToString(IFormatProvider provider) => throw null;
+
+        public Utf8String Trim() => throw null;
+
+        public Utf8String Trim(Utf8Char trimChar) => throw null;
+
+        public Utf8String Trim(Utf8Char[] trimChars) => throw null;
+
+        public Utf8String TrimEnd() => throw null;
+
+        public Utf8String TrimEnd(Utf8Char trimChar) => throw null;
+
+        public Utf8String TrimEnd(Utf8Char[] trimChars) => throw null;
+
+        public Utf8String TrimStart() => throw null;
+
+        public Utf8String TrimStart(Utf8Char trimChar) => throw null;
+
+        public Utf8String TrimStart(Utf8Char[] trimChars) => throw null;
+
+        TypeCode IConvertible.GetTypeCode() => throw null;
+
+        int IComparable.CompareTo(object obj) => throw null;
+
+        bool IConvertible.ToBoolean(IFormatProvider provider) => throw null;
+
+        byte IConvertible.ToByte(IFormatProvider provider) => throw null;
+
+        char IConvertible.ToChar(IFormatProvider provider) => throw null;
+
+        DateTime IConvertible.ToDateTime(IFormatProvider provider) => throw null;
+
+        decimal IConvertible.ToDecimal(IFormatProvider provider) => throw null;
+
+        double IConvertible.ToDouble(IFormatProvider provider) => throw null;
+
+        short IConvertible.ToInt16(IFormatProvider provider) => throw null;
+
+        int IConvertible.ToInt32(IFormatProvider provider) => throw null;
+
+        long IConvertible.ToInt64(IFormatProvider provider) => throw null;
+
+        sbyte IConvertible.ToSByte(IFormatProvider provider) => throw null;
+
+        float IConvertible.ToSingle(IFormatProvider provider) => throw null;
+
+        string IConvertible.ToString(IFormatProvider provider) => throw null;
+
+        object IConvertible.ToType(Type conversionType, IFormatProvider provider) => throw null;
+
+        ushort IConvertible.ToUInt16(IFormatProvider provider) => throw null;
+
+        uint IConvertible.ToUInt32(IFormatProvider provider) => throw null;
+
+        ulong IConvertible.ToUInt64(IFormatProvider provider) => throw null;
+
+        IEnumerator IEnumerable.GetEnumerator() => throw null;
+
+        IEnumerator<Utf8Char> IEnumerable<Utf8Char>.GetEnumerator() => throw null;
+    }
+}
