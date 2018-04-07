39cc528d95e5a9511032fef093b332ea5be44d19
Levi Broderick <levib@microsoft.com>
2018-04-07 15:20:16 -0700

    Optimize GetUtf8SequenceLength for multibyte sequences

--- src/System.Text.Utf8/System/Text/UnicodeHelpers.cs
+++ src/System.Text.Utf8/System/Text/UnicodeHelpers.cs
@@ -28,30 +28,77 @@ namespace System.Text
         }
 
         /// <summary>
-        /// Given a Unicode scalar value, gets the number of UTF-16 code units required to represent this value.
+        /// Given a Unicode scalar value, gets the number of UTF-8 code units required to represent this value.
         /// </summary>
         public static int GetUtf8SequenceLength(uint value)
         {
             Debug.Assert(IsValidUnicodeScalar(value));
 
-            // The logic below special-cases ASCII since it should be far and away
-            // the most common case. All other cases are handled together in a
-            // single branch. This tradeoff gives good performance on 1-byte, 3-byte,
-            // and 4-byte sequences, while sacrificing some performance in the 2-byte
-            // case. The alternative implementation commented out below gives good
-            // performance on multibyte sequences but is unacceptably slow for the
-            // common case of ASCII values.
+            // The logic below can handle all valid scalar values branchlessly.
+            // It gives good performance across all inputs. If the caller strongly
+            // suspects the input to be ASCII, the method GetUtf8SequenceLength_BiasForAscii
+            // has approx. 25% performance gain over this routine.
+
+            // The logic below works roughly as follows. Start with a default result of 4.
+            // If the scalar value is 1 or 2 UTF-8 code units, subtract 2 from the result.
+            // If the scalar value is 1 or 3 UTF-8 code units, subtract 1 from the result.
+            // Return the final result.
+
+            // 'a' will be -1 if input is < 0x800; else 'a' will be 0
+            // => 'a' will be -1 if input is 1 or 2 UTF-8 code units; else 'a' will be 0
+
+            uint a = (value - 0x0800U) >> 31;
+
+            // The number of UTF-8 code units for a given scalar is as follows:
+            // - U+0000..U+007F => 1 code unit
+            // - U+0080..U+07FF => 2 code units
+            // - U+0800..U+FFFF => 3 code units
+            // - U+10000+       => 4 code units
             //
-            // const ulong mask = 0b_001_001_001_001_001_001_001_001_010_010_010_010_011_011_011_011_011_100_100_100_100_100UL;
-            // return (int)(mask >> (int)(lzcnt(value) * 3 - 33)) & 7;
+            // If we XOR the incoming scalar with 0xF800, the chart mutates:
+            // - U+0000..U+F7FF => 3 code units
+            // - U+F800..U+F87F => 1 code unit
+            // - U+F880..U+FFFF => 2 code units
+            // - U+10000+       => 4 code units
+            //
+            // Since the 1- and 3-code unit cases are now clustered, they can
+            // both be checked together very cheaply.
+
+            value ^= 0xF800U;
+            value -= 0xF880U;   // if scalar is 1 or 3 code units, high byte = 0xFF; else high byte = 0x00
+            value += (4 << 24); // if scalar is 1 or 3 code units, high byte = 0x03; else high byte = 0x04
+            value >>= 24;       // shift high byte down
+
+            // Final return value:
+            // - U+0000..U+007F => 3 + (-1) * 2 = 1
+            // - U+0080..U+07FF => 4 + (-1) * 2 = 2
+            // - U+0800..U+FFFF => 3 + ( 0) * 2 = 3
+            // - U+10000+       => 4 + ( 0) * 2 = 4
+            return (int)(value + (a * 2));
+        }
+
+        /// <summary>
+        /// Given a Unicode scalar value, gets the number of UTF-8 code units required to represent this value.
+        /// This method is optimized for ASCII input values but can handle any valid scalar input.
+        /// </summary>
+        public static int GetUtf8SequenceLength_BiasForAscii(uint value)
+        {
+            Debug.Assert(IsValidUnicodeScalar(value));
+
+            // This method is similar to GetUtf8SequenceLength, but it optimizes for the case
+            // of ASCII input, sacrificing performance in the case of multi-byte UTF-8 sequences.
+
+            // If the caller thinks the input is likely to be ASCII, special-case it now.
 
             if (IsAsciiCodePoint(value))
             {
                 return 1;
             }
 
+            // Not ASCII.
+
             uint a = value - 0x10000U; // if input < 0x10000, high byte = 0xFF; else high byte = 0x00
-            a += 0x4000000U;           // if input < 0x10000, high byte = 0x03; else high byte = 0x04
+            a += (4 << 24);            // if input < 0x10000, high byte = 0x03; else high byte = 0x04
             a >>= 24;                  // shift high byte down; final value = 3 or 4
 
             int b = (int)value - 0x800; // if input < 0x800, high byte = 0xFF; else high byte = 0x00
