97f04801f2bf9f277e68bba4f77a768c68a9e374
MisinformedDNA <MisinformedDNA@users.noreply.github.com>
2018-04-20 16:49:07 -0500

    Port to bash

--- build.sh
+++ build.sh
@@ -85,7 +85,7 @@ fi
 
 echo "Building solution $myFile..."
 
-./$dotnetExePath build $myFile -c "$Configuration" /p:VersionSuffix="$BuildVersion"
+./$dotnetExePath build $myFile -c "$Configuration" /p:VersionSuffix="$BuildVersion" --no-restore
 ret=$?
 if [ $ret -ne 0 ]; then
   echo "Failed to build solution $myFile"
