c7c69d908aa9da8d05c6f43a5b7dbe04358bee4a
MisinformedDNA <MisinformedDNA@users.noreply.github.com>
2018-05-01 16:57:25 -0500

    Separate the files again

--- src/System.IO.FileSystem.Watcher.Polling/System/IO/PollingFileSystemEventArgs.cs
+++ src/System.IO.FileSystem.Watcher.Polling/System/IO/PollingFileSystemEventArgs.cs
@@ -3,8 +3,6 @@
 
 namespace System.IO.FileSystem
 {
-    public delegate void PollingFileSystemEventHandler(object sender, PollingFileSystemEventArgs e);
-
     public class PollingFileSystemEventArgs : EventArgs
     {
         public PollingFileSystemEventArgs(FileChange[] changes)
