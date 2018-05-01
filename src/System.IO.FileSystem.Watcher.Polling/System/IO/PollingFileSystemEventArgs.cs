cad97895e974ae6c53855e2cf648fd20a7917127
MisinformedDNA <MisinformedDNA@users.noreply.github.com>
2018-05-01 15:15:48 -0500

    Add license

--- src/System.IO.FileSystem.Watcher.Polling/System/IO/PollingFileSystemEventArgs.cs
+++ src/System.IO.FileSystem.Watcher.Polling/System/IO/PollingFileSystemEventArgs.cs
@@ -1,4 +1,7 @@
-﻿namespace System.IO.FileSystem
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace System.IO.FileSystem
 {
     public delegate void PollingFileSystemEventHandler(object sender, PollingFileSystemEventArgs e);
 
