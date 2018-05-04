dd1e9237d1a8183c582d5690aa7bd88237569034
Ahson Khan <ahkha@microsoft.com>
2018-05-04 15:56:57 -0700

    Add a script to easily clean the repository, on-demand.

---
+++ clean.cmd
@@ -0,0 +1,18 @@
+@echo off
+setlocal
+
+tasklist /FI "IMAGENAME eq dotnet.exe" 2>NUL | find /I /N "dotnet.exe">NUL
+if "%ERRORLEVEL%"=="0" (
+    echo Killing all instances of dotnet.exe that are running.
+    taskkill /F /IM dotnet.exe /T
+)
+
+:PROMPT
+SET /P AREYOUSURE=Are you sure you want to delete all unstaged files from the repo (Y/[N])? 
+IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
+
+echo Running "git clean -fdx".
+git clean -fdx
+
+:END
+endlocal
