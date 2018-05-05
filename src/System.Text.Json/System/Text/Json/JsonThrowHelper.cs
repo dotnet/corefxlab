388024d15c1a6faa51b55a8966c085c418d0afc2
Ahson Khan <ahkha@microsoft.com>
2018-05-05 02:27:08 -0700

    Optimize JsonReader by splitting the UTF-8 and UTF-16 implementations

---
+++ src/System.Text.Json/System/Text/Json/JsonThrowHelper.cs
@@ -0,0 +1,33 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Runtime.CompilerServices;
+
+namespace System.Text.Json
+{
+    internal static class JsonThrowHelper
+    {
+        public static void ThrowNotImplementedException()
+        {
+            throw GetNotImplementedException();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static NotImplementedException GetNotImplementedException()
+        {
+            return new NotImplementedException();
+        }
+
+        public static void ThrowJsonReaderException()
+        {
+            throw GetJsonReaderException();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static JsonReaderException GetJsonReaderException()
+        {
+            return new JsonReaderException();
+        }
+    }
+}
