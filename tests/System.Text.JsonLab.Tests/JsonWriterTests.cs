14edf52bcedb3922dff178dc1d8ca00757a55e76
Ahson Khan <ahkha@microsoft.com>
2018-05-07 08:46:59 -0700

    Adding JsonReader and Deserializer (from string) performance tests (#2265)
    * Adding JsonReader and Deserializer (from string) perf tests
    
    * Fixup System.Text.Json directory names to match new project name.
    
    * Fixup namespaces in System.Text.JsonLab tests

---
+++ tests/System.Text.JsonLab.Tests/JsonWriterTests.cs
@@ -0,0 +1,56 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Xunit;
+using System.Text.Formatting;
+using System.Buffers.Text;
+
+namespace System.Text.JsonLab.Tests
+{
+    public class JsonWriterTests
+    {
+        [Fact]
+        public void WriteJsonUtf8()
+        {
+            var formatter = new ArrayFormatter(1024, SymbolTable.InvariantUtf8);
+            var json = new JsonWriter(formatter, prettyPrint: true);
+            Write(ref json);
+
+            var formatted = formatter.Formatted;
+            var str = Encoding.UTF8.GetString(formatted.Array, formatted.Offset, formatted.Count);
+            Assert.Equal(expected, str.Replace("\r\n", "").Replace(" ", ""));
+        }
+
+        [Fact]
+        public void WriteJsonUtf16()
+        {
+            var formatter = new ArrayFormatter(1024, SymbolTable.InvariantUtf16);
+            var json = new JsonWriter(formatter, prettyPrint: false);
+            Write(ref json);
+
+            var formatted = formatter.Formatted;
+            var str = Encoding.Unicode.GetString(formatted.Array, formatted.Offset, formatted.Count);
+            Assert.Equal(expected, str.Replace(" ", ""));
+        }
+
+        static string expected = "{\"age\":30,\"first\":\"John\",\"last\":\"Smith\",\"phoneNumbers\":[\"425-000-1212\",\"425-000-1213\"],\"address\":{\"street\":\"1MicrosoftWay\",\"city\":\"Redmond\",\"zip\":98052}}";
+        static void Write(ref JsonWriter json)
+        {
+            json.WriteObjectStart();
+            json.WriteAttribute("age", 30);
+            json.WriteAttribute("first", "John");
+            json.WriteAttribute("last", "Smith");
+            json.WriteArrayStart("phoneNumbers");
+            json.WriteValue("425-000-1212");
+            json.WriteValue("425-000-1213");
+            json.WriteArrayEnd();
+            json.WriteObjectStart("address");
+            json.WriteAttribute("street", "1 Microsoft Way");
+            json.WriteAttribute("city", "Redmond");
+            json.WriteAttribute("zip", 98052);
+            json.WriteObjectEnd();
+            json.WriteObjectEnd();
+        }
+    }
+}
---
+++ tests/System.Text.JsonLab.Tests/PerfSmokeTests.cs
@@ -0,0 +1,108 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Buffers.Text;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text.JsonLab.Tests.Resources;
+using System.Runtime.InteropServices;
+using Xunit;
+
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
+
+namespace System.Text.JsonLab.Tests
+{
+    public class PerfSmokeTests
+    {
+        private static readonly Stopwatch Timer = new Stopwatch();
+
+        // Keep < 10 before checking in. Otherwise tests take over 2 seconds to execute.
+        private const int NumberOfIterations = 5;
+        private const int NumberOfSamples = 3;
+        private const int ExpectedMemoryBenchMark = 0;
+
+        // ReSharper disable once ConvertToConstant.Local
+        private static readonly bool OutputResults = true;
+
+        [Fact(Skip = "The tests are flaky and the GC sometimes reports allocations")]
+        public void ReadBasicJson()
+        {
+            Output("====== TEST ReadBasicJson ======");
+            ReadJsonHelper(TestJson.BasicJson); // Do not test first iteration
+            RunTest(TestJson.BasicJson);
+        }
+
+        [Fact(Skip = "The tests are flaky and the GC sometimes reports allocations")]
+        public void ReadProjectLockJson()
+        {
+            Output("====== TEST ReadProjectLockJson ======");
+            ReadJsonHelper(TestJson.ProjectLockJson); // Do not test first iteration
+            RunTest(TestJson.ProjectLockJson);
+        }
+
+        [Fact(Skip = "The tests are flaky and the GC sometimes reports allocations")]
+        public void ReadHeavyNestedJsonPerf()
+        {
+            Output("====== TEST ReadHeavyNestedJson ======");
+            ReadJsonHelper(TestJson.HeavyNestedJson); // Do not test first iteration
+            RunTest(TestJson.HeavyNestedJson);
+        }
+
+        private static void RunTest(string jsonStr)
+        {
+            var timeIterReadResults = new List<long>();
+            var memoryIterReadResults = new List<long>();
+
+            for (var j = 0; j < NumberOfSamples; j++)
+            {
+                GC.Collect(2);
+                var memoryBefore = GC.GetTotalMemory(true);
+                Timer.Restart();
+                for (var i = 0; i < NumberOfIterations; i++)
+                {
+                    ReadJsonHelper(jsonStr);
+                }
+                var time = Timer.ElapsedMilliseconds;
+                var memoryAfter = GC.GetTotalMemory(true);
+                var consumption = memoryAfter - memoryBefore;
+                timeIterReadResults.Add(time);
+                memoryIterReadResults.Add(consumption);
+                Assert.True(consumption <= ExpectedMemoryBenchMark, consumption.ToString());
+            }
+            Output(timeIterReadResults.Average().ToString(CultureInfo.InvariantCulture));
+            Output((memoryIterReadResults.Average()).ToString(CultureInfo.InvariantCulture));
+        }
+
+        private static void ReadJsonHelper(string jsonStr)
+        {
+            var reader = new JsonReader(MemoryMarshal.AsBytes(jsonStr.AsSpan()), SymbolTable.InvariantUtf16);
+            while (reader.Read())
+            {
+                var tokenType = reader.TokenType;
+                switch (tokenType)
+                {
+                    case JsonTokenType.StartObject:
+                    case JsonTokenType.EndObject:
+                    case JsonTokenType.StartArray:
+                    case JsonTokenType.EndArray:
+                        break;
+                    case JsonTokenType.PropertyName:
+                        break;
+                    case JsonTokenType.Value:
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+
+        private static void Output(string str)
+        {
+            if (!OutputResults) return;
+            Console.WriteLine(str);
+        }
+    }
+}
