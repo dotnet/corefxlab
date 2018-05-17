68f618058d33449af01f37cc586c0c861baf8d59
Levi Broderick <levib@microsoft.com>
2018-05-16 17:44:46 -0700

    Implement Utf8String.ToChar and ToBoolean

--- src/System.Text.Utf8/System/Text/Utf8String.Convertible..cs
+++ src/System.Text.Utf8/System/Text/Utf8String.Convertible..cs
@@ -2,13 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Buffers;
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Text.Utf8.Resources;
 
 namespace System.Text
 {
@@ -16,11 +11,98 @@ namespace System.Text
     {
         TypeCode IConvertible.GetTypeCode() => Type.GetTypeCode(typeof(Utf8String)); // simply bubble up what the runtime believes we are
 
-        bool IConvertible.ToBoolean(IFormatProvider provider) => throw null;
+        bool IConvertible.ToBoolean(IFormatProvider provider)
+        {
+            const uint TRUE_LITTLE_ENDIAN = 0x65757274U; // "uert"
+            const uint TRUE_BIG_ENDIAN = 0x74727565U; // "true"
+            const uint FALS_LITTLE_ENDIAN = 0x736C6166U; // "slaf"
+            const uint FALS_BIG_ENDIAN = 0x66616C73U; // "fals"
+
+            // This mimics Convert.ToBoolean(string, IFormatProvider).
+            // The data must equal "true" or "false" exactly, using an ordinal ignore case comparison.
+            // We also trim whitespace *and null* characters.
+
+            var data = Bytes;
+
+            // First, optimistically assume the data doesn't need to be trimmed, and try parsing directly.
+
+            // TODO: Use BinaryPrimitives.TryReadMachineEndian<T> when it's exposed via ref assemblies.
+
+            if (data.Length == 4)
+            {
+                uint dataAsDWord = Unsafe.ReadUnaligned<uint>(ref MemoryMarshal.GetReference(data)) | 0x20202020U; // normalize to lowercase
+                if (dataAsDWord == (BitConverter.IsLittleEndian ? TRUE_LITTLE_ENDIAN : TRUE_BIG_ENDIAN))
+                {
+                    return true;
+                }
+            }
+            else if (data.Length == 5)
+            {
+                uint dataAsDWord = Unsafe.ReadUnaligned<uint>(ref MemoryMarshal.GetReference(data)) | 0x20202020U; // normalize to lowercase
+                if (dataAsDWord == (BitConverter.IsLittleEndian ? FALS_LITTLE_ENDIAN : FALS_BIG_ENDIAN) && ((data[4] | 0x20U) == 0x65U /* "e" */))
+                {
+                    return false;
+                }
+            }
+
+            // Fast cases didn't match. Try trimming whitespace and nulls.
+            // Need to trim in a loop since whitespace and nulls could be nested.
+
+            if (data.Length <= 4)
+            {
+                goto Failure; // don't bother; nothing will match after whitespace removal
+            }
+
+            while (true)
+            {
+                int originalDataLength = data.Length;
+                data = Utf8TrimHelpers.TrimSingleScalar(Utf8TrimHelpers.TrimWhiteSpace(data, TrimType.Both), UnicodeScalar.DangerousCreateWithoutValidation(0) /* null */, TrimType.Both);
+                if (data.Length == originalDataLength)
+                {
+                    break; // no data was trimmed
+                }
+            }
+
+            // Try the checks again with the trimmed data.
+
+            if (data.Length == 4)
+            {
+                uint dataAsDWord = Unsafe.ReadUnaligned<uint>(ref MemoryMarshal.GetReference(data)) | 0x20202020U; // normalize to lowercase
+                if (dataAsDWord == (BitConverter.IsLittleEndian ? TRUE_LITTLE_ENDIAN : TRUE_BIG_ENDIAN))
+                {
+                    return true;
+                }
+            }
+            else if (data.Length == 5)
+            {
+                uint dataAsDWord = Unsafe.ReadUnaligned<uint>(ref MemoryMarshal.GetReference(data)) | 0x20202020U; // normalize to lowercase
+                if (dataAsDWord == (BitConverter.IsLittleEndian ? FALS_LITTLE_ENDIAN : FALS_BIG_ENDIAN) && ((data[4] | 0x20U) == 0x65U /* "e" */))
+                {
+                    return false;
+                }
+            }
+
+        Failure:
+
+            // No match
+
+            // TODO: What's an appropriate exception message?
+            throw new InvalidOperationException("This Utf8String does not represent a valid System.Boolean.");
+        }
 
         byte IConvertible.ToByte(IFormatProvider provider) => throw null;
 
-        char IConvertible.ToChar(IFormatProvider provider) => throw null;
+        char IConvertible.ToChar(IFormatProvider provider)
+        {
+            var (validity, scalar, numBytesConsumed) = UnicodeReader.PeekFirstScalar(MemoryMarshal.Cast<byte, Utf8Char>(Bytes));
+            if (validity != SequenceValidity.ValidSequence || numBytesConsumed != _length || !scalar.IsBmp)
+            {
+                // TODO: What's an appropriate exception message?
+                throw new InvalidOperationException("This Utf8String does not represent a single System.Char.");
+            }
+
+            return (char)scalar.Value;
+        }
 
         DateTime IConvertible.ToDateTime(IFormatProvider provider) => throw null;
 
