0918811ee8bbbb4fdbc17e23666ddbe60e3ea2e9
Krzysztof Cwalina <kcwalina@microsoft.com>
2018-06-05 14:02:13 -0700

    Added Stream Async Methods

--- src/System.Memory.Polyfill/System/Stream.cs
+++ src/System.Memory.Polyfill/System/Stream.cs
@@ -4,6 +4,9 @@
 
 using System.Buffers;
 using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace System
 {
@@ -25,6 +28,78 @@ namespace System
             finally
             {
                 if(pooled != null) ArrayPool<byte>.Shared.Return(pooled);
+            }
+#endif
+        }
+
+        public static async ValueTask<int> ReadAsync(this Stream stream, Memory<byte> buffer, CancellationToken cancellationToken = default)
+        {
+#if NETCOREAPP2_1
+            return await stream.ReadAsync(buffer, cancellationToken);
+#else
+            if (MemoryMarshal.TryGetArray(buffer, out ArraySegment<byte> array))
+            {
+                return await stream.ReadAsync(array.Array, array.Offset, array.Count, cancellationToken);
+            }
+            else
+            {
+                byte[] pooled = null;
+                try
+                {
+                    pooled = ArrayPool<byte>.Shared.Rent(buffer.Length);
+                    int read = await stream.ReadAsync(pooled, 0, pooled.Length, cancellationToken);
+                    pooled.AsSpan(0, read).CopyTo(buffer.Span);
+                    return read;
+                }
+                finally
+                {
+                    if (pooled != null) ArrayPool<byte>.Shared.Return(pooled);
+                }
+            }
+#endif
+        }
+
+        public static void Write(this Stream stream, ReadOnlySpan<byte> buffer)
+        {
+#if NETCOREAPP2_1
+            stream.Write(buffer);
+#else
+            byte[] pooled = null;
+            try
+            {
+                pooled = ArrayPool<byte>.Shared.Rent(buffer.Length);
+                buffer.CopyTo(pooled);
+                stream.Write(pooled, 0, pooled.Length);
+            }
+            finally
+            {
+                if(pooled != null) ArrayPool<byte>.Shared.Return(pooled);
+            }
+#endif
+        }
+
+        public static Task WriteAsync(this Stream stream, ReadOnlyMemory<byte> buffer, CancellationToken cancellationToken = default)
+        {
+#if NETCOREAPP2_1
+            return stream.WriteAsync(buffer, cancellationToken).AsTask();
+#else
+            if (MemoryMarshal.TryGetArray(buffer, out ArraySegment<byte> array))
+            {
+                return stream.WriteAsync(array.Array, array.Offset, array.Count, cancellationToken);
+            }
+            else
+            {
+                byte[] pooled = null;
+                try
+                {
+                    pooled = ArrayPool<byte>.Shared.Rent(buffer.Length);
+                    buffer.CopyTo(pooled);
+                    return stream.WriteAsync(pooled, 0, pooled.Length, cancellationToken);
+                }
+                finally
+                {
+                    if (pooled != null) ArrayPool<byte>.Shared.Return(pooled);
+                }
             }
 #endif
         }
--- tests/System.Memory.Polyfill.Tests/StreamTests.cs
+++ tests/System.Memory.Polyfill.Tests/StreamTests.cs
@@ -24,5 +24,56 @@ namespace System.Polyfill.Tests
                 Assert.Equal(i, span[i]);
             }
         }
+
+        [Fact]
+        public async void StreamReadAsync()
+        {
+            var buffer = new byte[100];
+            for (int i = 0; i < buffer.Length; i++) buffer[i] = (byte)i;
+            var stream = new MemoryStream(buffer);
+
+            buffer = new byte[100];
+            var memory = new Memory<byte>(buffer);
+            int read = await stream.ReadAsync(memory);
+
+            Assert.Equal(buffer.Length, read);
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                Assert.Equal(i, buffer[i]);
+            }
+        }
+
+        [Fact]
+        public void StreamWrite()
+        {
+            var span = new Span<byte>(new byte[100]);
+            for (int i = 0; i < span.Length; i++) span[i] = (byte)i;
+
+            var buffer = new byte[1000];
+            var stream = new MemoryStream(buffer);
+
+            stream.Write(span);
+            for (int i = 0; i < span.Length; i++)
+            {
+                Assert.Equal(i, buffer[i]);
+            }
+        }
+
+        [Fact]
+        public async void StreamWriteAsync()
+        {
+            var array = new byte[100];
+            var memory = new Memory<byte>(array);
+            for (int i = 0; i < array.Length; i++) array[i] = (byte)i;
+
+            var buffer = new byte[100];
+            var stream = new MemoryStream(buffer);
+            await stream.WriteAsync(memory);
+
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                Assert.Equal(i, buffer[i]);
+            }
+        }
     }
 }
