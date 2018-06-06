c3e90f6ac7fdd3bed2b46f31c2e8a2448bd0b4d0
Andrey Kurdyumov <kant2002@gmail.com>
2018-06-06 06:21:20 +0600

    Port Encoding (UTF-8/UTF-16/UTF-32) performance comparison test to BenchmarkDotNet (#2335)
    * Port Encoding (UTF-8/UTF-16/UTF-32) performance comparison test to BenchmarkDotNet
    Each conversion is made as separate test, to be able compare with baseline results using Encoding implementations
    
    * Address feedback
    - Explicitly specify accessibility
    - Use underscore for the private fields
    - Remove not needed fields
    - Create helper method for returning shared data
    
    * Address PR feedback
    - Change namespaces
    - Added license headers
    
    * Code formatting
    - Remove duplicate code
    - Replace usage of var with explicit type declaration

---
+++ tests/Benchmarks/System.Text.Primitives/CodePoint.cs
@@ -0,0 +1,45 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using BenchmarkDotNet.Code;
+
+namespace System.Text.Primitives.Benchmarks
+{
+    public class CodePoint : IParam
+    {
+        public CodePoint(int minCodePoint, int maxCodePoint, UtfEncoderHelper.SpecialTestCases special = UtfEncoderHelper.SpecialTestCases.None)
+        {
+            this.MinCodePoint = minCodePoint;
+            this.MaxCodePoint = maxCodePoint;
+            this.Special = special;
+        }
+
+        public object Value => this;
+
+        public string DisplayText
+        {
+            get
+            {
+                switch (Special)
+                {
+                    case UtfEncoderHelper.SpecialTestCases.None:
+                        return $"({MinCodePoint},{MaxCodePoint},None)";
+                    case UtfEncoderHelper.SpecialTestCases.AlternatingASCIIAndNonASCII:
+                        return $"({MinCodePoint},{MaxCodePoint},Alternating)";
+                    case UtfEncoderHelper.SpecialTestCases.MostlyASCIIAndSomeNonASCII:
+                        return $"({MinCodePoint},{MaxCodePoint},Mostly)";
+                    default:
+                        return $"({MinCodePoint},{MaxCodePoint},{Special})";
+                }
+            }
+        }
+
+        public int MinCodePoint { get; }
+        public int MaxCodePoint { get; }
+        public UtfEncoderHelper.SpecialTestCases Special { get; }
+
+        public string ToSourceCode() => $"new System.Text.Primitives.Benchmarks.CodePoint({MinCodePoint}, {MaxCodePoint}, System.Text.Primitives.Benchmarks.UtfEncoderHelper.SpecialTestCases.{Special})";
+
+    }
+}
