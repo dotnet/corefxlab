47ce583b35bb547bbaa0dd8489519711e2590c62
Krzysztof Cwalina <kcwalina@microsoft.com>
2018-06-07 11:13:57 -0700

    Completed Int32 APIs (#2353)

--- tests/System.Memory.Polyfill.Tests/Int32.cs
+++ tests/System.Memory.Polyfill.Tests/Int32.cs
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System.IO;
+using System.Globalization;
 using Xunit;
 
 namespace System.Polyfill.Tests
@@ -14,6 +14,34 @@ namespace System.Polyfill.Tests
             ReadOnlySpan<char> span = int.MaxValue.ToString().AsSpan();
             Assert.True(Int32Polyfill.TryParse(span, out int value));
             Assert.Equal(int.MaxValue, value);
+        }
+
+        [Fact]
+        public void Int32TryParseCulture()
+        {
+            var culture = CultureInfo.CreateSpecificCulture("pl-PL");
+            var formatted = (" " + int.MaxValue.ToString("N", culture));
+            Assert.True(Int32Polyfill.TryParse(formatted.AsSpan(), NumberStyles.AllowLeadingWhite | NumberStyles.AllowDecimalPoint | NumberStyles.AllowThousands, culture, out int value));
+            Assert.Equal(int.MaxValue, value);
+        }
+
+        [Fact]
+        public void Int32ParseCulture()
+        {
+            var culture = CultureInfo.CreateSpecificCulture("pl-PL");
+            var formatted = (" " + int.MaxValue.ToString("N", culture));
+            var value = Int32Polyfill.Parse(formatted.AsSpan(), NumberStyles.AllowLeadingWhite | NumberStyles.AllowDecimalPoint | NumberStyles.AllowThousands, culture);
+            Assert.Equal(int.MaxValue, value);
+        }
+
+        [Fact]
+        public void Int32TryFormat()
+        {
+            var culture = CultureInfo.CreateSpecificCulture("pl-PL");
+            var buffer = new char[100];
+            Assert.True(int.MinValue.TryFormat(buffer.AsSpan(), out int written, "N".AsSpan(), culture));
+            var result = buffer.AsSpan(0, written).ToString();
+            Assert.Equal(int.MinValue.ToString("N", culture), result);
         }
     }
 }
