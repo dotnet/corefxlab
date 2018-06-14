fbf1f2f82b402930fcb6482589739d18bc866de3
Andrey Kurdyumov <kant2002@gmail.com>
2018-06-14 22:07:16 +0300

    Migrate Base64 decoding benchmarks to Benchmark.DotNet (#2333)
    * Migrate Base64 decoding benchmarks to Benchmark.DotNet
    
    * Address PR feedback
    - Add explicit accessor
    - Use underscore for private fields
    - Change namespaces to conform with guidelines
    
    * Use helper classes from test project and remove not needed variables
    
    * Base64.Convert is not experimental API, so rename benchmark method

---
+++ tests/Benchmarks/System.Binary.Base64/Base64Encode.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Binary.Base64Experimental.Tests;
+using System.Buffers;
+using BenchmarkDotNet.Attributes;
+using Base64Encoder = System.Binary.Base64Experimental.Base64Experimental; // This name problematic, since Base64Experimental is part of namespace.
+
+namespace System.Binary.Base64.Benchmarks
+{
+    public class Base64Encode
+    {
+        [Params(10, 100, 1_000, 1_000_000)]
+        public int NumberOfBytes;
+        private static readonly StandardFormat format = new StandardFormat('M');
+
+        private static byte[] _source;
+        private static byte[] _bytesDestination;
+        private static char[] _charsDestination;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _source = new byte[NumberOfBytes];
+            Base64TestHelper.InitalizeBytes(_source);
+            _bytesDestination = new byte[Base64Encoder.GetMaxEncodedToUtf8Length(NumberOfBytes, format)];
+            _charsDestination = new char[Base64Encoder.GetMaxEncodedToUtf8Length(NumberOfBytes, format)];
+        }
+
+        [Benchmark]
+        public OperationStatus Base64ExperimentalWithLineBreaks()
+        {
+            return Base64Encoder.EncodeToUtf8(_source, _bytesDestination, out _, out _, format);
+        }
+
+        [Benchmark(Baseline = true)]
+        public int Base64ConvertWithLineBreaks()
+        {
+            return Convert.ToBase64CharArray(_source, 0, _source.Length, _charsDestination, 0, Base64FormattingOptions.InsertLineBreaks);
+        }
+    }
+}
---
+++ tests/Benchmarks/System.Binary.Base64/Stiching.cs
@@ -0,0 +1,68 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using BenchmarkDotNet.Attributes;
+using System.Binary.Base64Experimental.Tests;
+using Base64Decoder = System.Buffers.Text.Base64; // This name problematic, since Base64 is part of namespace.
+
+namespace System.Binary.Base64.Benchmarks
+{
+    public class Stiching
+    {
+        [Params(1_000, 5_000, 10_000, 20_000, 50_000)]
+        public int InputBufferSize;
+
+        private static byte[] _source;
+        private static byte[] _alignedSource1;
+        private static byte[] _alignedSource2;
+        private static byte[] _notAlignedSource1;
+        private static byte[] _notAlignedSource2;
+        private static byte[] _destination;
+        private static byte[] _expected;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _source = new byte[InputBufferSize];
+            Base64TestHelper.InitalizeDecodableBytes(_source);
+            _expected = new byte[InputBufferSize];
+            Base64Decoder.DecodeFromUtf8(_source, _expected, out int expectedConsumed, out int expectedWritten);
+
+            Base64TestHelper.SplitSourceIntoSpans(_source, false, out ReadOnlySpan<byte> alignedSource1, out ReadOnlySpan<byte> alignedSource2);
+            _alignedSource1 = alignedSource1.ToArray();
+            _alignedSource2 = alignedSource2.ToArray();
+
+            Base64TestHelper.SplitSourceIntoSpans(_source, false, out ReadOnlySpan<byte> notAlignedSource1, out ReadOnlySpan<byte> notAlignedSource2);
+            _notAlignedSource1 = notAlignedSource1.ToArray();
+            _notAlignedSource2 = notAlignedSource2.ToArray();
+
+            _destination = new byte[InputBufferSize]; // Plenty of space
+        }
+
+        [Benchmark(Baseline = true)]
+        public void NoStiching()
+        {
+            Base64TestHelper.DecodeNoNeedToStich(_alignedSource1, _alignedSource2, _destination, out _, out _);
+        }
+
+        [Benchmark]
+        [Arguments(10)]
+        [Arguments(32)]
+        [Arguments(50)]
+        [Arguments(64)]
+        [Arguments(100)]
+        [Arguments(500)]
+        public void StichingRequired(int stackSize)
+        {
+            Span<byte> stackSpan = stackalloc byte[stackSize];
+            Base64TestHelper.DecodeStichUsingStack(_notAlignedSource1, _notAlignedSource2, _destination, stackSpan, out _, out _);
+        }
+
+        [Benchmark]
+        public void StichingRequiredNoThirdCall()
+        {
+            Span<byte> stackSpan = stackalloc byte[600 * InputBufferSize / 1000];
+            Base64TestHelper.DecodeStichUsingStack(_notAlignedSource1, _notAlignedSource2, _destination, stackSpan, out _, out _);
+        }
+    }
+}
