94a10e88596616beb3bcace10a5e919520c31028
Edgardo Zoppi <edgardozoppi@users.noreply.github.com>
2018-07-10 14:32:58 -0700

    More progress related with Gpio events (#2389)
    * Adding WaitForEvent method to Gpio drivers.
    
    * Adding debounce timeout per pin.
    
    * Adding IsPinModeSupported method.
    
    * Small changes according to PR comments.
    
    * Adding GpioController class.
    
    * Adding EnableRaisingEvents to Pin class.
    Adding more samples.
    
    * Adding epoll system calls.
    
    * Adding event listeners, waiters, edge detection and debounce timeout in Unix driver using epoll system calls.

--- src/System.Devices.Gpio/System.Devices.Gpio/GpioDriver.cs
+++ src/System.Devices.Gpio/System.Devices.Gpio/GpioDriver.cs
@@ -3,17 +3,26 @@
 
 namespace System.Devices.Gpio
 {
-    public delegate void PinValueChangedEventHandler(GpioDriver driver, int bcmPinNumber);
+    public class PinValueChangedEventArgs : EventArgs
+    {
+        public PinValueChangedEventArgs(int bcmPinNumber) => BcmPinNumber = bcmPinNumber;
+
+        public int BcmPinNumber { get; }
+    }
 
     public abstract class GpioDriver : IDisposable
     {
-        public event PinValueChangedEventHandler PinValueChanged;
+        public event EventHandler<PinValueChangedEventArgs> ValueChanged;
 
         protected internal abstract int PinCount { get; }
 
-        protected internal abstract TimeSpan Debounce { get; set; }
-
         protected internal abstract int ConvertPinNumber(int pinNumber, PinNumberingScheme from, PinNumberingScheme to);
+
+        protected internal abstract bool IsPinModeSupported(PinMode mode);
+
+        protected internal abstract void OpenPin(int bcmPinNumber);
+
+        protected internal abstract void ClosePin(int bcmPinNumber);
 
         protected internal abstract void SetPinMode(int bcmPinNumber, PinMode mode);
 
@@ -23,15 +32,24 @@ namespace System.Devices.Gpio
 
         protected internal abstract PinValue Input(int bcmPinNumber);
 
-        protected internal abstract bool WasPinEventDetected(int bcmPinNumber);
+        protected internal abstract void SetDebounce(int bcmPinNumber, TimeSpan timeout);
 
-        protected internal abstract void SetPinEventsToDetect(int bcmPinNumber, PinEvent pinEvents);
+        protected internal abstract TimeSpan GetDebounce(int bcmPinNumber);
+
+        protected internal abstract void SetPinEventsToDetect(int bcmPinNumber, PinEvent events);
 
         protected internal abstract PinEvent GetPinEventsToDetect(int bcmPinNumber);
 
+        protected internal abstract void SetEnableRaisingPinEvents(int bcmPinNumber, bool enable);
+
+        protected internal abstract bool GetEnableRaisingPinEvents(int bcmPinNumber);
+
+        protected internal abstract bool WaitForPinEvent(int bcmPinNumber, TimeSpan timeout);
+
         protected internal void OnPinValueChanged(int bcmPinNumber)
         {
-            PinValueChanged?.Invoke(this, bcmPinNumber);
+            var e = new PinValueChangedEventArgs(bcmPinNumber);
+            ValueChanged?.Invoke(this, e);
         }
 
         public abstract void Dispose();
