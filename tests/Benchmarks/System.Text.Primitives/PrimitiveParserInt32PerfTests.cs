72e55ba698f3ab3ba9b794956d375682b361a801
fredeil <fredrik.eilertsen@gmail.com>
2018-09-15 09:36:08 +0200

    Add int32 baseline benchmark

--- tests/Benchmarks/System.Text.Primitives/PrimitiveParserInt32PerfTests.cs
+++ tests/Benchmarks/System.Text.Primitives/PrimitiveParserInt32PerfTests.cs
@@ -13,7 +13,16 @@ namespace System.Text.Primitives.Benchmarks
 {
     public partial class PrimitiveParserPerfTests
     {
-        public IEnumerable<object> ParseInt32Thai_Arguments()
+        public IEnumerable<object> ByteSpanToInt32_Arguments()		
+        {		
+            yield return new Utf8ByteArrayArgument("107374182");	
+            yield return new Utf8ByteArrayArgument("2147483647");	
+            yield return new Utf8ByteArrayArgument("0"); 		
+            yield return new Utf8ByteArrayArgument("000000000000000000003535abcdfg");		
+            yield return new Utf8ByteArrayArgument("21474abcdefghijklmnop");		
+        }
+
+        public IEnumerable<object> ParseInt32_Thai_Arguments()
         {
             yield return new Utf8ByteArrayArgument("๑๐๗๓๗๔๑๘๒");
             yield return new Utf8ByteArrayArgument("๒๑๔๗๔๘๓๖๔๗");
@@ -21,10 +30,15 @@ namespace System.Text.Primitives.Benchmarks
             yield return new Utf8ByteArrayArgument("๐๐๐๐๐๐๐๐๐๐๐๐๐๐๐๐๐๐๐๐๑๒๓๕abcdfg");
             yield return new Utf8ByteArrayArgument("๒๑๔๗๔abcdefghijklmnop");
         }
-        
+
+        [Benchmark(Baseline = true)]		
+        [ArgumentsSource(nameof(ByteSpanToInt32_Arguments))]		
+        public bool ByteSpanToInt32(Utf8ByteArrayArgument text) 
+            => Utf8Parser.TryParse(text.CreateSpan(), out int value, out int consumed);
+         
         [Benchmark]
-        [ArgumentsSource(nameof(ParseInt32Thai_Arguments))]
-        public bool ParseInt32Thai(Utf8ByteArrayArgument text) 
+        [ArgumentsSource(nameof(ParseInt32_Thai_Arguments))]
+        public bool ParseInt32_Thai(Utf8ByteArrayArgument text) 
             => CustomParser.TryParseInt32(text.CreateSpan(), out int value, out int bytesConsumed, 'G', TestHelper.ThaiTable);
     }
 }
