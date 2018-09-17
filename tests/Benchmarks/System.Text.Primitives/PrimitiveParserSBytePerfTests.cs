7a5409b01f128e905f8de00537574406c18cb966
Fredrik Eilertsen <fredeil@users.noreply.github.com>
2018-09-17 22:25:16 +0200

    Cleanup Utf8Parser tests (#2478)
    * Remove obsolete test
    
    * Remove redundant int16 perf tests
    
    * Remove redundant int32 perf tests
    
    * Remove redundant sbyte  perf tests
    
    * Remove redundant uint32 perf tests
    
    * Remove redundant uint64 perf tests
    
    * Add sbyte baseline benchmark
    
    * Add int16 baseline benchmark
    
    * Renamed arguments
    
    * Add int32 baseline benchmark
    
    * Match thai arguments with baseline arguments for in16
    
    * Match thai arguments with baseline arguments for sbyte
    
    * Fix thai translations

--- tests/Benchmarks/System.Text.Primitives/PrimitiveParserSBytePerfTests.cs
+++ tests/Benchmarks/System.Text.Primitives/PrimitiveParserSBytePerfTests.cs
@@ -13,59 +13,8 @@ namespace System.Text.Primitives.Benchmarks
 {
     public partial class PrimitiveParserPerfTests
     {
-        private static readonly string[] s_SByteTextArray = new string[17]
-        {
-           "95",
-            "2",
-            "112",
-            "-112",
-            "-21",
-            "-2",
-            "114",
-            "-114",
-            "-124",
-            "117",
-            "-117",
-            "-14",
-            "14",
-            "74",
-            "21",
-            "83",
-            "-127"
-        };
-        
-        public IEnumerable<object> SByte_BytesConsumed_VariableLength_Arguments()
-            => s_Int32TextArray.Select(text => new Utf8ByteArrayArgument(text));
 
-        [Benchmark]
-        [ArgumentsSource(nameof(SByte_BytesConsumed_VariableLength_Arguments))]
-        public bool PrimitiveParserByteSpanToSByte_BytesConsumed_VariableLength(Utf8ByteArrayArgument text)
-            => Utf8Parser.TryParse(text.CreateSpan(), out sbyte value, out int bytesConsumed);
-        
-        [Benchmark]
-        [ArgumentsSource(nameof(SByte_BytesConsumed_VariableLength_Arguments))]
-        public bool PrimitiveParserByteSpanToSByte_BytesConsumed_VariableLength_Baseline(Utf8ByteArrayArgument text)
-            => sbyte.TryParse(text.Text, out sbyte value);
-        
-        public IEnumerable<object> PrimitiveParserByteSpanToSByte_Arguments()
-        {
-            yield return new Utf8ByteArrayArgument("107"); // standard parse
-            yield return new Utf8ByteArrayArgument("127"); // max value
-            yield return new Utf8ByteArrayArgument("0"); 
-            yield return new Utf8ByteArrayArgument("-128"); // min value
-            yield return new Utf8ByteArrayArgument("-000000000000000000001235abcdfg");
-            yield return new Utf8ByteArrayArgument("-2147abcdefghijklmnop");
-        }
-
-        [Benchmark]
-        [ArgumentsSource(nameof(PrimitiveParserByteSpanToSByte_Arguments))]
-        public bool PrimitiveParserByteSpanToSByte_BytesConsumed(Utf8ByteArrayArgument text) => Utf8Parser.TryParse(text.CreateSpan(), out sbyte value, out int bytesConsumed);
-
-        [Benchmark]
-        [ArgumentsSource(nameof(PrimitiveParserByteSpanToSByte_Arguments))]
-        public bool PrimitiveParserByteSpanToSByte_BytesConsumed_Baseline(Utf8ByteArrayArgument text) => sbyte.TryParse(text.Text, out sbyte value);
-        
-        public IEnumerable<object> ParseSbyteThai_Arguments()
+        public IEnumerable<object> ByteSpanToSByte_Thai_Arguments()
         {
             yield return new Utf8ByteArrayArgument("๑๑๑");
             yield return new Utf8ByteArrayArgument("๑๒๔");
@@ -73,9 +22,25 @@ namespace System.Text.Primitives.Benchmarks
             yield return new Utf8ByteArrayArgument("๐๐๐๐๐๐๐๐๐๐๐๐๐๐๐๐๐๐๐๐๑๒๓๕abcdfg");
             yield return new Utf8ByteArrayArgument("๒๑๔๗๔abcdefghijklmnop");
         }
+
+        public IEnumerable<object> ByteSpanToSByte_Arguments()		
+        {		
+            yield return new Utf8ByteArrayArgument("111");	
+            yield return new Utf8ByteArrayArgument("124");		
+            yield return new Utf8ByteArrayArgument("2"); 		
+            yield return new Utf8ByteArrayArgument("000000000000000000003535abcdfg");		
+            yield return new Utf8ByteArrayArgument("21474abcdefghijklmnop");		
+        }
+
+        [Benchmark(Baseline = true)]
+        [ArgumentsSource(nameof(ByteSpanToSByte_Arguments))]
+        public bool ByteSpanToSByte(Utf8ByteArrayArgument text) 
+            => Utf8Parser.TryParse(text.CreateSpan(), out sbyte value, out int bytesConsumed);
+
         
         [Benchmark]
-        [ArgumentsSource(nameof(ParseSbyteThai_Arguments))]
-        public bool ParseSByteThai(Utf8ByteArrayArgument text) => CustomParser.TryParseSByte(text.CreateSpan(), out sbyte value, out int bytesConsumed, 'G', TestHelper.ThaiTable);
+        [ArgumentsSource(nameof(ByteSpanToSByte_Thai_Arguments))]
+        public bool ByteSpanToSByte_Thai(Utf8ByteArrayArgument text) 
+            => CustomParser.TryParseSByte(text.CreateSpan(), out sbyte value, out int bytesConsumed, 'G', TestHelper.ThaiTable);
     }
 }
