0a8f6fbb1bea3dfdddeb08c2ec00f383dd9d43b7
Greg Ingram <shaggygi97@gmail.com>
2018-09-20 21:08:18 -0400

    Added initial System.Devices.I2c project

---
+++ src/System.Devices.I2c/Provider/ProviderI2cConnectionSettings.cs
@@ -0,0 +1,33 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace System.Devices.I2c.Provider
+{
+    /// <summary>
+    /// Represents the connection settings you want to use for an inter-integrated circuit
+    /// (I²C) device.
+    /// </summary>
+    public sealed class ProviderI2cConnectionSettings : IProviderI2cConnectionSettings
+    {
+        /// <summary>
+        /// Gets or sets the bus address of the inter-integrated circuit (I²C) device.
+        /// </summary> 
+        public int SlaveAddress { get; set; }
+
+        /// <summary>
+        /// Gets or sets the sharing mode to use to connect to the inter-integrated circuit
+        /// (I²C) bus address. This mode determines whether other connections
+        /// to the I²C bus address can be opened while you are connect to the
+        /// I²C bus address.
+        /// </summary> 
+        public ProviderI2cSharingMode SharingMode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the bus speed to use for connecting to an inter-integrated circuit
+        /// (I²C) device. The bus speed is the frequency at which to clock the
+        /// I²C bus when accessing the device.
+        /// </summary>
+        public ProviderI2cBusSpeed BusSpeed { get; set; }
+    }
+}
---
+++ src/System.Devices.I2c/Provider/ProviderI2cTransferStatus.cs
@@ -0,0 +1,31 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace System.Devices.I2c.Provider
+{
+    /// <summary>
+    /// Describes whether the data transfers that the ReadPartial, WritePartial, or WriteReadPartial
+    /// method performed succeeded, or provides the reason that the transfers did not
+    /// succeed.
+    /// </summary>
+    public enum ProviderI2cTransferStatus
+    {
+        /// <summary>
+        /// The data was entirely transferred. For WriteReadPartial, the data for both the
+        /// write and the read operations was entirely transferred.
+        /// </summary>
+        FullTransfer = 0,
+
+        /// <summary>
+        /// The I²C device negatively acknowledged the data transfer before all
+        /// of the data was transferred.
+        /// </summary>
+        PartialTransfer = 1,
+
+        /// <summary>
+        /// The bus address was not acknowledged.
+        /// </summary>
+        SlaveAddressNotAcknowledged = 2
+    }
+}
