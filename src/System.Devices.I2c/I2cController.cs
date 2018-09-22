03e75781c9b5eb13295fab01ea9fa2551ef4ebff
Greg Ingram <shaggygi97@gmail.com>
2018-09-21 20:22:32 -0400

    Changed interfaces to abstract classes and documented

---
+++ src/System.Devices.I2c/I2cController.cs
@@ -0,0 +1,23 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace System.Devices.I2c
+{
+    /// <summary>
+    /// Represents the I²C controller for the system.
+    /// </summary>
+    public abstract class I2cController
+    {
+        /// <summary>
+        /// Gets the I²C device with the specified settings.
+        /// </summary>
+        /// <param name="settings">
+        /// The desired I²C connection settings.
+        /// </param>
+        /// <returns>
+        /// The I²C device.
+        /// </returns>
+        public abstract I2cDevice GetDevice(I2cConnectionSettings settings);
+    }
+}
