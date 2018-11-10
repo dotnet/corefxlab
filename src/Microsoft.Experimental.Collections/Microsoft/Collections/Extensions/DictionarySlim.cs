7a2b09bb8efecd98a07b3bc2abfebfb6993f5742
Dan Moseley <danmose@microsoft.com>
2018-11-10 08:57:40 -0800

    Enable errors in HashHelpers

--- src/Microsoft.Experimental.Collections/Microsoft/Collections/Extensions/DictionarySlim.cs
+++ src/Microsoft.Experimental.Collections/Microsoft/Collections/Extensions/DictionarySlim.cs
@@ -496,8 +496,8 @@ namespace System.Collections
 
         public static int GetPrime(int min)
         {
-            //if (min < 0)
-            //    throw new ArgumentException(SR.Arg_HTCapacityOverflow);
+            if (min < 0)
+                throw new ArgumentException(Strings.Arg_HTCapacityOverflow);
 
             for (int i = 0; i < primes.Length; i++)
             {
@@ -523,11 +523,11 @@ namespace System.Collections
 
             // Allow the hashtables to grow to maximum possible size (~2G elements) before encountering capacity overflow.
             // Note that this check works even when _items.Length overflowed thanks to the (uint) cast
-            //if ((uint)newSize > MaxPrimeArrayLength && MaxPrimeArrayLength > oldSize)
-            //{
-            //    Debug.Assert(MaxPrimeArrayLength == GetPrime(MaxPrimeArrayLength), "Invalid MaxPrimeArrayLength");
-            //    return MaxPrimeArrayLength;
-            //}
+            if ((uint)newSize > MaxPrimeArrayLength && MaxPrimeArrayLength > oldSize)
+            {
+                Debug.Assert(MaxPrimeArrayLength == GetPrime(MaxPrimeArrayLength), "Invalid MaxPrimeArrayLength");
+                return MaxPrimeArrayLength;
+            }
 
             return GetPrime(newSize);
         }
