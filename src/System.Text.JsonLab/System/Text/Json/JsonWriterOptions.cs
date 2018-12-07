a58dafa7db324f08f3466be7ea1721c3c02bffb0
Ahson Khan <ahkha@microsoft.com>
2018-12-06 17:11:08 -0800

    Split up types into separate files.

---
+++ src/System.Text.JsonLab/System/Text/Json/JsonWriterOptions.cs
@@ -0,0 +1,16 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace System.Text.JsonLab
+{
+    public struct JsonWriterOptions
+    {
+        // Or Minified, Minimzed, PrettyPrinted,
+        public bool Formatted { get; set; }
+
+        public bool SkipValidation { get; set; }
+
+        internal bool SlowPath => Formatted || !SkipValidation;
+    }
+}
