cd9c4ba2c9fa7b11523d253f6c93f5860e6b4b71
Gordon Phoon <t-gopho@microsoft.com>
2019-06-20 10:07:44 -0700

    Added basics for ServerObject

--- src/ALCProxy/CommObjects.cs
+++ src/ALCProxy/CommObjects.cs
@@ -1,14 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Reflection;
-using System.Text;
 
 namespace ALCProxy
 {
     interface IServerObject
     {
-        //void ReceiveMethod(MethodInfo method, object[] args);
         void ReturnResult(Type t, object instance);
+        object CallObject(MethodInfo method, object[] args);
     }
 
     interface IClientObject
@@ -18,7 +16,8 @@ namespace ALCProxy
 
     public class ClientObject : IClientObject
     {
-
+        //Can't make this an IServerObject directly due to the type-isolation barrier
+        private object _server;
         public ClientObject()
         {
 
@@ -27,7 +26,29 @@ namespace ALCProxy
         public object SendMethod(MethodInfo method, object[] args)
         {
             Console.WriteLine("Sending method!");
+            //return typeof(ServerObject).GetMethod("CallObject").Invoke(server, new object[] { method, args });
             return null;
         }
+    }
+
+    //Need to build DispatchBuilder to construct this seperately.
+    public class ServerObject : IServerObject
+    {
+        private object _instance;
+        public ServerObject(object instance)
+        {
+            _instance = instance;
+        }
+
+        public object CallObject(MethodInfo method, object[] args)
+        {
+            return _instance.GetType().GetMethod(method.Name).Invoke(_instance, args);
+        }
+
+        public void ReturnResult(Type t, object instance)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
