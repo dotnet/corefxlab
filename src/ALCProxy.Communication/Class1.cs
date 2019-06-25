077b45d742b76a20efdfa3d80d0ee2038ab16787
Gordon Phoon <t-gopho@microsoft.com>
2019-06-25 13:29:14 -0700

    Working version of DispatchProxy, but we may be able to simplify this further

---
+++ src/ALCProxy.Communication/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace ALCProxy.Communication
+{
+    public class ExampleDispatch<I> : DispatchProxy
+    {
+        public object instance;
+        internal static I Create<Dispatch>(Type type)
+        {
+            object proxy = Create<I, ExampleDispatch<I>>(); //Error throws here
+            ((ExampleDispatch<I>)proxy).SetParameters(type, new Type[] { }, new object[] { });
+            return (I)proxy;
+        }
+
+        private void SetParameters(Type instanceType, Type[] argTypes, object[] constructorArgs)
+        {
+            instance = instanceType.GetConstructor(argTypes).Invoke(constructorArgs);
+        }
+
+        protected override object Invoke(MethodInfo targetMethod, object[] args)
+        {
+            return targetMethod.Invoke(instance, args);
+        }
+    }
+}
--- src/ALCProxy.Communication/CommObjects.cs
+++ src/ALCProxy.Communication/CommObjects.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
@@ -35,7 +36,7 @@ namespace ALCProxy
 
         public static void Main()
         {
-
+            
         }
 
         private Type FindType(string typeName, Assembly a)
@@ -70,6 +71,11 @@ namespace ALCProxy
             // object instance = Activator.CreateInstance(t);
 
             //Load this assembly in so we can get the server into the ALC
+
+            Assembly aaa = alc.LoadFromAssemblyPath(Assembly.GetAssembly(typeof(DispatchProxy)).CodeBase.Substring(8));
+            //aaa = alc.LoadFromAssemblyPath(Assembly.GetAssembly(typeof(System.Activator)).CodeBase.Substring(8));
+
+
             Assembly aa = alc.LoadFromAssemblyPath(Assembly.GetAssembly(typeof(ClientObject)).CodeBase.Substring(8));
             Type serverType = FindType("ServerDispatch`1", aa);
             //Set up all the generics to allow for the serverDispatch to be created correctly
@@ -78,8 +84,6 @@ namespace ALCProxy
 
             object s = Activator.CreateInstance(constructedType);
 
-            var mm = constructedType.GetMethods();
-            // MethodInfo m = constructedType.GetMethod("Create`1");
             MethodInfo m = constructedType.GetMethod(
                 "Create",
                 BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy, null, new Type[] { typeof(Type) }, null);
@@ -87,19 +91,14 @@ namespace ALCProxy
             _server = m.Invoke(s, new object[] { objType });
 
 
-            // aa.GetType("ServerObject");
-
-
-
-            //TODO: Actually get the server into the ALC because while this does work, it's currently just the exact same stuff as it was before but with even more reflection.
-            //_server = (IServerObject)ServerDispatch<IServerObject>.Create<ServerDispatch<IServerObject>>(t);
         }
 
         public object SendMethod(MethodInfo method, object[] args)
         {
 
-            MethodInfo m = _server.GetType().GetMethod("CallObject");
-            return m.Invoke(_server, new object[] { method, args });
+            //MethodInfo m = _server.GetType().GetMethod("CallObject");
+            //return m.Invoke(_server, new object[] { method, args });
+            return method.Invoke(_server, args);
             //MethodInfo m = _server.GetType().GetMethod(((MethodInfo)args[0]).Name);
             //return m.Invoke(_server, (object[])args[1]);
 
@@ -117,212 +116,30 @@ namespace ALCProxy
     /// </summary>
     /// <typeparam name="I"></typeparam>
 
-    //public class ServerDispatch<I> : DispatchProxy
-    //{
-    //    public object instance;
-    //    internal static I Create<Dispatch>(Type type)
-    //    {
-    //        Console.WriteLine("Hello from somewhere in the middle of nowhere!");
-    //        var a = Assembly.GetExecutingAssembly();
-    //        Console.WriteLine(AssemblyLoadContext.GetLoadContext(a).Name);
-    //        Console.WriteLine(a.IsCollectible);
-    //        using (AssemblyLoadContext.GetLoadContext(a).EnterContextualReflection())
-    //        {            //TODO System.NotSupportedException: 'A non-collectible assembly may not reference a collectible assembly.'
-    //            Console.WriteLine(AssemblyLoadContext.GetLoadContext(Assembly.GetExecutingAssembly()).IsCollectible);
-    //            Console.WriteLine(typeof(ServerDispatch<I>).IsCollectible);
-    //            object proxy = ServerDispatch<I>.Create<I, ServerDispatch<I>>();
-    //            ((ServerDispatch<I>)proxy).SetParameters(type, new Type[] { }, new object[] { });
-    //            return (I)proxy;
-    //        }
-    //        //return (I)proxy;
-    //    }
-
-    //    internal new static I Create<I, Dispatch>()
-    //    {
-    //        return DispatchProxy.Create<I, ServerDispatch<I>>();
-    //    }
-
-    //    private void SetParameters(Type instanceType, Type[] argTypes, object[] constructorArgs)
-    //    {
-    //        instance = instanceType.GetConstructor(argTypes).Invoke(constructorArgs);
-    //    }
-
-    //    protected override object Invoke(MethodInfo targetMethod, object[] args)
-    //    {
-    //        //The first arg is the methodinfo, the second is the list of args
-    //        MethodInfo m = instance.GetType().GetMethod(((MethodInfo)args[0]).Name);
-    //        return m.Invoke(instance, (object[])args[1]);
-    //    }
-    //}
-
-    //I is the interface type, while the passed in type is the type of the object we want to create
-    public class ServerDispatch<I> : IServerObject
+    public class ServerDispatch<I> : ALCProxy.Communication.DispatchProxy, IServerObject
     {
-
-
         public object instance;
-
-        void InstantiateObject(Type classType)
+        internal static I Create<Dispatch>(Type type)
         {
-            //Creates the proxied object and saves it so we can slot it into the proxy later
-            instance = (classType.GetConstructor(Type.EmptyTypes).Invoke(new object[] { }));
-
+            object proxy = Create<I, ServerDispatch<I>>();
+            ((ServerDispatch<I>)proxy).SetParameters(type, new Type[] { }, new object[] { });
+            return (I)proxy;
         }
 
-
-        internal static IServerObject Create<Dispatch>(Type type)
+        private void SetParameters(Type instanceType, Type[] argTypes, object[] constructorArgs)
         {
-            ServerDispatch<I> proxy = new ServerDispatch<I>();
-            proxy.InstantiateObject(type);
-
-
-
-            return proxy;
-
-            //return (I)proxy;
+            instance = instanceType.GetConstructor(argTypes).Invoke(constructorArgs);
         }
 
-        public object CreateObject(Type classType)
+        protected override object Invoke(MethodInfo targetMethod, object[] args)
         {
-            //Creates the targeted object that we're trying to proxy
-            InstantiateObject(classType);
-
-            //Generate a name for our new assembly
-            var aName = Guid.NewGuid().ToString();
-
-            //Build the assembly and type
-            AssemblyBuilder ab = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName(aName),
-                AssemblyBuilderAccess.RunAndCollect);
-            ModuleBuilder mb =
-                ab.DefineDynamicModule(aName);
-            TypeBuilder tb = mb.DefineType(
-                aName,
-                TypeAttributes.Public |
-                TypeAttributes.Class |
-                TypeAttributes.AnsiClass |
-                TypeAttributes.BeforeFieldInit,
-                typeof(object),
-                new Type[] { typeof(I) });
-
-
-            //Basic constructor that instantiates the object, doesn't do anything here but we'll insert stuff into the fields later
-            ConstructorBuilder cb = tb.DefineDefaultConstructor(
-                MethodAttributes.Public |
-                MethodAttributes.SpecialName |
-                MethodAttributes.RTSpecialName);
-
-            //Creates the field in the class, but we don't set the field until after the proxy is instantiated
-            FieldBuilder fb = tb.DefineField("InstanceToCall", typeof(object), FieldAttributes.Public | FieldAttributes.RTSpecialName);
-            //Field that holds the type of the object, serves to increase performance by not having to call it every time in the methods
-            FieldBuilder fb2 = tb.DefineField("InstanceType", typeof(Type), FieldAttributes.Public | FieldAttributes.RTSpecialName);
-
-            //Build Methods
-            MethodInfo[] methods = classType.GetMethods();
-
-            //This stuff isn't being used yet, but it could be useful later on
-            //IEnumerable<MethodInfo> methodsToImplement = GetMethodsToImplement(classType);
-            //IEnumerable<string> methodNames =
-            //    methodsToImplement.Select(x => ((MethodInfo)x).Name);
-
-            //Run through each method, create, and insert the IL
-            foreach (MethodInfo m in methods)
-            {
-                MethodBody body = m.GetMethodBody();
-                if (m.IsStatic) //TODO make this available to more method types
-                {
-                    continue;
-                }
-
-                //TODO actually get args working
-                IEnumerable<ParameterInfo> methodArgs = m.GetParameters();
-
-                Type[] methodTypes =
-                    methodArgs.Select(x => x.ParameterType).ToArray<Type>();
-                int numParams = methodTypes.Length;
-
-
-                //To Work on for getting parameters and return types going
-                foreach (Type ty in methodTypes)
-                {
-
-                }
-
-
-                MethodBuilder callMethod = tb.DefineMethod(
-                    m.Name,
-                    m.Attributes,
-                    m.ReturnType,
-                    methodTypes); //TODO needs to change later to allow for parameters
-
-                ILGenerator il = callMethod.GetILGenerator();
-
-                SetupMethodIL(il, fb, fb2, m.Name, numParams);
-            }
-
-            //Creates the type now that the methods are set up
-            Type t = tb.CreateType();
-            ConstructorInfo c = t.GetConstructor(Type.EmptyTypes);
-
-            //Instantiate the proxy, so we can slot the object into the field
-            //TODO remove the prog requirements
-            object obj = c.Invoke(new object[] { });
-
-            //slotting object and type here...
-            t.InvokeMember("InstanceToCall", BindingFlags.DeclaredOnly |
-            BindingFlags.Public | BindingFlags.NonPublic |
-            BindingFlags.Instance | BindingFlags.SetField, null, obj, new object[] { instance });
-            t.InvokeMember("InstanceType", BindingFlags.DeclaredOnly |
-            BindingFlags.Public | BindingFlags.NonPublic |
-            BindingFlags.Instance | BindingFlags.SetField, null, obj, new object[] { instance.GetType() });
-
-            //and we're done!
-            return obj;
-        }
-
-        private void SetupMethodIL(ILGenerator il, FieldBuilder objField, FieldBuilder typeField, string methodName, int numParams)
-
-        //We Get the 
-        {
-            //Currently calls obj.GetType().GetMethod(methodName).Invoke(obj, args)
-            //TODO simplify this to reduce the amount of reflection needed at runtime of the proxy
-
-            il.Emit(OpCodes.Nop);
-            il.Emit(OpCodes.Ldarg_0);
-            //il.Emit(OpCodes.Ldfld, fb);
-            //il.EmitCall(OpCodes.Callvirt, typeof(object).GetMethod("GetType"), null);
-            il.Emit(OpCodes.Ldfld, typeField);
-            il.Emit(OpCodes.Ldstr, methodName);
-            il.EmitCall(OpCodes.Callvirt, typeof(Type).GetMethod("GetMethod", new Type[] { typeof(string) }), null);
-            il.Emit(OpCodes.Ldarg_0);
-            il.Emit(OpCodes.Ldfld, objField); //The instance of the object that is being called
-
-            //TODO swap null args with the list of given parameters
-
-            //il.Emit(OpCodes.ldc_I)
-
-            il.Emit(OpCodes.Ldnull);
-            //il.Emit(OpCodes.Ldarg_0);
-            //il.Emit(OpCodes.Ldc_I4_0);
-            //il.Emit(OpCodes.Newarr, typeof(object));
-            il.EmitCall(OpCodes.Callvirt, typeof(MethodInfo).GetMethod("Invoke", new Type[] { typeof(object), typeof(object[]) }), null);
-
-            //TODO figure out how to give a return type
-            il.Emit(OpCodes.Pop);
-
-            il.Emit(OpCodes.Ret);
-        }
-
-
-
-        public ServerDispatch() {
-
+            MethodInfo m = instance.GetType().GetMethod(targetMethod.Name);
+            return m.Invoke(instance, args);
         }
 
         public object CallObject(MethodInfo method, object[] args)
         {
-            MethodInfo m = instance.GetType().GetMethod(method.Name);
-            //return m.Invoke(instance, (object[])args[1]);
-            return m.Invoke(instance, args);
+            throw new NotImplementedException();
         }
     }
 
