b13f388724ce430c3641a37af9e32333b8cdb7b2
Prashanth Govindarajan <prgovi@microsoft.com>
2019-11-14 15:37:52 -0800

    Arcade will build, publish packages and symbols

--- src/System.Text.Primitives/Properties/AssemblyInfo.cs
+++ src/System.Text.Primitives/Properties/AssemblyInfo.cs
@@ -11,12 +11,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTrademark("")]
 
-[assembly: InternalsVisibleTo("System.Text.Primitives.Tests,PublicKey="+
-"0024000004800000940000000602000000240000525341310004000001000100039ac461fa5c82"+
-"c7dd2557400c4fd4e9dcdf7ac47e3d572548c04cd4673e004916610f4ea5cbf86f2b1ca1cb824f"+
-"2a7b3976afecfcf4eb72d9a899aa6786effa10c30399e6580ed848231fec48374e41b3acf88119"+
-"31343fc2f73acf72dae745adbcb7063cc4b50550618383202875223fc75401351cd89c44bf9b50"+
-"e7fa3796")]
+[assembly: InternalsVisibleTo("System.Text.Primitives.Tests,PublicKey=0024000004800000940000000602000000240000525341310004000001000100b5fc90e7027f67871e773a8fde8938c81dd402ba65b9201d60593e96c492651e889cc13f1415ebb53fac1131ae0bd333c5ee6021672d9718ea31a8aebd0da0072f25d87dba6fc90ffd598ed4da35e44c398c454307e8e33b8426143daec9f596836f97c8f74750e5975c64e2189f45def46b2a2b1247adc3652bf5c308055da9")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
