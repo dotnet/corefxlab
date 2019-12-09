c63b02b4abf4268a943db8408abbfe4f98e42744
Prashanth Govindarajan <prgovi@microsoft.com>
2019-12-09 14:02:24 -0800

    sq

--- tests/Microsoft.Data.Analysis.Tests/DataFrameTests.cs
+++ tests/Microsoft.Data.Analysis.Tests/DataFrameTests.cs
@@ -666,7 +666,7 @@ namespace Microsoft.Data.Analysis.Tests
             Assert.ThrowsAny<ArgumentException>(() => column.ReverseAddValue(5.5, inPlace: true));
             string str = "A String";
             Assert.ThrowsAny<NotImplementedException>(() => column.AddValue(str, inPlace: true));
-            Assert.ThrowsAny<NotImplementedException>(() => column.ReverseAddValue(str, inPlace: true));
+            Assert.ThrowsAny<ArgumentException>(() => column.ReverseAddValue(str, inPlace: true));
         }
 
         [Fact]
