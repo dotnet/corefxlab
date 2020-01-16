6f7ed6d935768092d03da22756e0e6d54699b02c
J W <woodjon@gmail.com>
2020-01-16 00:49:29 -0500

    Remove test for another issue

--- tests/Microsoft.Data.Analysis.Tests/DataFrame.IOTests.cs
+++ tests/Microsoft.Data.Analysis.Tests/DataFrame.IOTests.cs
@@ -242,22 +242,5 @@ CMT,1,1,181,0.6,CSH";
             Assert.Equal("CMT", reducedRows["vendor_id"][2]);
 
         }
-
-        [Fact(Skip="Skipping until 2787 is fixed. This test can be part of that fix.")]
-        public void TestReadCsvWithCommaInString()
-        {
-            string data = @"vendor_id,rate_code,passenger_count,trip_time_in_secs,trip_distance,payment_type,fare_amount
-'C,MT',1,1,1271,3.8,CRD,17.5
-CMT,1,1,474,1.5,CRD,8
-CMT,1,1,637,1.4,CRD,8.5
-CMT,1,1,181,0.6,CSH,4.5";
-
-            Stream GetStream(string streamData)
-            {
-                return new MemoryStream(Encoding.Default.GetBytes(streamData));
-            }
-
-            DataFrame df = DataFrame.LoadCsv(GetStream(data));
-        }
     }
 }
