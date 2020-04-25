ce5c992ecfc1b0dc823a2fd20e4865d2e455b693
Alex Williams <implementnap@gmail.com>
2020-04-25 01:47:29 -0500

    Added ToSpan Benchmark.

---
+++ tests/Benchmarks/System.Buffers/ToSpan.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Buffers;
+using System.Buffers.Tests;
+using System.Collections.Generic;
+using System.Collections.Sequences;
+using System.Linq;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+
+namespace Benchmarks.System.Buffers
+{
+    public class ToSpan
+    {
+        public static ReadOnlySequence<byte> Sequence = BufferFactory.Create(Enumerable.Repeat(0, 10000000)
+            .Select(x => new Memory<byte>(new byte[] { 0 })));
+
+        [Benchmark]
+        public void NewToSpan()
+        {
+            var Result = Sequence.ToSpan();
+        }
+
+        [Benchmark]
+        public void OldToSpan()
+        {
+            SequencePosition position = Sequence.Start;
+            ResizableArray<byte> array = new ResizableArray<byte>(1024);
+            while (Sequence.TryGet(ref position, out ReadOnlyMemory<byte> buffer))
+            {
+                array.AddAll(buffer.Span);
+            }
+            var Result = array.Span;
+        }
+    }
+}
