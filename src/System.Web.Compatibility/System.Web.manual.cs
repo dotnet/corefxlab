0a9c82bb79c7cee398e5fd394a0061e45f0c0d8b
Ian Kemp <ian.kemp@invu.net>
2020-09-02 15:23:57 +0100

    "implement" System.Web.Hosting.HostingEnvironment.ApplicationVirtualPath

--- src/System.Web.Compatibility/System.Web.manual.cs
+++ src/System.Web.Compatibility/System.Web.manual.cs
@@ -49,6 +49,7 @@ namespace System.Web.Hosting
     public partial class HostingEnvironment
     {
         public static string ApplicationPath => null;
+        public static string ApplicationVirtualPath => null;
         public static bool IsHosted => false;
         public static string SiteName => null;
 
