3a7bd84cf98785a012f01c21fea88f055587769f
Daniel Costea <daniel_costea@ymail.com>
2020-10-01 21:28:38 +0200

    Rename variable.

--- src/Microsoft.Data.Analysis/DataFrame.IO.cs
+++ src/Microsoft.Data.Analysis/DataFrame.IO.cs
@@ -375,7 +375,7 @@ namespace Microsoft.Data.Analysis
 
                     foreach (var row in dataFrame.Rows)
                     {
-                        bool firstColumn = true;
+                        bool firstRow = true;
                         foreach (var cell in row)
                         {
                             if (!firstRow)
